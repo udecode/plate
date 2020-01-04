6a8ea5f31e0cd5d21cb3f4dee59ddfb7fe99989b
zbeyens <ziyad.beyens@gmail.com>
2020-01-04 17:25:25 +0100

    feat: insertBreak

--- packages/slate-plugins/src/elements/action-item/withActionItem.ts
+++ packages/slate-plugins/src/elements/action-item/withActionItem.ts
@@ -3,8 +3,53 @@ import { Editor, Point, Range, Transforms } from 'slate';
 import { ACTION_ITEM } from './types';
 
 export const withActionItem = <T extends Editor>(editor: T) => {
-  const { deleteBackward } = editor;
+  const { deleteBackward, insertBreak } = editor;
 
+  /**
+   * Pressing enter on:
+   * - an empty block (except paragraph) replaces it with a new paragraph. Unwrap action item.
+   * - an action item block, add a new one
+   * TODO: same for list
+   */
+  editor.insertBreak = () => {
+    const match = Editor.above(editor, {
+      match: n => Editor.isBlock(editor, n),
+    });
+
+    if (match) {
+      const [matchingNode] = match;
+      if (matchingNode.type !== PARAGRAPH) {
+        if (
+          matchingNode.children[matchingNode.children.length - 1].text
+            .length === 0
+        ) {
+          Transforms.setNodes(editor, { type: PARAGRAPH });
+          Transforms.unwrapNodes(editor, {
+            match: n => n.type === ACTION_ITEM,
+            split: true,
+          });
+          return;
+        }
+        // if action item, add a new one
+        if (matchingNode.type === ACTION_ITEM) {
+          const checklist = {
+            type: ACTION_ITEM,
+            checked: false,
+            children: [{ text: '' }],
+          };
+          Transforms.insertNodes(editor, checklist);
+          return;
+        }
+      }
+    }
+
+    insertBreak();
+  };
+
+  /**
+   * If at the start of an action item (not selected),
+   * set to paragraph
+   */
   editor.deleteBackward = (...args) => {
     const { selection } = editor;
 
@@ -17,11 +62,7 @@ export const withActionItem = <T extends Editor>(editor: T) => {
         const [, path] = match;
         const start = Editor.start(editor, path);
         if (Point.equals(selection.anchor, start)) {
-          Transforms.setNodes(
-            editor,
-            { type: PARAGRAPH },
-            { match: n => n.type === ACTION_ITEM }
-          );
+          Transforms.setNodes(editor, { type: PARAGRAPH });
           return;
         }
       }
