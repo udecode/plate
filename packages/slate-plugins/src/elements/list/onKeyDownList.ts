d487f5d0b891c93b1cf033e497f327baae86cf95
Eivind Waaler <eivindwaaler@mnemonic.no>
2020-01-30 15:01:22 +0100

    Sort of working first version..

--- packages/slate-plugins/src/elements/list/onKeyDownList.ts
+++ packages/slate-plugins/src/elements/list/onKeyDownList.ts
@@ -1,16 +1,45 @@
-import { Editor, Path, Transforms } from 'slate';
+import { Editor, NodeEntry, Path, Text, Transforms } from 'slate';
 import { isBlockActive } from '../queries';
 import { isList } from './queries';
 import { ListType } from './types';
+
+const previousSibling = (
+  editor: Editor,
+  type: string,
+  siblingPath: Path,
+  lastPath: Path
+): NodeEntry | undefined => {
+  const previousItem = Editor.previous(editor, {
+    match: n => n.type === type,
+    at: lastPath,
+  });
+
+  if (!previousItem) {
+    return undefined;
+  }
+
+  const [, previousPath] = previousItem;
+
+  if (Path.isSibling(previousPath, siblingPath)) {
+    return previousItem;
+  }
+
+  return previousSibling(editor, type, siblingPath, previousPath);
+};
+
+/*
+  TODO - handle enter in paragraph in list item?
+
+  TODO - check if list exists before adding?
+
+  TODO - bug in sibling fn?
+ */
 
 export const onKeyDownList = () => (e: KeyboardEvent, editor: Editor) => {
   if (isBlockActive(editor, ListType.LIST_ITEM)) {
     if (e.key === 'Tab') {
       e.preventDefault();
 
-      const previousItem = Editor.previous(editor, {
-        match: n => n.type === ListType.LIST_ITEM,
-      });
       const currentItem = Editor.above(editor, {
         match: n => n.type === ListType.LIST_ITEM,
       });
@@ -18,31 +47,62 @@ export const onKeyDownList = () => (e: KeyboardEvent, editor: Editor) => {
         match: isList,
       });
 
-      console.log("elem", currentItem);
-      console.log("list", currentList);
-      console.log("prev", previousItem);
+      if (currentItem && currentList) {
+        const [currentElement, currentPath] = currentItem;
+        const [currentListElem, currentListPath] = currentList;
 
-      if (e.shiftKey) {
-        console.log('shift+tab pressed');
-      } else {
-        console.log('tab pressed');
-        if (
-          previousItem &&
-          currentItem &&
-          Path.isSibling(previousItem[1], currentItem[1])
-        ) {
-          console.log("new path", previousItem[1].concat([previousItem[0].children.length]));
-          Transforms.insertNodes(
-            editor,
-            {
-              type: currentList && currentList[0].type,
-              children: [currentItem[0]],
-            },
-            {
-              at: previousItem[1].concat([previousItem[0].children.length]),
+        if (e.shiftKey) {
+          const parentItem = Editor.above(editor, {
+            at: currentListPath,
+            match: n => n.type === ListType.LIST_ITEM,
+          });
+
+          if (parentItem) {
+            const [, parentPath] = parentItem;
+
+            Transforms.moveNodes(editor, {
+              at: currentPath,
+              to: Path.next(parentPath),
+            });
+
+            if (currentListElem.children.length === 1) {
+              Transforms.removeNodes(editor, {
+                at: currentListPath,
+              });
             }
+          }
+        } else {
+          const previousSiblingItem = previousSibling(
+            editor,
+            ListType.LIST_ITEM,
+            currentPath,
+            currentPath
           );
-          console.log('valid item!');
+
+          if (previousSiblingItem) {
+            const [previousElem, previousPath] = previousSiblingItem;
+            Transforms.removeNodes(editor, { at: currentPath });
+            previousElem.children.forEach((n: any, idx: number) => {
+              if (Text.isText(n)) {
+                Transforms.wrapNodes(
+                  editor,
+                  { type: 'paragraph', children: [] },
+                  { at: previousPath.concat(idx) }
+                );
+              }
+            });
+            Transforms.insertNodes(
+              editor,
+              {
+                type: currentListElem.type,
+                children: [currentElement],
+              },
+              {
+                at: previousPath.concat(previousElem.children.length),
+                select: true,
+              }
+            );
+          }
         }
       }
     }
