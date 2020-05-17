2f4a598a21ed83596ca97500feb82fe9f4346cd7
Petr Sahula <spetex@icloud.com>
2020-05-17 19:07:30 +0200

    Add second example with emphasis

--- packages/slate-plugins/src/__tests__/serializers/serialize-html/htmlSerialize.spec.ts
+++ packages/slate-plugins/src/__tests__/serializers/serialize-html/htmlSerialize.spec.ts
@@ -1,8 +1,13 @@
 import { htmlSerialize } from 'serializers/serialize-html';
 import { Node as SlateNode } from 'slate';
-import { BoldPlugin, htmlDeserialize, SlatePlugin } from '../../..';
+import {
+  BoldPlugin,
+  htmlDeserialize,
+  ItalicPlugin,
+  SlatePlugin,
+} from '../../..';
 
-const plugins: SlatePlugin[] = [BoldPlugin()];
+const plugins: SlatePlugin[] = [BoldPlugin(), ItalicPlugin()];
 
 const trimWhitespace = (rawHtml: string): string =>
   rawHtml.replace(/(\r\n|\n|\r|\t)/gm, '');
@@ -18,7 +23,7 @@ const htmlToSlateFormat = (rawHtml: string): SlateNode[] => {
 };
 
 const exampleHTML = `
-  Some paragraph of text with <strong>bold</strong> part.
+  Some paragraph of text with <strong>bold</strong> part and <em>italic</em> part.
 `;
 
 it('serialize to html', () => {
