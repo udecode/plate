5599d84c3137f7bce9c39c5aa617e392e4a1823a
Petr Sahula <spetex@icloud.com>
2020-05-23 12:44:04 +0200

    Make casing of sub/sup type consistent

--- packages/slate-plugins/src/marks/superscript/types.ts
+++ packages/slate-plugins/src/marks/superscript/types.ts
@@ -2,7 +2,7 @@ import { MarkPluginOptions, RenderLeafOptions } from 'mark';
 import { Text } from 'slate';
 import { RenderLeafProps } from 'slate-react';
 
-export const MARK_SUPERSCRIPT = 'SUPERSCRIPT';
+export const MARK_SUPERSCRIPT = 'superscript';
 
 // Data of Text node
 export interface SuperscriptNodeData {}
