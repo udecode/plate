01e461009982af47ee7472b20801f825d176e269
Mark Vujevits <vujevits.mark@gmail.com>
2020-05-24 21:59:50 +0200

    added a test which should fail if useMention does not accept broader types then { value: string}

--- packages/slate-plugins/src/__tests__/elements/mention/useMention/default.spec.tsx
+++ packages/slate-plugins/src/__tests__/elements/mention/useMention/default.spec.tsx
@@ -6,3 +6,11 @@ it('should be', () => {
 
   expect(result.current.index).toBe(0);
 });
+
+it('should accept arbirtrary data with value field', () => {
+  const { result } = renderHook(() =>
+    useMention([{ value: 'John Snow', id: 5, house: ['Targaryen', 'Stark'] }])
+  );
+
+  expect(result.current.index).toBe(0);
+});
