14c001e8beba577ac3526ac8ed13fc7c65e2b628
Petr Sahula <sahulpet@gmail.com>
2020-06-28 15:33:49 +0200

    Remove duplicate

--- packages/slate-plugins/src/elements/align/unwrapAlign.ts
+++ packages/slate-plugins/src/elements/align/unwrapAlign.ts
@@ -1,18 +1,9 @@
-import { castArray } from 'lodash';
-import { Editor, Transforms } from 'slate';
+import { Editor } from 'slate';
+import { unwrapNodesByType } from '../../'
 import { ALIGN_CENTER as align_center, ALIGN_LEFT as align_left, ALIGN_RIGHT as align_right } from './types';
 
-export const unwrapNodesByType = (editor: any, types: any, options = {}) => {
-  types = castArray(types);
-
-  Transforms.unwrapNodes(editor, {
-    match: (n) => types.includes(n.type),
-    ...options,
-  });
-};
-
 export const unwrapAlign = (
-  editor = Editor,
+  editor: Editor,
   { ALIGN_LEFT = align_left, ALIGN_CENTER = align_center, ALIGN_RIGHT = align_right } = {}
 ) => {
   unwrapNodesByType(editor, [ALIGN_LEFT, ALIGN_CENTER, ALIGN_RIGHT], { split: true });
