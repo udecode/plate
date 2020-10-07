1e4a75d85ef180ab517485082c117b087c958c74
William Corry <william@wrought-webdesign.co.uk>
2020-10-07 16:21:04 +0100

    added check on text as % causes an error

--- packages/slate-plugins/src/serializers/serialize-html/__tests__/nodes.spec.ts
+++ packages/slate-plugins/src/serializers/serialize-html/__tests__/nodes.spec.ts
@@ -1,11 +1,11 @@
-import { ListPlugin } from '../../../elements/list/ListPlugin';
-import { ParagraphPlugin } from '../../../elements/paragraph/ParagraphPlugin';
-import { BoldPlugin } from '../../../marks/bold/BoldPlugin';
-import { ItalicPlugin } from '../../../marks/italic/ItalicPlugin';
-import { serializeHTMLFromNodes } from '../serializeHTMLFromNodes';
-import { htmlStringToDOMNode } from '../utils/htmlStringToDOMNode';
+import { ListPlugin } from "../../../elements/list/ListPlugin";
+import { ParagraphPlugin } from "../../../elements/paragraph/ParagraphPlugin";
+import { BoldPlugin } from "../../../marks/bold/BoldPlugin";
+import { ItalicPlugin } from "../../../marks/italic/ItalicPlugin";
+import { serializeHTMLFromNodes } from "../serializeHTMLFromNodes";
+import { htmlStringToDOMNode } from "../utils/htmlStringToDOMNode";
 
-it('serialize complex example list with paragraphs to html', () => {
+it("serialize complex example list with paragraphs to html", () => {
   const render = htmlStringToDOMNode(
     serializeHTMLFromNodes([
       ItalicPlugin(),
@@ -14,51 +14,51 @@ it('serialize complex example list with paragraphs to html', () => {
       ListPlugin(),
     ])([
       {
-        type: 'p',
+        type: "p",
         children: [
           {
-            text: 'Some paragraph that contains, ',
+            text: "Some paragraph that contains, ",
           },
           {
-            text: 'italicized text',
+            text: "italicized text",
             italic: true,
           },
           {
-            text: ' and ',
+            text: " and ",
           },
           {
-            text: 'bolded text',
+            text: "bolded text",
             bold: true,
           },
           {
-            text: ' is first.',
+            text: " is first.",
           },
         ],
       },
       {
-        type: 'ul',
+        type: "ul",
         children: [
           {
-            type: 'li',
+            type: "li",
             children: [
               {
-                type: 'p',
+                type: "p",
                 children: [
                   {
-                    text: 'Item one in list',
+                    text: "Item one in list",
                   },
                 ],
               },
             ],
           },
           {
-            type: 'li',
+            type: "li",
             children: [
               {
-                type: 'p',
+                type: "p",
                 children: [
                   {
-                    text: 'Item two in list',
+                    text: "Item two in list",
                   },
                 ],
               },
@@ -68,18 +68,18 @@ it('serialize complex example list with paragraphs to html', () => {
       },
     ])
   );
-  expect(render.getElementsByTagName('p').length).toEqual(3);
-  expect(render.getElementsByTagName('p')[0].outerHTML).toBe(
+  expect(render.getElementsByTagName("p").length).toEqual(3);
+  expect(render.getElementsByTagName("p")[0].outerHTML).toBe(
     '<p class="slate-p">Some paragraph that contains, <em class="slate-italic">italicized text</em> and <strong class="slate-bold">bolded text</strong> is first.</p>'
   );
-  expect(render.getElementsByTagName('ul').length).toEqual(1);
-  expect(render.getElementsByTagName('li').length).toEqual(2);
-  expect(render.getElementsByTagName('ul')[0].innerHTML).toBe(
+  expect(render.getElementsByTagName("ul").length).toEqual(1);
+  expect(render.getElementsByTagName("li").length).toEqual(2);
+  expect(render.getElementsByTagName("ul")[0].innerHTML).toBe(
     '<li class="slate-li"><p class="slate-p">Item one in list</p></li><li class="slate-li"><p class="slate-p">Item two in list</p></li>'
   );
 });
 
-it('serialize complex example with no type on top level node to html', () => {
+it("serialize complex example with no type on top level node to html", () => {
   const render = serializeHTMLFromNodes([
     ItalicPlugin(),
     BoldPlugin(),
@@ -89,24 +89,24 @@ it('serialize complex example with no type on top level node to html', () => {
     {
       children: [
         {
-          type: 'p',
+          type: "p",
           children: [
             {
-              text: 'Some paragraph that contains, ',
+              text: "Some paragraph that contains, ",
             },
             {
-              text: 'italicized text',
+              text: "italicized text",
               italic: true,
             },
             {
-              text: ' and ',
+              text: " and ",
             },
             {
-              text: 'bolded text',
+              text: "bolded text",
               bold: true,
             },
             {
-              text: ' is first.',
+              text: " is first.",
             },
           ],
         },
@@ -118,7 +118,7 @@ it('serialize complex example with no type on top level node to html', () => {
   );
 });
 
-it('serialize complex example with multiple no types on top level node to html', () => {
+it("serialize complex example with multiple no types on top level node to html", () => {
   const render = serializeHTMLFromNodes([
     ItalicPlugin(),
     BoldPlugin(),
@@ -128,34 +128,57 @@ it('serialize complex example with multiple no types on top level node to html',
     {
       children: [
         {
-          type: 'p',
+          type: "p",
           children: [
             {
-              text: 'Some paragraph that contains, ',
+              text: "Some paragraph that contains, ",
             },
             {
-              text: 'italicized text',
+              text: "italicized text",
               italic: true,
             },
             {
-              text: ' and ',
+              text: " and ",
             },
             {
-              text: 'bolded text',
+              text: "bolded text",
               bold: true,
             },
             {
-              text: ' is first.',
+              text: " is first.",
             },
           ],
         },
       ],
     },
     {
-      children: [{ text: 'FOO', bold: true }],
+      children: [{ text: "FOO", bold: true }],
     },
   ]);
   expect(render).toBe(
     '<div><p class="slate-p">Some paragraph that contains, <em class="slate-italic">italicized text</em> and <strong class="slate-bold">bolded text</strong> is first.</p></div><div><strong class="slate-bold">FOO</strong></div>'
+  );
+});
+
+it("serialize string with %", () => {
+  const render = serializeHTMLFromNodes([ParagraphPlugin()])([
+    {
+      children: [
+        {
+          type: "p",
+          children: [
+            {
+              text: "None encoded string 100%",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      children: [{ text: "Encoded string 100%25" }],
+    },
+  ]);
+  expect(render).toBe(
+    '<div><p class="slate-p">None encoded string 100%</p></div><div>Encoded string 100%</div>'
   );
 });
