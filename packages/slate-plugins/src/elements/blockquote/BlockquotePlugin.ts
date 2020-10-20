c6b8895f1b9bdb9eec79e0d9785ca9601d710dd9
baudic_j <julien@cycle.app>
2020-10-20 16:34:57 +0200

    IMP(hotkey-types): update BlockquotePlugin

--- packages/slate-plugins/src/elements/blockquote/BlockquotePlugin.ts
+++ packages/slate-plugins/src/elements/blockquote/BlockquotePlugin.ts
@@ -2,6 +2,8 @@ import { SlatePlugin } from '@udecode/slate-plugins-core';
 import { deserializeBlockquote } from './deserializeBlockquote';
 import { renderElementBlockquote } from './renderElementBlockquote';
 import { BlockquotePluginOptions } from './types';
+import { onKeyDownTypeDefault } from '../../common/utils/onKeyDownTypeDefault';
+import { DEFAULTS_BLOCKQUOTE } from './defaults';
 
 /**
  * Enables support for block quotes, useful for
@@ -12,4 +14,9 @@ export const BlockquotePlugin = (
 ): SlatePlugin => ({
   renderElement: renderElementBlockquote(options),
   deserialize: deserializeBlockquote(options),
+  onKeyDown: onKeyDownTypeDefault({
+    key: 'blockquote',
+    defaultOptions: DEFAULTS_BLOCKQUOTE,
+    options,
+  }),
 });
