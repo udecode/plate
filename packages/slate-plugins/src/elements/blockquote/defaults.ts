d0cdf4a533c5a4c866651d9cf3f73b7481ba4a0c
baudic_j <julien@cycle.app>
2020-10-20 16:33:27 +0200

    IMP(hotkey-types): add hotkey for blockquote

--- packages/slate-plugins/src/elements/blockquote/defaults.ts
+++ packages/slate-plugins/src/elements/blockquote/defaults.ts
@@ -1,15 +1,17 @@
 import { BlockquoteElement } from './components/BlockquoteElement';
 import { BlockquoteKeyOption, BlockquotePluginOptionsValues } from './types';
+import { TypeOnKeyDownOptions } from '../../common/utils/onKeyDownType';
 
 export const ELEMENT_BLOCKQUOTE = 'blockquote';
 
 export const DEFAULTS_BLOCKQUOTE: Record<
   BlockquoteKeyOption,
-  Required<BlockquotePluginOptionsValues>
+  Required<BlockquotePluginOptionsValues> & TypeOnKeyDownOptions
 > = {
   blockquote: {
     component: BlockquoteElement,
     type: ELEMENT_BLOCKQUOTE,
+    hotkey: 'mod+shift+.',
     rootProps: {
       className: 'slate-blockquote',
       as: 'blockquote',
