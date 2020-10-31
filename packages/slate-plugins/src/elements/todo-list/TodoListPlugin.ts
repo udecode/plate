e29baf6a90d52692a0ddd9f49cf2668fabe0b873
zbeyens <ziyad.beyens@gmail.com>
2020-10-31 03:43:44 +0100

    feat: hotkeys

--- packages/slate-plugins/src/elements/todo-list/TodoListPlugin.ts
+++ packages/slate-plugins/src/elements/todo-list/TodoListPlugin.ts
@@ -1,4 +1,6 @@
 import { SlatePlugin } from '@udecode/slate-plugins-core';
+import { getOnHotkeyToggleNodeTypeDefault } from '../../common/utils/getOnHotkeyToggleNodeTypeDefault';
+import { DEFAULTS_TODO_LIST } from './defaults';
 import { deserializeTodoList } from './deserializeTodoList';
 import { renderElementTodoList } from './renderElementTodoList';
 import { TodoListPluginOptions } from './types';
@@ -8,4 +10,9 @@ export const TodoListPlugin = (
 ): SlatePlugin => ({
   renderElement: renderElementTodoList(options),
   deserialize: deserializeTodoList(options),
+  onKeyDown: getOnHotkeyToggleNodeTypeDefault({
+    key: 'todo_li',
+    defaultOptions: DEFAULTS_TODO_LIST,
+    options,
+  }),
 });
