6a80bdf8e1fcffe899b421dd0d26efd9699eb621
Dylan Schiemann <dylan@livingspec.com>
2021-01-03 06:22:44 -0700

    Missing exports for next table tab/shift-tab handler

---
+++ packages/slate-plugins/src/elements/table/queries/index.ts
@@ -0,0 +1,5 @@
+export * from './getCellInNextTableRow';
+export * from './getCellInPreviousTableRow';
+export * from './getNextTableCell';
+export * from './getPreviousTableCell';
+export * from './getTableCellEntry';
