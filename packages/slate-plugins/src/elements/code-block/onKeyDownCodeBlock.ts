8c37c54ce3f1253d1d5cafab6109064ae5d9ff6d
Dylan Schiemann <dylan@livingspec.com>
2021-02-22 14:10:23 -0700

    code-block refactor select all shortcut

--- packages/slate-plugins/src/elements/code-block/onKeyDownCodeBlock.ts
+++ packages/slate-plugins/src/elements/code-block/onKeyDownCodeBlock.ts
@@ -1,4 +1,4 @@
-import { Editor } from 'slate';
+import { Editor, Transforms } from 'slate';
 import { getCodeLineEntry } from './queries/getCodeLineEntry';
 import { getIndentDepth } from './queries/getIndentDepth';
 import { indentCodeLine } from './transforms/indentCodeLine';
@@ -13,6 +13,7 @@ import { CodeBlockOnKeyDownOptions, CodeLineOnKeyDownOptions } from './types';
 export const onKeyDownCodeBlock = (
   options?: CodeBlockOnKeyDownOptions & CodeLineOnKeyDownOptions
 ) => (e: KeyboardEvent, editor: Editor) => {
+  console.log(e);
   if (e.key === 'Tab') {
     const res = getCodeLineEntry(editor, {}, options);
     if (!res) return;
@@ -36,12 +37,23 @@ export const onKeyDownCodeBlock = (
     return;
   }
 
-  if (e.key === 'mod+a') {
+  // FIXME: would prefer this as mod+a, but doesn't work
+  if (e.key === 'a' && (e.metaKey || e.ctrlKey)) {
+    console.log(e);
+    e.preventDefault();
+    e.stopPropagation();
+    const res = getCodeLineEntry(editor, {}, options);
+    if (!res) return;
+    console.log(res);
+    const { codeBlock } = res;
+    const [, codeBlockPath] = codeBlock;
+    Transforms.select(editor, codeBlockPath);
     // select all text
     return;
   }
 
   if (e.key === 'mod+enter') {
+    // FIXME: this already works, verify if we need to add something specific here
     // exit code block, move cursor to block after current code-block
   }
 };
