20e546aefe9308e422ae0723a5967ba1da52ebef
Albert Putra Purnama <albertputrapurnama@gmail.com>
2021-02-24 14:04:00 -0800

    Update react-dnd to v13 and dnd-html5-backend to v12

--- packages/slate-plugins/src/dnd/hooks/useDragBlock.ts
+++ packages/slate-plugins/src/dnd/hooks/useDragBlock.ts
@@ -2,16 +2,19 @@ import { useDrag } from 'react-dnd';
 import { Editor } from 'slate';
 
 export const useDragBlock = (editor: Editor, id: string) => {
-  return useDrag({
-    item: { type: 'block', id },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
+  return useDrag(
+    () => ({
+      item: { type: 'block', id },
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+      begin: () => {
+        document.body.classList.add('dragging');
+      },
+      end: () => {
+        document.body.classList.remove('dragging');
+      },
     }),
-    begin: () => {
-      document.body.classList.add('dragging');
-    },
-    end: () => {
-      document.body.classList.remove('dragging');
-    },
-  });
+    []
+  );
 };
