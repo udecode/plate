841914a4386b50b5087730779d90d9f126332add
Dylan Schiemann <dylan@livingspec.com>
2021-03-06 23:16:49 -0700

    during cleanup I accidentially inverted the logic for preserving vs. adding a new block for code-block

--- packages/slate-plugins/src/elements/code-block/transforms/insertEmptyCodeBlock.ts
+++ packages/slate-plugins/src/elements/code-block/transforms/insertEmptyCodeBlock.ts
@@ -31,7 +31,7 @@ export const insertEmptyCodeBlock = (
   const defaultType = pluginsOptions.defaultType || DEFAULT_ELEMENT;
   const level = pluginsOptions.level || 1;
 
-  if (isExpanded(editor.selection) || isBlockAboveEmpty(editor)) {
+  if (isExpanded(editor.selection) || !isBlockAboveEmpty(editor)) {
     const selectionPath = Editor.path(editor, editor.selection);
     const insertPath = Path.next(selectionPath.slice(0, level + 1));
     Transforms.insertNodes(
