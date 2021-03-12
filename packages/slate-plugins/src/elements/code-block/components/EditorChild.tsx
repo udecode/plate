e5ac66304c7c9ff55c48d14b7bcb828809b20777
Dylan Schiemann <dylan@livingspec.com>
2021-03-12 09:02:01 -0700

    WIP code-block as void

--- packages/slate-plugins/src/elements/code-block/components/EditorChild.tsx
+++ packages/slate-plugins/src/elements/code-block/components/EditorChild.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
-import { RenderElementProps } from 'slate-react';
+import { useMemo, useState } from 'react';
+import { EditablePlugins } from '@udecode/slate-plugins-core';
+import { createEditor } from 'slate';
+import { withHistory } from 'slate-history';
+import { Slate, withReact } from 'slate-react';
+import { pipe, SlateDocument } from '../../../common';
+import { CodeBlockPlugin } from '../CodeBlockPlugin';
 import { EditorChildProps } from '../types';
 
-export const EditorChild = ({
-  attributes,
-  children,
-}: RenderElementProps & EditorChildProps) => {
+const plugins = [CodeBlockPlugin];
+
+const withPlugins = [withReact, withHistory] as const;
+
+export const EditorChild = ({ initialValue }: EditorChildProps) => {
+  const [value, setValue] = useState(initialValue);
+
+  const editor = useMemo(() => pipe(createEditor(), ...withPlugins), []);
+
   return (
-    <div {...attributes} contentEditable>
-      {children}
-    </div>
+    <Slate
+      editor={editor}
+      value={value!}
+      onChange={(newValue) => setValue(newValue as SlateDocument)}
+    >
+      <EditablePlugins plugins={plugins} autoFocus />
+    </Slate>
   );
 };
