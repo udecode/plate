ebe9273ef6f773fffc85da69737428304102e4dd
zbeyens <ziyad.beyens@gmail.com>
2021-03-28 04:47:35 +0200

    types

--- packages/core/src/hooks/useTSlate.ts
+++ packages/core/src/hooks/useTSlate.ts
@@ -1,10 +1,10 @@
 import { HistoryEditor } from 'slate-history/dist/history-editor';
 import { ReactEditor, useSlate } from 'slate-react';
 import { SPEditor } from '../types/SPEditor';
+import { TEditor } from '../types/TEditor';
 
 /**
  * Typed {@link useSlate} & SPEditor.
  */
-export const useTSlate = <
-  TEditor extends TEditor = ReactEditor & HistoryEditor
->() => useSlate() as TEditor & SPEditor;
+export const useTSlate = <T extends TEditor = ReactEditor & HistoryEditor>() =>
+  (useSlate() as unknown) as T & SPEditor;
--- packages/core/src/hooks/useTSlateStatic.ts
+++ packages/core/src/hooks/useTSlateStatic.ts
@@ -1,10 +1,11 @@
 import { HistoryEditor } from 'slate-history/dist/history-editor';
 import { ReactEditor, useSlateStatic } from 'slate-react';
 import { SPEditor } from '../types/SPEditor';
+import { TEditor } from '../types/TEditor';
 
 /**
  * Typed {@link useSlateStatic} & SPEditor.
  */
 export const useTSlateStatic = <
-  TEditor extends TEditor = ReactEditor & HistoryEditor
->() => useSlateStatic() as TEditor & SPEditor;
+  T extends TEditor = ReactEditor & HistoryEditor
+>() => (useSlateStatic() as unknown) as T & SPEditor;
