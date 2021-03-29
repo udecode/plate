29ad804cb6eeb5c7bc083e0a32c07b3cd01dd8ef
zbeyens <ziyad.beyens@gmail.com>
2021-03-29 21:40:38 +0200

    refactor

--- packages/common/src/__tests__/transforms/withNodeID/insert-node-without-filterText.spec.tsx
+++ packages/common/src/__tests__/transforms/withNodeID/insert-node-without-filterText.spec.tsx
@@ -2,7 +2,7 @@
 import { jsx } from '@udecode/slate-plugins-test-utils';
 import { Editor } from 'slate';
 import { withHistory } from 'slate-history';
-import { withNodeId } from '../../../../../node-id/src/createKbdPlugin';
+import { withNodeId } from '../../../../../node-id/src/createNodeIdPlugin';
 import { idCreatorFixture } from './fixtures';
 
 jsx;
