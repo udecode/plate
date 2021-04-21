109f7755539da20b5ff79c15ca05fa748179becf
zbeyens <ziyad.beyens@gmail.com>
2021-04-21 17:02:59 +0200

    style

--- packages/core/src/types/SlatePlugin/OnKeyDown.ts
+++ packages/core/src/types/SlatePlugin/OnKeyDown.ts
@@ -4,4 +4,6 @@ import { SPEditor } from '../SPEditor';
  * Function called on key down event.
  * To prevent the next handler from running, return false.
  */
-export type OnKeyDown<T extends SPEditor = SPEditor> = (editor: T) => (e: any) => boolean | void;
+export type OnKeyDown<T extends SPEditor = SPEditor> = (
+  editor: T
+) => (e: any) => boolean | void;
