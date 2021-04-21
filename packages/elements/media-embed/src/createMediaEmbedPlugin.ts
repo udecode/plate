ba333ecb8651a8d2c34aca7ef3f8de7459556ddb
Dylan Schiemann <dylan@livingspec.com>
2021-04-21 16:27:10 -0700

    make media-embed pluginKey configurable

--- packages/elements/media-embed/src/createMediaEmbedPlugin.ts
+++ packages/elements/media-embed/src/createMediaEmbedPlugin.ts
@@ -10,9 +10,11 @@ import { getMediaEmbedDeserialize } from './getMediaEmbedDeserialize';
  * Enables support for embeddable media such as YouTube
  * or Vimeo videos, Instagram posts and tweets or Google Maps.
  */
-export const createMediaEmbedPlugin = (): SlatePlugin => ({
-  pluginKeys: ELEMENT_MEDIA_EMBED,
-  renderElement: getRenderElement(ELEMENT_MEDIA_EMBED),
-  deserialize: getMediaEmbedDeserialize(),
-  voidTypes: getSlatePluginTypes(ELEMENT_MEDIA_EMBED),
+export const createMediaEmbedPlugin = ({
+  pluginKey = ELEMENT_MEDIA_EMBED,
+}): SlatePlugin => ({
+  pluginKeys: pluginKey,
+  renderElement: getRenderElement(pluginKey),
+  deserialize: getMediaEmbedDeserialize(pluginKey),
+  voidTypes: getSlatePluginTypes(pluginKey),
 });
