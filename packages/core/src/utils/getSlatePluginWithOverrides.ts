4bf6b49cae3dfe1e5efb984fb967f0891a6357d4
Horacio Herrera <hi@horacioh.com>
2021-05-06 01:40:45 +0200

    improve typings on createAutoformatPlugin

--- packages/core/src/utils/getSlatePluginWithOverrides.ts
+++ packages/core/src/utils/getSlatePluginWithOverrides.ts
@@ -5,8 +5,13 @@ import { WithOverride } from '../types';
 import { SlatePlugin } from '../types/SlatePlugin/SlatePlugin';
 import { SPEditor } from '../types/SPEditor';
 
-export const getSlatePluginWithOverrides = <T extends SPEditor = SPEditor>(
-  withOverrides: (...args: any) => WithOverride<T>
-) => (options?: Parameters<typeof withOverrides>[0]): SlatePlugin<T> => ({
+export const getSlatePluginWithOverrides = <
+  T extends SPEditor = SPEditor,
+  V extends (...args: any) => WithOverride<T> = (
+    ...args: any
+  ) => WithOverride<T>
+>(
+  withOverrides: V
+) => (...options: Parameters<V>): SlatePlugin<T> => ({
   withOverrides: withOverrides(options),
 });
