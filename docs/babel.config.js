e4e36c249f21b30b3092b454c74c9b4e50d5c185
zbeyens <ziyad.beyens@gmail.com>
2021-06-13 13:34:58 +0200

    docs: fix FOUC

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -3,7 +3,7 @@ const babelConfig = require('../babel.config');
 module.exports = {
   plugins: [...babelConfig.plugins, '@babel/plugin-transform-modules-commonjs'],
   presets: [
-    require.resolve('@docusaurus/core/lib/babel/preset'),
     ...babelConfig.presets,
+    require.resolve('@docusaurus/core/lib/babel/preset'),
   ],
 };
