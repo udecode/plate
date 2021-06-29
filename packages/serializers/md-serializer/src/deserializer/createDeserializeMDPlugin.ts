ed8566497b15fad0f08c257b128203eb251ebdb0
Dylan Schiemann <dylan@livingspec.com>
2021-06-29 05:06:38 -0700

    revert

--- packages/serializers/md-serializer/src/deserializer/createDeserializeMDPlugin.ts
+++ packages/serializers/md-serializer/src/deserializer/createDeserializeMDPlugin.ts
@@ -1,10 +1,7 @@
-import { isBlockAboveEmpty, setNodes } from '@udecode/slate-plugins-common';
+import { setNodes } from '@udecode/slate-plugins-common';
 import {
-  getInlineTypes,
   getSlatePluginWithOverrides,
-  SlatePlugin,
   SPEditor,
-  TDescendant,
   TElement,
   WithOverride,
 } from '@udecode/slate-plugins-core';
@@ -12,71 +9,28 @@ import { Transforms } from 'slate';
 import { ReactEditor } from 'slate-react';
 import { deserializeMD } from './utils/deserializeMD';
 
-export interface WithDeserializeMDOptions<
-  T extends SPEditor = SPEditor & ReactEditor
-> {
-  plugins?: SlatePlugin<T>[];
-
-  /**
-   * Function called before inserting the deserialized md.
-   * Default: if the block above is empty and the first fragment node type is not inline,
-   * set the selected node type to the first fragment node type.
-   */
-  preInsert?: (fragment: TDescendant[]) => TDescendant[];
-
-  /**
-   * Function called to insert the deserialized md.
-   * Default: Transforms.insertFragment.
-   */
-  insert?: (fragment: TDescendant[]) => void;
-}
-
 /**
  * Enables support for deserializing content
  * from Markdown format to Slate format.
  */
-export const withDeserializeMD = <
-  T extends ReactEditor & SPEditor = ReactEditor & SPEditor
->({
-  plugins = [],
-  ...options
-}: WithDeserializeMDOptions<T> = {}): WithOverride<T> => (editor) => {
+export const withDeserializeMD = (): WithOverride<ReactEditor & SPEditor> => (
+  editor
+) => {
   const { insertData } = editor;
-
-  const {
-    preInsert = (fragment) => {
-      const inlineTypes = getInlineTypes(editor, plugins);
-
-      const firstNodeType = fragment[0].type as string | undefined;
-
-      // replace the selected node type by the first block type
-      if (
-        isBlockAboveEmpty(editor) &&
-        firstNodeType &&
-        !inlineTypes.includes(firstNodeType)
-      ) {
-        setNodes<TElement>(editor, { type: fragment[0].type });
-      }
-
-      return fragment;
-    },
-
-    insert = (fragment) => {
-      Transforms.insertFragment(editor, fragment);
-    },
-  } = options;
 
   editor.insertData = (data) => {
     const content = data.getData('text/plain');
 
     if (content) {
-      let fragment = deserializeMD(editor, content);
+      const fragment = deserializeMD(editor, content);
 
       if (!fragment.length) return;
 
-      fragment = preInsert(fragment);
+      if (fragment[0].type) {
+        setNodes<TElement>(editor, { type: fragment[0].type });
+      }
 
-      insert(fragment);
+      Transforms.insertFragment(editor, fragment);
       return;
     }
 
