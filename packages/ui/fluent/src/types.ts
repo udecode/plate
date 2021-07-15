4be81bc4fa66be98f2f154d047ded00308f0a1d7
zbeyens <ziyad.beyens@gmail.com>
2021-07-15 13:24:51 +0200

    v0.59.1

--- packages/ui/fluent/src/types.ts
+++
@@ -1,7 +0,0 @@
-import { classNamesFunction } from '@uifabric/utilities';
-import { ClassName, RootStyleSet } from './StyledNode/StyledNode.types';
-
-export const getRootClassNames = <
-  TStyleProps = ClassName,
-  TStyles = RootStyleSet
->() => classNamesFunction<TStyleProps, TStyles>();
