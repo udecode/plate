bdbf3ba0d0459a41b991764256396ca8727d7cdd
zbeyens <ziyad.beyens@gmail.com>
2021-07-22 02:19:57 +0200

    refactor

--- packages/core/src/types/SlatePlugin/Deserialize.ts
+++ packages/core/src/types/SlatePlugin/Deserialize.ts
@@ -30,7 +30,7 @@ export type Deserialize<T extends SPEditor = SPEditor> = (
 
   /**
    * Function called on `editor.insertData` just before `editor.insertFragment`.
-   * Default: if the block above is empty and the first fragment node type is not inline,
+   * Default: if the block above the selection is empty and the first fragment node type is not inline,
    * set the selected node type to the first fragment node type.
    * @return if true, the next handlers will be skipped.
    */
