35dc34a9b9c5b9da40bf49792870ed84f04580d3
Dylan Schiemann <dylan@livingspec.com>
2021-07-23 17:37:13 -0700

    parse embed code when pasting into media-embed url field

--- packages/elements/media-embed-ui/src/MediaEmbedElement/MediaEmbedUrlInput.tsx
+++ packages/elements/media-embed-ui/src/MediaEmbedElement/MediaEmbedUrlInput.tsx
@@ -13,12 +13,25 @@ export const MediaEmbedUrlInput = ({
 }) => {
   const [value, setValue] = React.useState(url);
 
+  const validateUrl = (newUrl: string) => {
+    // if not starting with http, assume pasting of full iframe embed code
+    if (newUrl.substring(0, 4) !== 'http') {
+      const regex = /(?<=src=").*?(?=[*"])/g;
+      const src = newUrl.match(regex)?.[0];
+      if (src) {
+        newUrl = src;
+      }
+    }
+    return newUrl;
+  };
+
   return (
     <input
       value={value}
       onClick={(e) => e.stopPropagation()}
       onChange={(e) => {
         const newUrl = e.target.value;
+        validateUrl(newUrl);
         setValue(newUrl);
         onChange(newUrl);
       }}
