b0a019c91f267f6a8f46d122fabd3a0107b51e66
zbeyens <ziyad.beyens@gmail.com>
2021-07-31 16:53:31 +0200

    fix

---
+++ packages/elements/list/src/getListDeserialize.spec.tsx
@@ -0,0 +1,121 @@
+/** @jsx jsx */
+
+import { SPEditor, TDescendant } from '@udecode/plate-core';
+import { jsx } from '@udecode/plate-test-utils';
+import { createEditorPlugins } from '../../../plate/src/utils/createEditorPlugins';
+import { createListPlugin } from './createListPlugin';
+import { getListDeserialize } from './getListDeserialize';
+
+jsx;
+
+const editorTest = (input: any, fragment: any, expected: any) => {
+  const editor = createEditorPlugins({
+    editor: input,
+    plugins: [createListPlugin()],
+  });
+
+  const deserializer = getListDeserialize()(editor);
+
+  // const filteredFragment = deserializer.getFragment?.(fragment) ?? [];
+
+  // deserializer.preInsert?.(fragment);
+  // console.log(editor.selection);
+  editor.insertFragment(fragment);
+
+  expect(editor.children).toEqual(expected.children);
+};
+
+describe('when pasting ul > 2 li fragment', () => {
+  describe('when selection in li', () => {
+    it('should filter out ul', () => {
+      const input = ((
+        <editor>
+          <hul>
+            <hli>
+              <hlic>one</hlic>
+            </hli>
+            <hli>
+              <hlic>
+                two
+                <cursor />
+              </hlic>
+            </hli>
+            <hli>
+              <hlic>four</hlic>
+            </hli>
+          </hul>
+        </editor>
+      ) as any) as SPEditor;
+
+      const fragment = ((
+        <fragment>
+          <hul>
+            <hli>
+              <hlic>three</hlic>
+            </hli>
+          </hul>
+        </fragment>
+      ) as any) as TDescendant[];
+
+      const expected = ((
+        <editor>
+          <hul>
+            <hli>
+              <hlic>one</hlic>
+            </hli>
+            <hli>
+              <hlic>two</hlic>
+            </hli>
+            <hli>
+              <hlic>three</hlic>
+            </hli>
+            <hli>
+              <hlic>four</hlic>
+            </hli>
+          </hul>
+        </editor>
+      ) as any) as SPEditor;
+
+      editorTest(input, fragment, expected);
+    });
+  });
+
+  describe('when selection not in li', () => {
+    it('should paste the list', () => {
+      const input = ((
+        <editor>
+          <hp>
+            one
+            <cursor />
+          </hp>
+        </editor>
+      ) as any) as SPEditor;
+
+      const fragment = ((
+        <fragment>
+          <hul>
+            <hli>
+              <hlic>three</hlic>
+            </hli>
+          </hul>
+        </fragment>
+      ) as any) as TDescendant[];
+
+      const expected = ((
+        <editor>
+          <hp>
+            one
+            <cursor />
+          </hp>
+          <hul>
+            <hli>
+              <hlic>three</hlic>
+            </hli>
+          </hul>
+        </editor>
+      ) as any) as SPEditor;
+
+      editorTest(input, fragment, expected);
+    });
+  });
+});
--- packages/elements/list/src/withList.ts
+++ packages/elements/list/src/withList.ts
@@ -1,14 +1,31 @@
-import { SPEditor, WithOverride } from '@udecode/plate-core';
+import { findNode } from '@udecode/plate-common';
+import {
+  getPlatePluginOptions,
+  SPEditor,
+  TDescendant,
+  WithOverride,
+} from '@udecode/plate-core';
+import { Path, Transforms } from 'slate';
+import { getListNormalizer } from './normalizers/getListNormalizer';
+import { ELEMENT_LI, ELEMENT_OL, ELEMENT_UL } from './defaults';
 import { getListDeleteBackward } from './getListDeleteBackward';
 import { getListDeleteFragment } from './getListDeleteFragment';
 import { getListInsertBreak } from './getListInsertBreak';
-import { getListNormalizer } from './normalizers';
 import { WithListOptions } from './types';
 
 export const withList = ({
   validLiChildrenTypes,
 }: WithListOptions = {}): WithOverride<SPEditor> => (editor) => {
-  const { insertBreak, deleteBackward, deleteFragment } = editor;
+  const {
+    insertBreak,
+    deleteBackward,
+    deleteFragment,
+    insertFragment,
+  } = editor;
+
+  const li = getPlatePluginOptions(editor, ELEMENT_LI);
+  const ul = getPlatePluginOptions(editor, ELEMENT_UL);
+  const ol = getPlatePluginOptions(editor, ELEMENT_OL);
 
   editor.insertBreak = () => {
     if (getListInsertBreak(editor)) return;
@@ -26,6 +43,41 @@ export const withList = ({
     if (getListDeleteFragment(editor)) return;
 
     deleteFragment();
+  };
+
+  editor.insertFragment = (fragment: TDescendant[]) => {
+    const liEntry = findNode(editor, { match: { type: li.type } });
+
+    let filtered: TDescendant[] = [];
+
+    if (liEntry) {
+      const [, liPath] = liEntry;
+
+      fragment.forEach((node) => {
+        if ([ul.type, ol.type].includes(node.type)) {
+          filtered.push(...node.children);
+          return;
+        }
+
+        filtered.push(node);
+      });
+
+      // FIXME: fork insertFragment for edge cases
+      Transforms.insertNodes(editor, filtered, {
+        at: Path.next(liPath),
+        select: true,
+      });
+      return;
+    }
+    filtered = fragment;
+
+    const firstNode = fragment[0];
+
+    if ([ul.type, ol.type].includes(firstNode.type)) {
+      filtered = [{ text: '' }, ...filtered];
+    }
+
+    insertFragment(filtered);
   };
 
   editor.normalizeNode = getListNormalizer(editor, { validLiChildrenTypes });
