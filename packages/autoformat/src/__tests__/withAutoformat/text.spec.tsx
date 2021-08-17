6fac052eaf419b24b94a0e041973ad172e41bd7b
zbeyens <ziyad.beyens@gmail.com>
2021-08-17 14:03:16 +0200

    test

--- packages/autoformat/src/__tests__/withAutoformat/text.spec.tsx
+++ packages/autoformat/src/__tests__/withAutoformat/text.spec.tsx
@@ -127,6 +127,50 @@ describe('when //', () => {
   });
 });
 
+describe('when typing %%%', () => {
+  it('should autoformat', () => {
+    const input = (
+      <editor>
+        <hp>
+          %
+          <cursor />
+          hello
+        </hp>
+      </editor>
+    ) as any;
+
+    const output1 = (
+      <editor>
+        <hp>
+          ‰
+          <cursor />
+          hello
+        </hp>
+      </editor>
+    ) as any;
+
+    const output2 = (
+      <editor>
+        <hp>
+          ‱
+          <cursor />
+          hello
+        </hp>
+      </editor>
+    ) as any;
+
+    const editor = withAutoformat(optionsAutoformat)(withReact(input));
+
+    editor.insertText('%');
+
+    expect(input.children).toEqual(output1.children);
+
+    editor.insertText('%');
+
+    expect(input.children).toEqual(output2.children);
+  });
+});
+
 describe('when using quotes', () => {
   it('should autoformat to smart quotes', () => {
     const input = (
