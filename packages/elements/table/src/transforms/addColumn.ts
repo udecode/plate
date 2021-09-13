5923d0b9598e5c546c17be0df98803af2e3edb21
Tim Ramage <tjramage@users.noreply.github.com>
2021-09-13 13:31:17 +0100

    Update addColumn.ts

--- packages/elements/table/src/transforms/addColumn.ts
+++ packages/elements/table/src/transforms/addColumn.ts
@@ -8,36 +8,37 @@ import { getEmptyCellNode } from '../utils/getEmptyCellNode';
 export const addColumn = (editor: SPEditor, { header }: TablePluginOptions) => {
   if (
     someNode(editor, {
-      match: { type: getPlatePluginType(editor, ELEMENT_TABLE) },
+      match: { type: getPlatePluginType(editor, ELEMENT_TABLE) }
     })
   ) {
     const currentCellItem = getAbove(editor, {
       match: {
         type: [
           getPlatePluginType(editor, ELEMENT_TH),
-          getPlatePluginType(editor, ELEMENT_TD),
-        ],
-      },
-    });
+          getPlatePluginType(editor, ELEMENT_TD)
+        ]
+      }
+    })
 
     const currentTableItem = getAbove(editor, {
-      match: { type: getPlatePluginType(editor, ELEMENT_TABLE) },
-    });
+      match: { type: getPlatePluginType(editor, ELEMENT_TABLE) }
+    })
 
     if (currentCellItem && currentTableItem) {
-      const nextCellPath = Path.next(currentCellItem[1]);
-      const newCellPath = nextCellPath.slice();
-      const replacePathPos = newCellPath.length - 2;
-      const currentRowIdx = nextCellPath[replacePathPos];
+      const nextCellPath = Path.next(currentCellItem[1])
+      const newCellPath = nextCellPath.slice()
+      const replacePathPos = newCellPath.length - 2
+      const currentRowIdx = nextCellPath[replacePathPos]
 
-      currentTableItem[0].children.forEach((row, rowIdx) => {
-        newCellPath[replacePathPos] = rowIdx;
+      currentTableItem[0].children.forEach((row: TElement, rowIdx) => {
+        newCellPath[replacePathPos] = rowIdx
+        const isHeaderRow = header || row.children[0].type === ELEMENT_TH
 
-        insertNodes<TElement>(editor, getEmptyCellNode(editor, { header }), {
+        insertNodes<TElement>(editor, getEmptyCellNode(editor, { header: isHeaderRow }), {
           at: newCellPath,
-          select: rowIdx === currentRowIdx,
-        });
-      });
+          select: rowIdx === currentRowIdx
+        })
+      })
     }
   }
-};
+}
