7fb7ee0c89a1a930c3914e3b4d3c6d4dc1b47b51
zbeyens <ziyad.beyens@gmail.com>
2021-09-22 17:59:06 +0200

    feat: new plugin

---
+++ packages/elements/horizontal-rule-ui/src/HrElement/HrElement.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useFocused, useSelected } from 'slate-react';
+import { getHrElementStyles } from './HrElement.styles';
+import { HrElementProps } from './HrElement.types';
+
+// const plugins = [
+//   createHistoryPlugin(),
+//   createReactPlugin(),
+//   createSingleLinePlugin(),
+// ];
+
+export const HrElement = (props: HrElementProps) => {
+  const { attributes, children, nodeProps } = props;
+
+  const selected = useSelected();
+  const focused = useFocused();
+  const styles = getHrElementStyles({ ...props, selected, focused });
+
+  return (
+    <div
+      {...attributes}
+      css={styles.root?.css}
+      className={styles.root?.className}
+    >
+      <hr
+        contentEditable={false}
+        {...nodeProps}
+        css={styles.hr?.css}
+        className={styles.hr?.className}
+      />
+      {children}
+    </div>
+  );
+};
---
+++ packages/elements/horizontal-rule/src/createHorizontalRulePlugin.ts
@@ -0,0 +1,21 @@
+import {
+  getPlatePluginTypes,
+  getRenderElement,
+  PlatePlugin,
+} from '@udecode/plate-core';
+import { ELEMENT_HR } from './defaults';
+import { getHorizontalRuleDeserialize } from './getHorizontalRuleDeserialize';
+
+export const createHorizontalRulePlugin = (): PlatePlugin => ({
+  pluginKeys: ELEMENT_HR,
+  renderElement: getRenderElement(ELEMENT_HR),
+  voidTypes: getPlatePluginTypes(ELEMENT_HR),
+  deserialize: getHorizontalRuleDeserialize(),
+  // serialize: {
+  // 	element: ({ element }) => {
+  // 		if (element.type === ELEMENT_HR) {
+  // 			return <hr />
+  // 		}
+  // 	}
+  // }
+});
