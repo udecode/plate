7285e2b7e1c01480162d17a656dff4f7e4bd5a15
zbeyens <ziyad.beyens@gmail.com>
2021-09-27 23:15:22 +0200

    refactor

--- packages/core/src/components/EditorStateEffect.tsx
+++ packages/core/src/components/EditorStateEffect.tsx
@@ -1,34 +1,19 @@
 import { memo, useEffect } from 'react';
-import { Editor } from 'slate';
-import { useEditorRef } from '../hooks';
 import { useEditorState } from '../hooks/useEditorState';
 import { usePlateActions } from '../stores/plate/plate.actions';
 import { PlateProps } from './Plate';
 
-export const EditorStateEffect = memo(
-  ({
-    id,
-    normalizeInitialValue,
-  }: Pick<PlateProps, 'id' | 'normalizeInitialValue'>) => {
-    const { setSelection, incrementKeyChange } = usePlateActions(id);
-    const editorState = useEditorState();
-    const editor = useEditorRef();
+export const EditorStateEffect = memo(({ id }: Pick<PlateProps, 'id'>) => {
+  const { setSelection, incrementKeyChange } = usePlateActions(id);
+  const editorState = useEditorState();
 
-    useEffect(() => {
-      incrementKeyChange();
-    });
+  useEffect(() => {
+    incrementKeyChange();
+  });
 
-    useEffect(() => {
-      setSelection(editorState.selection ? { ...editorState.selection } : null);
-    }, [editorState.selection, setSelection]);
+  useEffect(() => {
+    setSelection(editorState.selection ? { ...editorState.selection } : null);
+  }, [editorState.selection, setSelection]);
 
-    useEffect(() => {
-      if (editor && normalizeInitialValue) {
-        Editor.normalize(editor, { force: true });
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [editor]);
-
-    return null;
-  }
-);
+  return null;
+});
