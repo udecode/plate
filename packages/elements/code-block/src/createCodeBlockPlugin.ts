57bec888160b5358fdf5a157faf192371e7d6e20
Dylan Schiemann <dylan@dojotoolkit.org>
2021-10-01 15:59:18 -0700

    more incremental work on code-block configuration

--- packages/elements/code-block/src/createCodeBlockPlugin.ts
+++ packages/elements/code-block/src/createCodeBlockPlugin.ts
@@ -1,19 +1,15 @@
 import { getRenderElement, PlatePlugin } from '@udecode/plate-core';
-import { DEFAULTS_CODE_BLOCK, KEYS_CODE_BLOCK } from './defaults';
+import { KEYS_CODE_BLOCK } from './defaults';
 import { getCodeBlockDeserialize } from './getCodeBlockDeserialize';
 import { getCodeBlockOnKeyDown } from './getCodeBlockOnKeyDown';
 import { getCodeBlockRenderLeaf } from './getCodeBlockRenderLeaf';
 import { getCodeLineDecorate } from './getCodeLineDecorate';
-import { CodeBlockPluginOptions } from './types';
 import { withCodeBlock } from './withCodeBlock';
 
 /**
  * Enables support for pre-formatted code blocks.
  */
-export const createCodeBlockPlugin = ({
-  syntax = DEFAULTS_CODE_BLOCK?.syntax,
-  syntaxPopularFirst = DEFAULTS_CODE_BLOCK?.syntaxPopularFirst,
-}: CodeBlockPluginOptions = {}): PlatePlugin => ({
+export const createCodeBlockPlugin = (): PlatePlugin => ({
   pluginKeys: KEYS_CODE_BLOCK,
   renderElement: getRenderElement(KEYS_CODE_BLOCK),
   renderLeaf: getCodeBlockRenderLeaf(),
