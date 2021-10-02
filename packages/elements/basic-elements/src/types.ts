334f07599a544a7ba1d774e724ba8d04a45f178b
Dylan Schiemann <dylan@dojotoolkit.org>
2021-10-02 05:53:30 -0700

    remove unnecessary change from basic-elements

--- packages/elements/basic-elements/src/types.ts
+++ packages/elements/basic-elements/src/types.ts
@@ -1,8 +1,5 @@
-import { CodeBlockPluginOptions } from '@udecode/plate-code-block';
 import { HeadingPluginOptions } from '@udecode/plate-heading';
 
 export interface BasicElementPluginsOptions {
   heading?: HeadingPluginOptions;
-  syntax?: CodeBlockPluginOptions;
-  syntaxPopularFirst?: CodeBlockPluginOptions;
 }
