a574a7537f7a4a25bb6a527a08ad6698da1dc7b1
Nemanja Tosic <nemanja@livingspec.com>
2021-10-07 18:56:40 +0200

    Fix code block paste when selection is expanded

--- packages/elements/code-block/src/getCodeBlockInsertFragment.ts
+++ packages/elements/code-block/src/getCodeBlockInsertFragment.ts
@@ -1,6 +1,6 @@
 import { findNode } from '@udecode/plate-common';
 import { getPlatePluginType, SPEditor, TDescendant } from '@udecode/plate-core';
-import { Node, Path, Transforms } from 'slate';
+import { Node, Transforms } from 'slate';
 import { ELEMENT_CODE_BLOCK, ELEMENT_CODE_LINE } from './defaults';
 
 export function getCodeBlockInsertFragment(editor: SPEditor) {
@@ -20,33 +20,18 @@ export function getCodeBlockInsertFragment(editor: SPEditor) {
   }
 
   return (fragment: TDescendant[]) => {
-    const codeLineEntry = findNode(editor, {
-      match: { type: codeLineType },
-      mode: 'lowest',
-    });
-
-    if (codeLineEntry) {
-      const [codeLineNode, codeLinePath] = codeLineEntry;
-      const isEmptyCodeLine = Node.string(codeLineNode);
-
-      if (!isEmptyCodeLine) {
-        Transforms.removeNodes(editor, { at: codeLinePath });
-      }
-
-      return Transforms.insertNodes(
-        editor,
-        fragment.flatMap((node) =>
-          node.type === codeBlockType
-            ? extractCodeLinesFromCodeBlock(node)
-            : convertNodeToCodeLine(node)
-        ),
-        {
-          at: isEmptyCodeLine ? Path.next(codeLinePath) : codeLinePath,
-          select: true,
-        }
-      );
+    const inCodeLine = findNode(editor, { match: { type: codeLineType } });
+    if (!inCodeLine) {
+      return insertFragment(fragment);
     }
 
-    insertFragment(fragment);
+    return Transforms.insertFragment(
+      editor,
+      fragment.flatMap((node) =>
+        node.type === codeBlockType
+          ? extractCodeLinesFromCodeBlock(node)
+          : convertNodeToCodeLine(node)
+      )
+    );
   };
 }
