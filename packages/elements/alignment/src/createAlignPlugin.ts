dc3c67bbd79c900c811606a0adf31c272e7529e7
Csaba Benkő <benko.csaba89@gmail.com>
2021-10-27 16:47:55 +0200

    feat: move cssPropName, transformCssValue so it won't be overwritten

--- packages/elements/alignment/src/createAlignPlugin.ts
+++ packages/elements/alignment/src/createAlignPlugin.ts
@@ -15,14 +15,16 @@ export const createAlignPlugin = (
   overrideProps: getAlignOverrideProps(),
   withOverrides: (editor) => {
     // TODO: extend plate-core to register options
-    editor.options[KEY_ALIGN] = defaults(options, {
-      type: KEY_ALIGN,
-      types: [getPlatePluginType(editor, ELEMENT_DEFAULT)],
-      alignments: DEFAULT_ALIGNMENTS,
-      defaultAlignment: DEFAULT_ALIGNMENT,
+    editor.options[KEY_ALIGN] = {
+      ...defaults(options, {
+        type: KEY_ALIGN,
+        types: [getPlatePluginType(editor, ELEMENT_DEFAULT)],
+        alignments: DEFAULT_ALIGNMENTS,
+        defaultAlignment: DEFAULT_ALIGNMENT,
+      }),
       // this will be used by the getOverrideProps so it can set the current value to the proper css prop
       cssPropName: 'textAlign',
-    });
+    };
 
     return editor;
   },
