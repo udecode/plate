87b133cee230c79eaca7e6afb6e237bbc57f98c2
zbeyens <ziyad.beyens@gmail.com>
2021-10-28 12:52:00 +0200

    fix

--- packages/core/src/components/DefaultLeaf.tsx
+++ packages/core/src/components/DefaultLeaf.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { RenderLeafProps } from 'slate-react';
 
-export const DefaultLeaf = (props: RenderLeafProps) => {
-  // text and leaf is included so it won't be rendered bz the span from the additionalProps
-  const { attributes, children, text, leaf, ...additionalProps } = props;
-  return (
-    <span {...attributes} {...additionalProps}>
-      {children}
-    </span>
-  );
-};
+export const DefaultLeaf = ({
+  attributes,
+  children,
+  text,
+  leaf,
+  ...props
+}: RenderLeafProps) => (
+  <span {...attributes} {...props}>
+    {children}
+  </span>
+);
