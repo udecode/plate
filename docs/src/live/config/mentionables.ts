f87553f936ec40939ab2fd3938e4e3672d4344ba
Nemanja Tosic <nemanja@livingspec.com>
2021-10-29 19:53:17 +0200

    Fix tests

--- docs/src/live/config/mentionables.ts
+++ docs/src/live/config/mentionables.ts
@@ -1,6 +1,6 @@
-import { ComboboxItemData } from '@udecode/plate';
+import { TComboboxItem } from '@udecode/plate';
 
-export const MENTIONABLES: ComboboxItemData[] = [
+export const MENTIONABLES: TComboboxItem<{ email: string }>[] = [
   { key: '0', text: 'Aayla Secura', data: { email: 'aayla_secura@force.com' } },
   { key: '1', text: 'Adi Gallia', data: { email: 'adi_gallia@force.com' } },
   {
