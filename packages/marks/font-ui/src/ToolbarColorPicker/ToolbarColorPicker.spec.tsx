b0ef850c9687b95d3fe12ae0937dd2d214ad17c2
Alex Vladut <alexandruvladut@gmail.com>
2021-10-30 18:15:13 +0300

    test: rerender component to reflect selection changes

--- packages/marks/font-ui/src/ToolbarColorPicker/ToolbarColorPicker.spec.tsx
+++ packages/marks/font-ui/src/ToolbarColorPicker/ToolbarColorPicker.spec.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { act, render, screen, waitFor } from '@testing-library/react';
+import React, { ReactElement, ReactNode } from 'react';
+import { render, RenderOptions, screen } from '@testing-library/react';
 import userEvents from '@testing-library/user-event';
-import { Plate, SPEditor } from '@udecode/plate-core';
+import { Plate, SPEditor, TDescendant } from '@udecode/plate-core';
 import {
   createFontBackgroundColorPlugin,
   createFontColorPlugin,
@@ -13,52 +13,72 @@ import { HeadingToolbar } from '@udecode/plate-toolbar';
 import { createEditor, Editor, Transforms } from 'slate';
 import { ToolbarColorPicker } from './ToolbarColorPicker';
 
-const plugins = [
+const DEFAULT_PLUGINS = [
   createFontColorPlugin(),
   createFontBackgroundColorPlugin(),
   createFontSizePlugin(),
 ];
+
+const DEFAULT_INITIAL_VALUE: TDescendant[] = [
+  {
+    type: 'p',
+    children: [
+      {
+        text: 'This text has white color, black background.',
+      },
+    ],
+  },
+];
+
+function renderWithPlate(
+  ui: ReactElement,
+  {
+    editor,
+    initialValue = DEFAULT_INITIAL_VALUE,
+    ...options
+  }: {
+    editor: SPEditor;
+    initialValue?: TDescendant[];
+  } & RenderOptions
+) {
+  const Wrapper = ({ children }: { children?: ReactNode }) => (
+    <Plate
+      editor={editor as SPEditor}
+      plugins={DEFAULT_PLUGINS}
+      initialValue={initialValue}
+    >
+      <HeadingToolbar>{children}</HeadingToolbar>
+    </Plate>
+  );
+  return render(ui, { wrapper: Wrapper, ...options });
+}
 
 describe('ToolbarColorPicker', () => {
   it('should apply font text color', async () => {
     const editor = createEditor();
 
     const Component = () => (
-      <Plate
-        editor={editor as SPEditor}
-        plugins={plugins}
-        initialValue={[
-          {
-            type: 'p',
-            children: [
-              {
-                text: 'This text has white color, black background.',
-              },
-            ],
-          },
-        ]}
-      >
-        <HeadingToolbar>
-          <ToolbarColorPicker
-            pluginKey={MARK_COLOR}
-            icon={<div>Color</div>}
-            selectedIcon={<div>Selected</div>}
-            tooltip={{ content: 'Text color' }}
-          />
-        </HeadingToolbar>
-      </Plate>
+      <ToolbarColorPicker
+        pluginKey={MARK_COLOR}
+        icon={<div>Color</div>}
+        selectedIcon={<div>Selected</div>}
+        tooltip={{ content: 'Text color' }}
+      />
     );
 
-    render(<Component />);
+    const { rerender } = renderWithPlate(<Component />, {
+      editor: editor as SPEditor,
+    });
 
     const toolbarButton = screen.getByTestId('ToolbarButton');
 
-    act(() => {
-      Transforms.select(editor, {
-        anchor: Editor.start(editor, []),
-        focus: Editor.end(editor, []),
-      });
+    Transforms.select(editor, {
+      anchor: Editor.start(editor, []),
+      focus: Editor.end(editor, []),
     });
+
+    // force rerender to reflect the editor's selection changes
+    rerender(<Component />);
 
     userEvents.click(toolbarButton);
 
@@ -87,41 +107,26 @@ describe('ToolbarColorPicker', () => {
     const editor = createEditor();
 
     const Component = () => (
-      <Plate
-        editor={editor as SPEditor}
-        plugins={plugins}
-        initialValue={[
-          {
-            type: 'p',
-            children: [
-              {
-                text: 'This text has white color, black background.',
-              },
-            ],
-          },
-        ]}
-      >
-        <HeadingToolbar>
-          <ToolbarColorPicker
-            pluginKey={MARK_BG_COLOR}
-            icon={<div>Background Color</div>}
-            selectedIcon={<div>Selected</div>}
-            tooltip={{ content: 'Background color' }}
-          />
-        </HeadingToolbar>
-      </Plate>
+      <ToolbarColorPicker
+        pluginKey={MARK_BG_COLOR}
+        icon={<div>Background Color</div>}
+        selectedIcon={<div>Selected</div>}
+        tooltip={{ content: 'Background color' }}
+      />
     );
 
-    render(<Component />);
+    const { rerender } = renderWithPlate(<Component />, {
+      editor: editor as SPEditor,
+    });
 
     const toolbarButton = screen.getByTestId('ToolbarButton');
 
-    act(() => {
-      Transforms.select(editor, {
-        anchor: Editor.start(editor, []),
-        focus: Editor.end(editor, []),
-      });
+    Transforms.select(editor, {
+      anchor: Editor.start(editor, []),
+      focus: Editor.end(editor, []),
     });
+    // force rerender to reflect the editor's selection changes
+    rerender(<Component />);
 
     userEvents.click(toolbarButton);
 
@@ -146,45 +151,39 @@ describe('ToolbarColorPicker', () => {
     ]);
   });
 
-  it.skip('should clear selected font text color', async () => {
+  it('should clear selected font text color', async () => {
     const editor = createEditor();
 
     const Component = () => (
-      <Plate
-        id="font"
-        editor={editor as SPEditor}
-        plugins={plugins}
-        initialValue={[
-          {
-            type: 'p',
-            children: [
-              {
-                text: 'This text has white color, black background.',
-                color: '#D0DFE3',
-              },
-            ],
-          },
-        ]}
-      >
-        <HeadingToolbar>
-          <ToolbarColorPicker
-            pluginKey={MARK_COLOR}
-            icon={<div>Color</div>}
-            selectedIcon={<div>Selected</div>}
-            tooltip={{ content: 'Text color' }}
-          />
-        </HeadingToolbar>
-      </Plate>
+      <ToolbarColorPicker
+        pluginKey={MARK_COLOR}
+        icon={<div>Color</div>}
+        selectedIcon={<div>Selected</div>}
+        tooltip={{ content: 'Text color' }}
+      />
     );
 
-    render(<Component />);
-
-    act(() => {
-      Transforms.select(editor, {
-        anchor: Editor.start(editor, []),
-        focus: Editor.end(editor, []),
-      });
+    const { rerender } = renderWithPlate(<Component />, {
+      editor: editor as SPEditor,
+      initialValue: [
+        {
+          type: 'p',
+          children: [
+            {
+              text: 'This text has white color, black background.',
+              color: '#D0DFE3',
+            },
+          ],
+        },
+      ],
     });
+
+    Transforms.select(editor, {
+      anchor: Editor.start(editor, []),
+      focus: Editor.end(editor, []),
+    });
+    // force rerender to reflect the editor's selection changes
+    rerender(<Component />);
 
     const toolbarButton = screen.getByTestId('ToolbarButton');
     userEvents.click(toolbarButton);
