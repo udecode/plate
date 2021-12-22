d7d5ba3f7c002da07d5e4bb86acbea44075880ac
sctang2020 <62458858+sctang2020@users.noreply.github.com>
2021-12-22 16:04:27 -0600

    set the cursor to the first cell for new table (#1322)
    * change order of plugins to fix test case failure
    
    * 1216: put cursor at first cell for newly created table
    
    * add changeset
    
    Co-authored-by: O'Neal Tang <oneal@livingskytech.com>

--- packages/nodes/table/src/transforms/insertTable.ts
+++ packages/nodes/table/src/transforms/insertTable.ts
@@ -1,10 +1,13 @@
 import {
+  getAbove,
   getPluginType,
   insertNodes,
   PlateEditor,
+  selectEditor,
   someNode,
   TElement,
 } from '@udecode/plate-core';
+import { Editor } from 'slate';
 import { ELEMENT_TABLE } from '../createTablePlugin';
 import { TablePluginOptions } from '../types';
 import { getEmptyTableNode } from '../utils/getEmptyTableNode';
@@ -19,5 +22,15 @@ export const insertTable = (
     })
   ) {
     insertNodes<TElement>(editor, getEmptyTableNode(editor, { header }));
+    if (editor.selection) {
+      const tableEntry = getAbove(editor, {
+        match: { type: getPluginType(editor, ELEMENT_TABLE) },
+      });
+      if (!tableEntry) {
+        return;
+      }
+      const point = Editor.start(editor, tableEntry[1]);
+      selectEditor(editor, { at: point });
+    }
   }
 };
