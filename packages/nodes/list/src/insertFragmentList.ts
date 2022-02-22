b1364868fa7f65f179308e1813bafbca404ef7e5
Darren Waddell <darren.waddell@gmail.com>
2022-02-22 14:15:55 +0000

    List insert text (#1386)
    * fix: attempt to fix lists item insert text
    
    * fix: apply changes based on the new code structure
    
    * fix: remove unnecessary findNode calls
    
    * refactor: insert list into empty node by using transformers instead of directly manipulating the children
    
    * Refactor insertFragmentList
    
    * test: add test for pasting list into selection across multiple lics
    
    * refactor: extract function for formatting fragment nodes to be inserted
    
    * fix: refetch li node after removing selection to retrieve the current path
    
    * test: update tests to reflect expected behaviour
    
    * fix: remove reference to createDeserializeAstPlugin - now in core
    
    * test: remove duplicate tests for list items
    
    * feat: normalize non-li list children
    
    * refactor: test list normalizations with Editor.normalize
    
    editor.normalizeNode would do one pass through the normalizers, which is
    not what the editor does.
    
    * style: remove duplicate export
    
    * chore: add changeset
    
    Co-authored-by: Alex Vladut <alexandruvladut@gmail.com>
    Co-authored-by: Nemanja Tosic <nemanja@livingspec.com>

--- packages/nodes/list/src/insertFragmentList.ts
+++ packages/nodes/list/src/insertFragmentList.ts
@@ -4,19 +4,18 @@ import {
   PlateEditor,
   PlatePlugin,
   TDescendant,
+  TElement,
 } from '@udecode/plate-core';
-import { Node, NodeEntry, Path, Transforms } from 'slate';
-import { ELEMENT_LI, ELEMENT_OL, ELEMENT_UL } from './createListPlugin';
+import { Element, Node, NodeEntry, Path, Transforms } from 'slate';
+import { ELEMENT_LI } from './createListPlugin';
+import { getListItemContentType, getListItemType, isListRoot } from './queries';
 
 export const insertFragmentList = (editor: PlateEditor) => {
   const { insertFragment } = editor;
 
-  const li = getPlugin(editor, ELEMENT_LI);
-  const ul = getPlugin(editor, ELEMENT_UL);
-  const ol = getPlugin(editor, ELEMENT_OL);
-
-  const isListRoot = (node: TDescendant): boolean =>
-    [ul.type, ol.type].includes(node.type);
+  const listItemPlugin = getPlugin(editor, ELEMENT_LI);
+  const listItemType = getListItemType(editor);
+  const listItemContentType = getListItemContentType(editor);
 
   const getFirstAncestorOfType = (
     root: TDescendant,
@@ -31,13 +30,28 @@ export const insertFragmentList = (editor: PlateEditor) => {
     return [Node.get(root, ancestor), ancestor];
   };
 
+  const findListItemsWithContent = <T extends TDescendant>(first: T): T[] => {
+    let prev = null;
+    let node = first;
+    while (
+      isListRoot(editor, node) ||
+      (node.type === listItemType &&
+        node.children[0].type !== listItemContentType)
+    ) {
+      prev = node;
+      [node] = node.children;
+    }
+
+    return prev ? prev.children : [node];
+  };
+
   /**
    * Removes the "empty" leading lis. Empty in this context means lis only with other lis as children.
    *
    * @returns If argument is not a list root, returns it, otherwise returns ul[] or li[].
    */
   const trimList = <T extends TDescendant>(listRoot: T): T[] => {
-    if (!isListRoot(listRoot)) {
+    if (!isListRoot(editor, listRoot)) {
       return [listRoot];
     }
 
@@ -49,47 +63,144 @@ export const insertFragmentList = (editor: PlateEditor) => {
           ? commonAncestor
           : Node.common(listRoot, textEntry[1], commonAncestor[1]),
       // any list item would do, we grab the first one
-      getFirstAncestorOfType(listRoot, textEntries[0], li)
+      getFirstAncestorOfType(listRoot, textEntries[0], listItemPlugin)
     );
 
-    return isListRoot(commonAncestorEntry[0])
+    const [first, ...rest] = isListRoot(editor, commonAncestorEntry[0])
       ? commonAncestorEntry[0].children
       : [commonAncestorEntry[0]];
+    return [...findListItemsWithContent(first), ...rest];
   };
 
-  const convertFragmentToList = (fragment: TDescendant[]) => {
-    const trimmedFragment = fragment.flatMap((node) => trimList(node));
+  const wrapNodeIntoListItem = (node: TDescendant): TDescendant => {
+    return node.type === listItemType
+      ? node
+      : {
+          type: listItemType,
+          children: [node],
+        };
+  };
 
-    if (trimmedFragment.every((node) => node.type === ELEMENT_LI)) {
-      return trimmedFragment;
+  /**
+   * Checks if the fragment only consists of a single LIC in which case it is considered the user's intention was to copy a text, not a list
+   */
+  const isSingleLic = (fragment: TDescendant[]) => {
+    const isFragmentOnlyListRoot =
+      fragment.length === 1 && isListRoot(editor, fragment[0]);
+
+    return (
+      isFragmentOnlyListRoot &&
+      [...Node.nodes({ children: fragment })]
+        .filter((entry): entry is NodeEntry<TElement> =>
+          Element.isElement(entry[0])
+        )
+        .filter(([node]) => node.type === listItemContentType).length === 1
+    );
+  };
+
+  const getTextAndListItemNodes = (
+    fragment: TDescendant[],
+    liEntry: NodeEntry,
+    licEntry: NodeEntry
+  ) => {
+    const [, liPath] = liEntry;
+    const [licNode, licPath] = licEntry;
+    const isEmptyNode = !Node.string(licNode);
+    const [first, ...rest] = fragment
+      .flatMap(trimList)
+      .map(wrapNodeIntoListItem);
+    let textNode;
+    let listItemNodes;
+    if (isListRoot(editor, fragment[0])) {
+      if (isSingleLic(fragment)) {
+        textNode = first;
+        listItemNodes = rest;
+      } else if (isEmptyNode) {
+        // FIXME: is there a more direct way to set this?
+        const li = Node.get(editor, liPath) as Element;
+        const [, ...currentSublists] = li.children;
+        const [newLic, ...newSublists] = first.children;
+        Transforms.insertNodes(editor, newLic, {
+          at: Path.next(licPath),
+          select: true,
+        });
+        Transforms.removeNodes(editor, {
+          at: licPath,
+        });
+        if (newSublists?.length) {
+          if (currentSublists?.length) {
+            // TODO: any better way to compile the path where the LIs of the newly inserted element will be inserted?
+            const path = [...liPath, 1, 0];
+            Transforms.insertNodes(editor, newSublists[0].children, {
+              at: path,
+              select: true,
+            });
+          } else {
+            Transforms.insertNodes(editor, newSublists, {
+              at: Path.next(licPath),
+              select: true,
+            });
+          }
+        }
+
+        textNode = { text: '' };
+        listItemNodes = rest;
+      } else {
+        textNode = { text: '' };
+        listItemNodes = [first, ...rest];
+      }
+    } else {
+      textNode = first;
+      listItemNodes = rest;
     }
 
-    return trimmedFragment.map((node) => ({
-      type: ELEMENT_LI,
-      children: node.children ?? [node],
-    }));
+    return { textNode, listItemNodes };
   };
 
   return (fragment: TDescendant[]) => {
-    const liEntry = findNode(editor, {
-      match: { type: li.type },
+    let liEntry = findNode(editor, {
+      match: { type: listItemType },
+      mode: 'lowest',
+    });
+    // not inserting into a list item, delegate to other plugins
+    if (!liEntry) {
+      return insertFragment(
+        isListRoot(editor, fragment[0]) ? [{ text: '' }, ...fragment] : fragment
+      );
+    }
+
+    // delete selection (if necessary) so that it can check if needs to insert into an empty block
+    Transforms.insertFragment(editor, [{ text: '' }]);
+
+    // refetch to find the currently selected LI after the deletion above is performed
+    liEntry = findNode(editor, {
+      match: { type: listItemType },
       mode: 'lowest',
     });
 
-    if (liEntry) {
-      const [, liPath] = liEntry;
-
-      // FIXME: fork insertFragment for edge cases
-      return Transforms.insertNodes(editor, convertFragmentToList(fragment), {
-        at: Path.next(liPath),
-        select: true,
-      });
+    const licEntry = findNode(editor, {
+      match: { type: listItemContentType },
+      mode: 'lowest',
+    });
+    if (!licEntry) {
+      return insertFragment(
+        isListRoot(editor, fragment[0]) ? [{ text: '' }, ...fragment] : fragment
+      );
     }
 
-    const filtered: TDescendant[] = isListRoot(fragment[0])
-      ? [{ text: '' }, ...fragment]
-      : fragment;
+    const { textNode, listItemNodes } = getTextAndListItemNodes(
+      fragment,
+      liEntry!,
+      licEntry
+    );
 
-    return insertFragment(filtered);
+    Transforms.insertFragment(editor, [textNode]); // insert text if needed
+
+    const [, liPath] = liEntry!;
+
+    return Transforms.insertNodes(editor, listItemNodes, {
+      at: Path.next(liPath),
+      select: true,
+    });
   };
 };
--- packages/nodes/list/src/queries/getListTypes.ts
+++ packages/nodes/list/src/queries/getListTypes.ts
@@ -1,6 +1,27 @@
 import { getPluginType, PlateEditor } from '@udecode/plate-core';
-import { ELEMENT_OL, ELEMENT_UL } from '../createListPlugin';
+import {
+  ELEMENT_LI,
+  ELEMENT_LIC,
+  ELEMENT_OL,
+  ELEMENT_UL,
+} from '../createListPlugin';
+
+export const getUnorderedListType = (editor: PlateEditor) => {
+  return getPluginType(editor, ELEMENT_UL);
+};
+
+export const getOrderedListType = (editor: PlateEditor) => {
+  return getPluginType(editor, ELEMENT_OL);
+};
 
 export const getListTypes = (editor: PlateEditor) => {
-  return [getPluginType(editor, ELEMENT_OL), getPluginType(editor, ELEMENT_UL)];
+  return [getOrderedListType(editor), getUnorderedListType(editor)];
+};
+
+export const getListItemType = (editor: PlateEditor) => {
+  return getPluginType(editor, ELEMENT_LI);
+};
+
+export const getListItemContentType = (editor: PlateEditor) => {
+  return getPluginType(editor, ELEMENT_LIC);
 };
