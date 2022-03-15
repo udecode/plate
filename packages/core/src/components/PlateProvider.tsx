775ecbd590ba7d416889441f3cbc5cda4c5f5b11
Ziad Beyens <ziyad.beyens@gmail.com>
2022-03-15 18:11:54 +0100

    Fix: PlateProvider (#1440)
    * fix
    
    * Create seven-students-change.md

--- packages/core/src/components/PlateProvider.tsx
+++ packages/core/src/components/PlateProvider.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
+import { Provider } from 'jotai';
 import { withHOC } from '../common/hoc/withHOC';
 import { usePlatesStoreEffect } from '../hooks/usePlatesStoreEffect';
 import { usePlatesSelectors } from '../stores/plate/platesStore';
+import { plateIdAtom } from '../stores/plateIdAtom';
 
 export const PlateProvider = ({
   id = 'main',
@@ -16,7 +18,11 @@ export const PlateProvider = ({
 
   if (!hasId) return null;
 
-  return <React.Fragment key={id}>{children}</React.Fragment>;
+  return (
+    <Provider key={id} initialValues={[[plateIdAtom, id]]}>
+      {children}
+    </Provider>
+  );
 };
 
 export const withPlateProvider = <T,>(Component: FC<T>, hocProps?: T) =>
