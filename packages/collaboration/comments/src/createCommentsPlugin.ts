cc1743b7cd388c366124d0c9104e5d27f56e5e7c
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-04-08 10:35:34 +0200

    Doing something with the comment node when the carret is put on it

--- packages/collaboration/comments/src/createCommentsPlugin.ts
+++ packages/collaboration/comments/src/createCommentsPlugin.ts
@@ -1,4 +1,8 @@
-import { createPluginFactory } from '@udecode/plate-core';
+import {
+  createPluginFactory,
+  getAbove,
+  getPluginType,
+} from '@udecode/plate-core';
 import { CommentsPlugin } from './types.js';
 
 export const ELEMENT_COMMENT = 'comment';
@@ -7,4 +11,20 @@ export const createCommentsPlugin = createPluginFactory<CommentsPlugin>({
   key: ELEMENT_COMMENT,
   isElement: true,
   isInline: true,
+  handlers: {
+    onChange(editor) {
+      const type = getPluginType(editor, ELEMENT_COMMENT);
+      return () => {
+        const commentNode = getAbove(editor, {
+          match: { type },
+        });
+        if (commentNode) {
+          const comment = commentNode[0].comment as string;
+          if (comment) {
+            // window.alert(comment);
+          }
+        }
+      };
+    },
+  },
 });
