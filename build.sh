580239c604e853db84d87f944a08eebe262c9597
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-06-04 02:52:10 +0200

    Performance optimization

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 lerna exec --scope @xolvio/plate-comments -- yarn build && \
-rm -rf packages/ui/collaboration/comments/node_modules && \
-yarn && \
 lerna exec --scope @xolvio/plate-ui-comments -- yarn build
