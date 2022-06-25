259d72b7bf3e82cd0b0b28be37ba3c87dfc0fc3a
zbeyens <ziad.beyens@gmail.com>
2022-06-26 00:23:48 +0200

    refactor

--- examples/apps/cra/src/index.tsx
+++ examples/apps/cra/src/index.tsx
@@ -1,4 +1,8 @@
-import { Playground } from '@udecode/examples/playground/Playground';
+import React from "react";
+import ReactDOM from "react-dom";
+import "tippy.js/dist/tippy.css";
+import { Playground } from "../../../src/playground/Playground";
+import "../../../src/styles/styles.css";
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Playground />, rootElement);
