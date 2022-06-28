29c25dd1e9b00bce656bbbf47fd403eedc97d0b7
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-06-28 21:11:20 +0200

    Build script that just builds

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-rm -rf node_modules && \
-rm -rf packages/collaboration/comments/node_modules && \
-rm -rf packages/ui/collaboration/comments/node_modules && \
-yarn && \
 lerna exec --scope @xolvio/plate-comments -- yarn build && \
 lerna exec --scope @xolvio/plate-ui-comments -- yarn build
