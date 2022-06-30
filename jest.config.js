f1db181118fe646d44fc89c00dd9e5ca184d4bc9
zbeyens <ziad.beyens@gmail.com>
2022-06-30 20:18:41 +0200

    chore

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,28 @@
+/* eslint-disable import/extensions */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const appRoot = require('app-root-path');
+
+const { getJestCachePath } = require(`${appRoot}/config/cache.config`);
+
+const packageJson = require(`${process.cwd()}/package.json`);
+const {
+  compilerOptions: baseTsConfig,
+} = require(`${process.cwd()}/tsconfig.json`);
+
+// Take the paths from tsconfig automatically from base tsconfig.json
+// @link https://kulshekhar.github.io/ts-jest/docs/paths-mapping
+const getTsConfigBasePaths = () => {
+  return baseTsConfig.paths
+    ? pathsToModuleNameMapper(baseTsConfig.paths, {
+        prefix: '<rootDir>/',
+      })
+    : {};
+};
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  displayName: `${packageJson.name}:unit`,
+  cacheDirectory: getJestCachePath(packageJson.name),
   // TODO
   collectCoverageFrom: [
     'packages/**/src/**/*.{ts,tsx}',
@@ -12,6 +36,7 @@ module.exports = {
   ],
   globals: {
     'ts-jest': {
+      diagnostics: true,
       tsconfig: '<rootDir>/config/tsconfig.test.json',
     },
   },
@@ -19,6 +44,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/scripts/styleMock.js',
+    ...getTsConfigBasePaths(),
     // '^@udecode/plate-ui-dnd$': '<rootDir>/packages/dnd/src',
     '^@udecode/plate-core$': '<rootDir>/packages/core/src',
     // '^@udecode/plate-basic-elements$':
