ea525bd25b630f66459925126adbe40d4b5480ff
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-07-08 15:41:53 +0200

    fix: prevent default when pressing "Ctrl + Enter" or "Cmd + Enter"

--- packages/ui/collaboration/comments/src/Thread/TextArea.tsx
+++ packages/ui/collaboration/comments/src/Thread/TextArea.tsx
@@ -265,6 +265,7 @@ export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
           );
         } else if ((event.ctrlKey || event.metaKey) && event.code === 'Enter') {
           onSubmit();
+          event.preventDefault();
         } else if (event.code === 'Enter') {
           const selectedContact = filteredContacts[selectedContactIndex];
           onContactSelected(selectedContact);
