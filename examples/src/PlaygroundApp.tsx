42cd0f512795ea5d5d95d49c803b7f06914a1e0c
zbeyens <ziad.beyens@gmail.com>
2022-07-13 20:26:03 +0200

    fix

--- examples/src/PlaygroundApp.tsx
+++ examples/src/PlaygroundApp.tsx
@@ -1,11 +1,8 @@
-import React, { CSSProperties, useEffect, useMemo, useRef } from 'react';
+import React, { CSSProperties, useMemo, useRef } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import {
   AutoformatPlugin,
-  Combobox,
-  comboboxActions,
-  ComboboxItemProps,
   createAlignPlugin,
   createAutoformatPlugin,
   createBlockquotePlugin,
@@ -47,11 +44,9 @@ import {
   createTrailingBlockPlugin,
   createUnderlinePlugin,
   ELEMENT_CODE_BLOCK,
-  isDefined,
   MentionCombobox,
   Plate,
   StyledElement,
-  useComboboxSelectors,
 } from '@udecode/plate';
 import { createJuicePlugin } from '@udecode/plate-juice';
 import {
@@ -96,79 +91,6 @@ components = withStyledPlaceHolders(components);
 const styles: Record<string, CSSProperties> = {
   container: { position: 'relative' },
 };
-
-export const COMBOBOX_TRIGGER_SLASH_COMMAND = '/';
-export const COMBOBOX_KEY_SLASH_COMMAND = 'slash_command';
-
-const COMMAND_BLOCK_ADD_PARAGRAPH = {
-  id: 'block.add.paragraph',
-  label: 'Block: Add Paragraph',
-  inlineLabel: 'Paragraph',
-  description: 'Plain text',
-  keybinding: '⌘⏎',
-};
-
-const SlashCommandComboboxEffect = () => {
-  const search = useComboboxSelectors.text();
-
-  useEffect(() => {
-    const commands = [COMMAND_BLOCK_ADD_PARAGRAPH, COMMAND_BLOCK_ADD_PARAGRAPH];
-
-    const items = commands
-      .map((item) => ({
-        key: item.id,
-        text: item.inlineLabel,
-        data: {
-          description: item.description,
-          keybinding: item.keybinding,
-        },
-      }))
-      .filter(
-        (c) =>
-          !isDefined(search) ||
-          c.text?.toLowerCase().includes(search.toLowerCase())
-      );
-
-    comboboxActions.items(items);
-  }, [search]);
-
-  return null;
-};
-
-const SlashCommandComboboxItem = ({ item }: ComboboxItemProps<{}>) => {
-  const data = item.data as any;
-
-  return (
-    <div className="inline-flex w-full p-2">
-      <div className="flex w-full justify-between">
-        <div className="flex-column">
-          <div className="text-sm">{item.text}</div>
-          {isDefined(data.description) && (
-            <div className="mt-0.5 text-xs text-neutralSecondary">
-              {data.description}
-            </div>
-          )}
-        </div>
-        {isDefined(data.keybinding) && (
-          <div>
-            {/* <Keybind className="p-1 text-xs">{data.keybinding}</Keybind> */}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
-
-const SlashCommandCombobox = () => (
-  <Combobox
-    id={COMBOBOX_KEY_SLASH_COMMAND}
-    component={SlashCommandComboboxEffect}
-    trigger={COMBOBOX_TRIGGER_SLASH_COMMAND}
-    onRenderItem={SlashCommandComboboxItem}
-    // onSelectItem={useSlashCommandOnSelectItem()}
-    onSelectItem={() => {}}
-  />
-);
 
 const App = () => {
   const containerRef = useRef(null);
