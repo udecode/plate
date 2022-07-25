6a0fd22123e3e9f4e6597442f37360f746028a99
Dylan Schiemann <dylan@dojotoolkit.org>
2022-07-25 09:00:36 -0700

    improve performance of list normalizations

--- packages/core/src/slate/node/getNode.ts
+++ packages/core/src/slate/node/getNode.ts
@@ -1,18 +1,27 @@
-import { Node, Path } from 'slate';
+import { Path, Text } from 'slate';
 import { NodeOf, TNode } from './TNode';
 
 /**
  * Get the descendant node referred to by a specific path.
  * If the path is an empty array, it refers to the root node itself.
  * If the node is not found, return null.
+ * Based on Slate get and has, performance optimization without overhead of
+ * stringify on throwing
  */
 export const getNode = <N extends NodeOf<R>, R extends TNode = TNode>(
   root: R,
   path: Path
 ) => {
-  try {
-    return Node.get(root, path) as N;
-  } catch (err) {
-    return null;
+  for (let i = 0; i < path.length; i++) {
+    const p = path[i]
+
+    if (Text.isText(root) || !root.children[p]) {
+      return null;
+    }
+
+    root = root.children[p] as R;
   }
+
+  return root;
 };
+
