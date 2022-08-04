cd997dc23d4e09b5c1d7f6adbebf94507d6d0bd7
Mourad <mouradmourafiq@users.noreply.github.com>
2022-08-04 13:23:54 +0200

    Allow passing options to isSelectionAtBlockEnd
    Makes isSelectionAtBlock[Start/End]'s behavior consistent

--- packages/core/src/queries/isSelectionAtBlockEnd.ts
+++ packages/core/src/queries/isSelectionAtBlockEnd.ts
@@ -1,3 +1,4 @@
+import { GetAboveNodeOptions } from '../slate/editor/getAboveNode';
 import { isEndPoint } from '../slate/editor/isEndPoint';
 import { TEditor, Value } from '../slate/editor/TEditor';
 import { getBlockAbove } from './getBlockAbove';
@@ -6,9 +7,10 @@ import { getBlockAbove } from './getBlockAbove';
  * Is the selection focus at the end of its parent block.
  */
 export const isSelectionAtBlockEnd = <V extends Value>(
-  editor: TEditor<V>
+  editor: TEditor<V>,
+  options?: GetAboveNodeOptions<V>
 ): boolean => {
-  const path = getBlockAbove(editor)?.[1];
+  const path = getBlockAbove(editor, options)?.[1];
 
   return !!path && isEndPoint(editor, editor.selection?.focus, path);
 };
