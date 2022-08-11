fc5d7a7191819d9f81cde78da8481d2753df0017
zbeyens <ziad.beyens@gmail.com>
2022-08-11 22:58:09 +0200

    feat

--- packages/nodes/table/src/withTable.ts
+++ packages/nodes/table/src/withTable.ts
@@ -3,6 +3,8 @@ import { TablePlugin } from './types';
 import { withDeleteTable } from './withDeleteTable';
 import { withGetFragmentTable } from './withGetFragmentTable';
 import { withInsertFragmentTable } from './withInsertFragmentTable';
+import { withInsertTextTable } from './withInsertTextTable';
+import { withNormalizeTable } from './withNormalizeTable';
 
 export const withTable = <
   V extends Value = Value,
@@ -11,9 +13,11 @@ export const withTable = <
   editor: E,
   plugin: WithPlatePlugin<TablePlugin<V>, V, E>
 ) => {
+  editor = withNormalizeTable<V, E>(editor);
   editor = withDeleteTable<V, E>(editor);
   editor = withGetFragmentTable<V, E>(editor);
   editor = withInsertFragmentTable<V, E>(editor, plugin);
+  editor = withInsertTextTable<V, E>(editor, plugin);
 
   return editor;
 };
