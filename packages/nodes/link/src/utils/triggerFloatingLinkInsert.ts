d63d65f8c6de8758e837297dc9650662d508960e
zbeyens <ziad.beyens@gmail.com>
2022-08-12 18:23:34 +0200

    fix

--- packages/nodes/link/src/utils/triggerFloatingLinkInsert.ts
+++ packages/nodes/link/src/utils/triggerFloatingLinkInsert.ts
@@ -35,18 +35,6 @@ export const triggerFloatingLinkInsert = <V extends Value>(
 
   if (isRangeAcrossBlocks(editor, { at: editor.selection })) return;
 
-  // get lowest nodes in selection
-  // const _entries = getNodeEntries(editor, {
-  //   mode: 'lowest',
-  // });
-  // const entries = [..._entries];
-  // if (entries.length !== 1) return;
-
-  // const [entry] = entries;
-
-  // void node
-  // if (!isText(entry[0])) return;
-
   const hasLink = someNode(editor, {
     match: { type: getPluginType(editor, ELEMENT_LINK) },
   });
