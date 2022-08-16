34820f39c4298d2c2626fafed513c61be4b42cae
zbeyens <ziad.beyens@gmail.com>
2022-08-16 16:04:09 +0200

    fix

--- packages/core/src/hooks/plate/usePlatesStoreEffect.ts
+++ packages/core/src/hooks/plate/usePlatesStoreEffect.ts
@@ -6,15 +6,19 @@ import { platesActions, platesSelectors } from '../../stores/plate/platesStore';
 /**
  * On mount: create plate store and set it to the plates store.
  * If id is not defined, event id is used.
+ * To avoid conflict between `unset` on unmount + `set` on mount (immer),
+ * we run `set` in the next tick
  */
 export const usePlatesStoreEffect = <V extends Value>(
   id?: string,
   props?: PlateProps<V>
 ) => {
   useEffect(() => {
-    if (!platesSelectors.has(id)) {
-      platesActions.set(id!, props);
-    }
+    setTimeout(() => {
+      if (!platesSelectors.has(id)) {
+        platesActions.set(id!, props);
+      }
+    }, 0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 };
