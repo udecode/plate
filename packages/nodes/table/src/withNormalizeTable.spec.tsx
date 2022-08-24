8c856b7a96c4d751a7df5579d7ac10ee5af00e79
Ziad Beyens <ziyad.beyens@gmail.com>
2022-08-24 22:47:22 +0200

    fix: unwrap nested tables (#1819)
    * fix
    
    * Create great-schools-train.md

--- packages/nodes/table/src/withNormalizeTable.spec.tsx
+++ packages/nodes/table/src/withNormalizeTable.spec.tsx
@@ -55,4 +55,55 @@ describe('withNormalizeTable', () => {
       expect(editor.children).toEqual(output.children);
     });
   });
+
+  // https://github.com/udecode/editor-protocol/issues/76
+  describe('table in a table', () => {
+    it('should unwrap the nested table, tr, td', async () => {
+      const input = ((
+        <editor>
+          <htable>
+            <htr>
+              <htd>
+                <hp>a</hp>
+                <htable>
+                  <htr>
+                    <htd>
+                      <hp>b</hp>
+                    </htd>
+                    <htd>
+                      <hp>c</hp>
+                    </htd>
+                  </htr>
+                </htable>
+              </htd>
+            </htr>
+          </htable>
+        </editor>
+      ) as any) as PlateEditor;
+
+      const output = ((
+        <editor>
+          <htable>
+            <htr>
+              <htd>
+                <hp>a</hp>
+                <hp>b</hp>
+                <hp>c</hp>
+              </htd>
+            </htr>
+          </htable>
+        </editor>
+      ) as any) as PlateEditor;
+
+      const editor = createPlateEditor({
+        editor: input,
+        plugins: [createTablePlugin()],
+      });
+
+      normalizeEditor(editor, {
+        force: true,
+      });
+      expect(editor.children).toEqual(output.children);
+    });
+  });
 });
