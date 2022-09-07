9459ec293cc47d990bcf4abdb91c841381d91ce0
zbeyens <ziad.beyens@gmail.com>
2022-09-07 15:39:28 +0200

    feat

--- packages/core/src/types/react/RenderFunction.ts
+++ packages/core/src/types/react/RenderFunction.ts
@@ -1,11 +1,12 @@
 /**
  * Render function interface for providing overrideable render callbacks.
  */
+import { ReactNode } from 'react';
 import { AnyObject } from '../misc/AnyObject';
 
 export interface RenderFunction<P = AnyObject> {
-  (
-    props: P,
-    defaultRender?: (props?: P) => JSX.Element | null
-  ): JSX.Element | null;
+  (props: P, defaultRender?: (props?: P) => JSX.Element | null):
+    | JSX.Element
+    | ReactNode
+    | null;
 }
--- packages/selection/src/createBlockSelectionPlugin.tsx
+++ packages/selection/src/createBlockSelectionPlugin.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import { createPluginFactory } from '@udecode/plate-core';
+import { createPluginFactory, QueryNodeOptions } from '@udecode/plate-core';
+import { BlockSelectable } from './components/BlockSelectable';
 import { BlockSelectionArea } from './components/BlockSelectionArea';
+import { BlockStartArea } from './components/BlockStartArea';
 import { blockSelectionActions } from './blockSelectionStore';
-import { injectBlockSelectionComponent } from './injectBlockSelectionComponent';
 
-export const KEY_SELECTION = 'selection';
+export const KEY_BLOCK_SELECTION = 'blockSelection';
 
-export const createBlockSelectionPlugin = createPluginFactory({
-  key: KEY_SELECTION,
-  inject: {
-    aboveComponent: injectBlockSelectionComponent,
-  },
-  handlers: {
-    onFocus: () => () => {
-      blockSelectionActions.reset();
+export interface BlockSelectionPlugin {
+  query?: QueryNodeOptions;
+}
+
+export const createBlockSelectionPlugin = createPluginFactory<BlockSelectionPlugin>(
+  {
+    key: KEY_BLOCK_SELECTION,
+    options: {
+      query: {
+        maxLevel: 1,
+      },
     },
-  },
-  then: (editor) => ({
-    renderAboveEditable: ({ children }) => {
-      return BlockSelectionArea({ children, editor });
+    inject: {
+      aboveComponent: () => ({ element, children }) =>
+        BlockSelectable({ element, children }),
     },
-  }),
-});
+    handlers: {
+      onFocus: () => () => {
+        blockSelectionActions.reset();
+      },
+    },
+    renderAboveEditable: ({ children }) => (
+      <BlockSelectionArea>
+        <BlockStartArea size={28} placement="left" />
+        <BlockStartArea size={50} placement="top" />
+        <BlockStartArea size={50} placement="right" />
+        <BlockStartArea size={50} placement="bottom" />
+        {children}
+      </BlockSelectionArea>
+    ),
+  }
+);
