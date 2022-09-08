1e053594310ccff11e61e65f4b7fbb09015dd299
zbeyens <zbeyens@users.noreply.github.com>
2022-09-08 01:36:42 +0000

    docs

--- docs/docs/sandpack/plate-versions.ts
+++ docs/docs/sandpack/plate-versions.ts
@@ -1,4 +1,4 @@
-export const plateVersion = '16.6.1';
+export const plateVersion = '16.8.0';
 export const testUtilsVersion = '13.4.0';
-export const excalidrawVersion = '16.5.0';
-export const juiceVersion = '16.5.0';
+export const excalidrawVersion = '16.8.0';
+export const juiceVersion = '16.8.0';
