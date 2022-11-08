0e5830b370775644b2b5713fbfc1dcca21b8d30f
Sunny Hirai <thesunny@gmail.com>
2022-11-07 17:57:11 -0800

    Use KEY_CLOUD constant in createCloudPlugin

--- packages/cloud/src/cloud/createCloudPlugin.ts
+++ packages/cloud/src/cloud/createCloudPlugin.ts
@@ -3,12 +3,14 @@ import { onDrop, onPaste } from './handlers';
 import { CloudEditor, CloudPlugin } from './types';
 import { withCloudOverrides } from './withCloudOverrides';
 
+export const KEY_CLOUD = 'cloud';
+
 export const createCloudPlugin = createPluginFactory<
   CloudPlugin,
   Value,
   CloudEditor<Value>
 >({
-  key: 'cloud',
+  key: KEY_CLOUD,
   withOverrides: withCloudOverrides,
   handlers: {
     onDrop: (editor) => (e) => onDrop(editor, e),
