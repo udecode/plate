d22ff3e1ec8b9d1998914a7a5e660e3834d09e82
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-11-21 17:42:07 +0100

    removed mention dependency

--- packages/nodes/emoji/src/handlers/emojiOnKeyDownHandler.ts
+++ packages/nodes/emoji/src/handlers/emojiOnKeyDownHandler.ts
@@ -1,9 +1,48 @@
-import { PlateEditor, Value } from '@udecode/plate-core';
+import { KeyboardEvent } from 'react';
 import {
-  KeyboardEventHandler,
-  moveSelectionByOffset,
-  MoveSelectionByOffsetOptions,
-} from '@udecode/plate-mention';
+  HandlerReturnType,
+  moveSelection,
+  PlateEditor,
+  Value,
+} from '@udecode/plate-core';
+import isHotkey from 'is-hotkey';
+import { Range } from 'slate';
+
+/**
+ * TODO: move to core
+ */
+type KeyboardEventHandler = (event: KeyboardEvent) => HandlerReturnType;
+
+interface MoveSelectionByOffsetOptions<V extends Value = Value> {
+  query?: (editor: PlateEditor<V>) => boolean;
+}
+
+// TODO: move to core
+const moveSelectionByOffset: <V extends Value>(
+  editor: PlateEditor<V>,
+  options?: MoveSelectionByOffsetOptions<V>
+) => KeyboardEventHandler = (editor, { query = () => true } = {}) => (
+  event
+) => {
+  const { selection } = editor;
+
+  if (!selection || Range.isExpanded(selection) || !query(editor)) {
+    return false;
+  }
+
+  if (isHotkey('left', event)) {
+    event.preventDefault();
+    moveSelection(editor, { unit: 'offset', reverse: true });
+    return true;
+  }
+
+  if (isHotkey('right', event)) {
+    event.preventDefault();
+    moveSelection(editor, { unit: 'offset' });
+    return true;
+  }
+};
+// ------>
 
 export const emojiOnKeyDownHandler: <V extends Value>(
   options?: MoveSelectionByOffsetOptions<V>
