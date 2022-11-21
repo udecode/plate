9509d3657a161eb20548db7effe1376cf4f7c04d
Florian <f.oellerich@epages.com>
2022-11-21 18:20:30 +0100

    plugin-table: Add `at` option for inserts (#2006)
    * Add `at` option to insertTableColumn
    
    * Add `at` option to insertTableRow
    
    * Create cuddly-cups-cover.md
    
    Co-authored-by: Ziad Beyens <ziad.beyens@gmail.com>

--- packages/nodes/table/src/transforms/insertTableRow.spec.tsx
+++ packages/nodes/table/src/transforms/insertTableRow.spec.tsx
@@ -86,4 +86,83 @@ describe('insertTableRow', () => {
       expect(editor.selection).toEqual(output.selection);
     });
   });
+
+  describe('when inserting a table row at specific path', () => {
+    it('should insert a tr with empty cells', () => {
+      const input = ((
+        <editor>
+          <htable>
+            <htr>
+              <htd>
+                <hp>11</hp>
+              </htd>
+              <htd>
+                <hp>12</hp>
+              </htd>
+            </htr>
+            <htr>
+              <htd>
+                <hp>
+                  21
+                  <cursor />
+                </hp>
+              </htd>
+              <htd>
+                <hp>22</hp>
+              </htd>
+            </htr>
+          </htable>
+        </editor>
+      ) as any) as PlateEditor;
+
+      const output = ((
+        <editor>
+          <htable>
+            <htr>
+              <htd>
+                <hp>
+                  <cursor />
+                </hp>
+              </htd>
+              <htd>
+                <hp>
+                  <htext />
+                </hp>
+              </htd>
+            </htr>
+            <htr>
+              <htd>
+                <hp>11</hp>
+              </htd>
+              <htd>
+                <hp>12</hp>
+              </htd>
+            </htr>
+            <htr>
+              <htd>
+                <hp>21</hp>
+              </htd>
+              <htd>
+                <hp>22</hp>
+              </htd>
+            </htr>
+          </htable>
+        </editor>
+      ) as any) as PlateEditor;
+
+      const editor = createPlateEditor({
+        editor: input,
+        plugins: [
+          createTablePlugin({
+            options: { newCellChildren: [{ text: '' }] },
+          }),
+        ],
+      });
+
+      insertTableRow(editor, { at: [0, 0] });
+
+      expect(editor.children).toEqual(output.children);
+      expect(editor.selection).toEqual(output.selection);
+    });
+  });
 });
