a3fbd4fd3b17b00024ddc8aec3b02bd1f2068588
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-12-15 11:36:49 +0100

    udpated types

--- packages/nodes/emoji/src/utils/EmojiLibrary/EmojiFloatingGrid.ts
+++ packages/nodes/emoji/src/utils/EmojiLibrary/EmojiFloatingGrid.ts
@@ -1,6 +1,6 @@
 import { createRef, RefObject } from 'react';
 import { EmojiCategoryList } from '../../types';
-import { AGrid } from './Grid';
+import { AGrid } from '../Grid';
 
 export class EmojiFloatingGrid extends AGrid<
   RefObject<HTMLDivElement>,
--- packages/nodes/emoji/src/utils/EmojiLibrary/EmojiFloatingGrid.types.ts
+++ packages/nodes/emoji/src/utils/EmojiLibrary/EmojiFloatingGrid.types.ts
@@ -1,6 +1,6 @@
 import { RefObject } from 'react';
 import { EmojiCategoryList } from '../../types';
-import { IGrid } from './Grid.types';
+import { IGrid } from '../Grid.types';
 
 export type EmojiFloatingGridType = IGrid<
   RefObject<HTMLDivElement>,
