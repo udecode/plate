0efd3197048da575fd473707a69b45ca1d4da366
Seiichi Nishikata <seiichi.nishikata456@gmail.com>
2023-01-11 21:08:40 +0900

    Remove unnecessary query in deserialize markdown. (#2115)

---
+++ .changeset/happy-jobs-judge.md
@@ -0,0 +1,5 @@
+---
+'@udecode/plate-serializer-md': patch
+---
+
+Fix markdown string to Node conversion process not working properly in markdown string pasting.
--- packages/serializers/md/src/deserializer/createDeserializeMdPlugin.ts
+++ packages/serializers/md/src/deserializer/createDeserializeMdPlugin.ts
@@ -9,19 +9,6 @@ export const createDeserializeMdPlugin = createPluginFactory({
     editor: {
       insertData: {
         format: 'text/plain',
-        query: ({ data, dataTransfer }) => {
-          const htmlData = dataTransfer.getData('text/html');
-          if (htmlData) return false;
-
-          const { files } = dataTransfer;
-          if (!files?.length) {
-            // if content is simply a URL pass through to not break LinkPlugin
-            if (isUrl(data)) {
-              return false;
-            }
-          }
-          return true;
-        },
         getFragment: ({ data }) => deserializeMd(editor, data),
       },
     },
