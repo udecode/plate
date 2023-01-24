28808d0dfd8004c862d88fbc9dea148f12fdc824
zbeyens <ziad.beyens@gmail.com>
2023-01-24 03:31:55 +0100

    fix

---
+++ packages/core/src/plugins/createEditorProtocolPlugin.ts
@@ -0,0 +1,74 @@
+import { getBlockAbove, isSelectionAtBlockStart } from '../queries/index';
+import { getMarks, isExpanded, isStartPoint, Value } from '../slate/index';
+import { removeMark } from '../transforms/index';
+import { PlateEditor } from '../types/index';
+import { createPluginFactory } from '../utils/plate/createPluginFactory';
+
+export const KEY_EDITOR_PROTOCOL = 'editorProtocol';
+
+export const withEditorProtocol = <
+  V extends Value = Value,
+  E extends PlateEditor<V> = PlateEditor<V>
+>(
+  editor: E
+) => {
+  const { deleteBackward, deleteForward, deleteFragment } = editor;
+
+  const resetMarks = () => {
+    // move to isSelectionAtBlockStart
+    const path = getBlockAbove(editor)?.[1];
+    if (!path) return;
+
+    let isAtBlockStart = isStartPoint(editor, editor.selection?.focus, path);
+    isAtBlockStart =
+      isAtBlockStart ||
+      (isExpanded(editor.selection) &&
+        isStartPoint(editor, editor.selection?.anchor, path));
+
+    if (isAtBlockStart) {
+      const marks = getMarks(editor);
+
+      if (marks) {
+        // remove all marks
+        removeMark(editor, {
+          key: Object.keys(marks) as any,
+        });
+      }
+    }
+  };
+
+  editor.deleteBackward = (unit) => {
+    deleteBackward(unit);
+
+    resetMarks();
+  };
+
+  editor.deleteForward = (unit) => {
+    deleteForward(unit);
+
+    resetMarks();
+  };
+
+  editor.deleteFragment = (direction) => {
+    deleteFragment(direction);
+
+    if (isSelectionAtBlockStart(editor)) {
+      const marks = getMarks(editor);
+
+      if (marks) {
+        // remove all marks
+        removeMark(editor, {
+          key: Object.keys(marks) as any,
+        });
+      }
+    }
+    resetMarks();
+  };
+
+  return editor;
+};
+
+export const createEditorProtocolPlugin = createPluginFactory({
+  key: KEY_EDITOR_PROTOCOL,
+  withOverrides: withEditorProtocol,
+});
