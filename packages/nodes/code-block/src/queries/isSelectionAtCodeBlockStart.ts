d136f2063f7c96cc341e52be9f3a7f58a4f19ba8
Joe Anderson <joe@mousetrapped.co.uk>
2023-01-27 16:49:20 +0000

    Add isCodeBlockEmpty and isSelectionAtCodeBlockStart
    Queries for use with the reset node plugin to ensure correct behaviour
    with code blocks.

---
+++ packages/nodes/code-block/src/queries/isSelectionAtCodeBlockStart.ts
@@ -0,0 +1,22 @@
+import {
+  isExpanded,
+  isStartPoint,
+  PlateEditor,
+  Value,
+} from '@udecode/plate-core';
+import { getCodeLineEntry } from './getCodeLineEntry';
+
+/**
+ * Is the selection at the start of the first code line in a code block
+ */
+export const isSelectionAtCodeBlockStart = <V extends Value>(
+  editor: PlateEditor<V>
+) => {
+  const { selection } = editor;
+  if (!selection || isExpanded(selection)) return false;
+
+  const { codeBlock } = getCodeLineEntry(editor) ?? {};
+  if (!codeBlock) return false;
+
+  return isStartPoint(editor, selection.anchor, codeBlock[1]);
+};
