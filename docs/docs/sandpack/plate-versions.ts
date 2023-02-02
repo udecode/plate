2cc2890182145bcd92affe3d91a9a9205491b669
zbeyens <zbeyens@users.noreply.github.com>
2023-02-02 19:29:38 +0000

    docs

--- docs/docs/sandpack/plate-versions.ts
+++ docs/docs/sandpack/plate-versions.ts
@@ -1,8 +1,8 @@
-export const plateVersion = '19.4.1';
+export const plateVersion = '19.4.2';
 export const testUtilsVersion = '13.4.0';
-export const excalidrawVersion = '19.2.0';
-export const juiceVersion = '19.2.0';
-export const selectionVersion = '19.2.0';
-export const cloudVersion = '19.2.0';
-export const cloudUiVersion = '19.2.0';
-export const dndVersion = '19.2.0';
+export const excalidrawVersion = '19.4.2';
+export const juiceVersion = '19.4.2';
+export const selectionVersion = '19.4.2';
+export const cloudVersion = '19.4.2';
+export const cloudUiVersion = '19.4.2';
+export const dndVersion = '19.4.2';
