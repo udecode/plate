cfbf5b85cf69437ea2b5e1ab4df2b8859f5c46ca
dylans <dylans@users.noreply.github.com>
2023-04-05 21:43:45 +0000

    docs

--- docs/docs/sandpack/plate-versions.ts
+++ docs/docs/sandpack/plate-versions.ts
@@ -1,9 +1,9 @@
-export const plateVersion = '20.6.0';
+export const plateVersion = '20.6.3';
 export const testUtilsVersion = '13.4.0';
 export const excalidrawVersion = '20.4.0';
 export const juiceVersion = '20.4.0';
 export const selectionVersion = '20.4.0';
 export const cloudVersion = '20.4.0';
-export const cloudUiVersion = '20.4.0';
+export const cloudUiVersion = '20.6.2';
 export const dndVersion = '20.4.0';
 export const dndUiVersion = '20.4.0';
