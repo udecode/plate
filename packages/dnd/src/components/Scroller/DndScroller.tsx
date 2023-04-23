a89cff3eb38bd53c5adf0ad3dd7eeeba65d8df41
Ziad Beyens <ziad.beyens@gmail.com>
2023-04-23 20:22:58 +0200

    Revert "Revert "fix: add delay to DnD scroller (#2354)" (#2356)"
    This reverts commit cb8e4edf700a5cca0c573be4a00d4da88029fb17.

--- packages/dnd/src/components/Scroller/DndScroller.tsx
+++ packages/dnd/src/components/Scroller/DndScroller.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { dndStore } from '../../dndStore';
 import { Scroller, ScrollerProps } from './Scroller';
 
 export const DndScroller = (props: Partial<ScrollerProps>) => {
   const isDragging = dndStore.use.isDragging();
+  const [show, setShow] = React.useState(false)
 
-  return <Scroller enabled={isDragging} {...props} />;
+  useEffect(() => {
+    if (isDragging) {
+      const timeout = setTimeout(() => {
+        setShow(true);
+      }, 100);
+      return () => clearTimeout(timeout);
+    } else {
+      setShow(false)
+    }
+  }, [isDragging, show]);
+
+  return <Scroller enabled={isDragging && show} {...props} />;
 };
