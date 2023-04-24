8a9e32c389f650fe5475f3867a6b86228c548fc2
Ziad Beyens <ziad.beyens@gmail.com>
2023-04-24 21:24:26 +0200

    🐛 trailing-block type (#2359)
    * 🐛 type
    
    * Create shaggy-ties-sin.md

---
+++ .changeset/shaggy-ties-sin.md
@@ -0,0 +1,5 @@
+---
+"@udecode/plate-trailing-block": patch
+---
+
+🐛 `type` option was not used
--- packages/editor/trailing-block/src/withTrailingBlock.ts
+++ packages/editor/trailing-block/src/withTrailingBlock.ts
@@ -1,5 +1,7 @@
 import {
+  ELEMENT_DEFAULT,
   getLastNodeByLevel,
+  getPluginType,
   insertElements,
   PlateEditor,
   queryNode,
@@ -18,8 +20,7 @@ export const withTrailingBlock = <
 >(
   editor: E,
   {
-    type,
-    options: { level, ...query },
+    options: { type = getPluginType(editor, ELEMENT_DEFAULT), level, ...query },
   }: WithPlatePlugin<TrailingBlockPlugin, V, E>
 ) => {
   const { normalizeNode } = editor;
