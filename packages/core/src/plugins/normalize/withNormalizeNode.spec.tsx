722f269cdc79794306ef7fb33b7afd670e87da1c
zbeyens <ziad.beyens@gmail.com>
2023-04-25 20:48:16 +0200

    ✅

---
+++ packages/core/src/plugins/normalize/withNormalizeNode.spec.tsx
@@ -0,0 +1,117 @@
+/** @jsx jsx */
+
+import { jsx } from '@udecode/plate-test-utils';
+import { isText } from '@udecode/slate';
+import { createPlateEditor } from '../../utils/index';
+
+jsx;
+
+describe('withNormalizeNode', () => {
+  it('normalizeNode', () => {
+    const input = (
+      <editor>
+        <hp>
+          <htext>
+            <cursor />
+          </htext>
+        </hp>
+      </editor>
+    ) as any;
+
+    const output = (
+      <editor>
+        <hp>
+          <htext>
+            a
+            <cursor />
+          </htext>
+        </hp>
+      </editor>
+    ) as any;
+
+    const editor = createPlateEditor({
+      editor: input,
+      plugins: [
+        {
+          key: 'testt',
+          withOverrides: (e) => {
+            const { normalizeNode } = e;
+
+            e.normalizeNode = (entry) => {
+              const [node] = entry;
+
+              // mishandled case -> infinite loop
+              if (isText(node)) {
+                e.insertText(' ');
+              }
+
+              normalizeNode(entry);
+            };
+
+            return e;
+          },
+        },
+      ],
+    });
+
+    editor.insertText('a');
+    editor.undo();
+    editor.redo();
+
+    expect(editor.children).toEqual(output.children);
+  });
+
+  it('apply', () => {
+    const input = (
+      <editor>
+        <hp>
+          <htext>
+            <cursor />
+          </htext>
+        </hp>
+      </editor>
+    ) as any;
+
+    const output = (
+      <editor>
+        <hp>
+          <htext>
+            <cursor />
+          </htext>
+        </hp>
+      </editor>
+    ) as any;
+
+    const editor = createPlateEditor({
+      editor: input,
+      plugins: [
+        {
+          key: 'testt',
+          withOverrides: (e) => {
+            const { apply } = e;
+
+            e.apply = (op) => {
+              // infinite loop
+              e.insertText(' ');
+
+              apply(op);
+            };
+
+            return e;
+          },
+        },
+      ],
+    });
+
+    editor.insertText('a');
+    // TODO: Error: Cannot find a descendant at path [10]
+    // editor.moveNodes({
+    //   at: [10],
+    //   to: [11],
+    // });
+    editor.undo();
+    editor.redo();
+
+    expect(editor.children).toEqual(output.children);
+  });
+});
