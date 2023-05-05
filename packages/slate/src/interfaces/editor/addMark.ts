bf9dec09d3a23c17b5ae6ea919de9dcb7c9a85c6
zbeyens <ziad.beyens@gmail.com>
2023-05-05 10:30:50 +0200

    ✨ addRangeMark

--- packages/slate/src/interfaces/editor/addMark.ts
+++ packages/slate/src/interfaces/editor/addMark.ts
@@ -1,4 +1,4 @@
-import { Editor } from 'slate';
+import { Editor, Node, Path, Range, Text, Transforms } from 'slate';
 import { TEditor, Value } from './TEditor';
 
 /**
@@ -12,3 +12,67 @@ export const addMark = <V extends Value>(
   key: string,
   value: any
 ) => Editor.addMark(editor as any, key, value);
+
+export const addRangeMark = <V extends Value>(
+  editor: TEditor<V>,
+  props: any,
+  {
+    at = editor.selection,
+  }: {
+    at?: Range | null;
+  } = {}
+) => {
+  if (at) {
+    const match = (node: Node, path: Path) => {
+      if (!Text.isText(node)) {
+        return false; // marks can only be applied to text
+      }
+      const parentEntry = Editor.parent(editor as any, path);
+      if (!parentEntry) return false;
+
+      const [parentNode] = parentEntry;
+
+      return (
+        !editor.isVoid(parentNode as any) ||
+        editor.markableVoid(parentNode as any)
+      );
+    };
+    const expandedSelection = Range.isExpanded(at);
+    let markAcceptingVoidSelected = false;
+    if (!expandedSelection) {
+      const selectedEntry = Editor.node(editor as any, at);
+      if (!selectedEntry) return;
+
+      const [selectedNode, selectedPath] = selectedEntry;
+
+      if (selectedNode && match(selectedNode, selectedPath)) {
+        const parentEntry = Editor.parent(editor as any, selectedPath);
+        if (!parentEntry) return;
+
+        const [parentNode] = parentEntry;
+
+        markAcceptingVoidSelected =
+          parentNode && editor.markableVoid(parentNode as any);
+      }
+    }
+    if (expandedSelection || markAcceptingVoidSelected) {
+      Transforms.setNodes(editor as any, props, {
+        match,
+        split: true,
+        voids: true,
+        at,
+      });
+    }
+    // else {
+    //   const marks = {
+    //     ...(Editor.marks(editor as any) || {}),
+    //     [key]: value,
+    //   };
+    //
+    //   editor.marks = marks;
+    //   if (!FLUSHING.get(editor as any)) {
+    //     editor.onChange();
+    //   }
+    // }
+  }
+};
