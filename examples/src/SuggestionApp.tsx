097d6b5cc662513a1d01a34d31f8d5bc4d68a96f
Joe Anderson <joe@mousetrapped.co.uk>
2023-05-24 20:28:37 +0100

    Handle multiple marks

--- examples/src/SuggestionApp.tsx
+++ examples/src/SuggestionApp.tsx
@@ -1,5 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
-import * as Popover from '@radix-ui/react-popover';
+import {
+  Popover,
+  PopoverProps,
+  PopoverAnchor,
+  PopoverAnchorProps,
+  PopoverPortal,
+  PopoverPortalProps,
+  PopoverContent,
+  PopoverContentProps,
+  PopoverClose,
+  PopoverCloseProps,
+} from '@radix-ui/react-popover';
 import { cva } from 'class-variance-authority';
 import {
   createSuggestionPlugin,
@@ -7,7 +18,6 @@ import {
   Plate,
   PlateProvider,
   Value,
-  StyledLeaf,
   StyledLeafProps,
   TSuggestionText,
   useSuggestionSelectors,
@@ -15,7 +25,9 @@ import {
   getBlockAbove,
   findNodePath,
   focusEditor,
-  // PlateSuggestionLeaf,
+  usePlateEditorState,
+  getNodeEntries,
+  toDOMNode,
 } from '@udecode/plate';
 import { basicNodesPlugins } from './basic-nodes/basicNodesPlugins';
 import { editableProps } from './common/editableProps';
@@ -55,21 +67,6 @@ const PlateSuggestionLeaf = <V extends Value = Value>(
 
   const isActive = activeSuggestionId === getSuggestionId(text);
 
-  // TODO: Refactor into useOverrideableState helper
-  const [isOpen, setIsOpen] = useState(isActive);
-  useEffect(() => setIsOpen(isActive), [isActive]);
-
-  // Close when text changes
-  // TODO: Refactor into useEffectAfterFirst helper
-  const isFirstRef = useRef(true);
-  useEffect(() => {
-    if (isFirstRef.current) {
-      isFirstRef.current = false;
-      return;
-    }
-    setIsOpen(false);
-  }, [text.text]);
-
   const blockAbove = getBlockAbove(editor, {
     at: findNodePath(editor, text),
     match: (n) => n[MARK_SUGGESTION],
@@ -79,35 +76,101 @@ const PlateSuggestionLeaf = <V extends Value = Value>(
   const [hue] = useState(() => Math.floor(Math.random() * 360));
 
   return (
-    <Popover.Root
+    <span
+      {...attributes}
+      {...nodeProps}
+      className={suggestionLeafVariants({
+        isActive,
+        isDeletion: text.suggestionDeletion,
+      })}
+      style={{
+        ['--suggestion-primary-color' as any]: `hsl(${hue}, 60%, 25%)`, // TW 800
+        ['--suggestion-accent-color' as any]: `hsl(${hue}, 89%, 93%)`, // TW 100
+      }}
+    >
+      {children}
+    </span>
+  );
+};
+
+interface PlateFloatingSuggestionsProps {
+  popoverProps?: {
+    rootProps?: Partial<PopoverProps>;
+    anchorProps?: Partial<PopoverAnchorProps>;
+    portalProps?: Partial<PopoverPortalProps>;
+    contentProps?: Partial<PopoverContentProps>;
+    closeProps?: Partial<PopoverCloseProps>;
+  };
+}
+
+const PlateFloatingSuggestions = ({
+  popoverProps: {
+    rootProps = {},
+    anchorProps = {},
+    portalProps = {},
+    contentProps = {},
+    closeProps = {},
+  } = {},
+}: PlateFloatingSuggestionsProps) => {
+  const editor = usePlateEditorState();
+
+  const activeSuggestionId = useSuggestionSelectors().activeSuggestionId();
+
+  // TODO: Refactor into useOverrideableState helper
+  const [isOpen, setIsOpen] = useState(activeSuggestionId !== null);
+  useEffect(() => setIsOpen(activeSuggestionId !== null), [activeSuggestionId]);
+
+  // Close when text changes
+  // TODO: Refactor into useEffectAfterFirst helper
+  // const isFirstRef = useRef(true);
+  // useEffect(() => {
+  //   if (isFirstRef.current) {
+  //     isFirstRef.current = false;
+  //     return;
+  //   }
+  //   setIsOpen(false);
+  // }, [text.text]);
+
+  return (
+    <Popover
       open={isOpen}
       onOpenChange={(desiredOpen) => {
         // Only allow Radix UI to close the popover, not open it
         if (!desiredOpen) setIsOpen(false);
       }}
+      {...rootProps}
     >
-      <Popover.Anchor asChild>
-        <span
-          {...attributes}
-          {...nodeProps}
-          className={suggestionLeafVariants({
-            isActive,
-            isDeletion: text.suggestionDeletion,
-          })}
-          style={{
-            ['--suggestion-primary-color' as any]: `hsl(${hue}, 60%, 25%)`, // TW 800
-            ['--suggestion-accent-color' as any]: `hsl(${hue}, 89%, 93%)`, // TW 100
-          }}
-        >
-          {children}
-        </span>
-      </Popover.Anchor>
-      <Popover.Portal>
-        <Popover.Content
+      <PopoverAnchor
+        virtualRef={{
+          current: {
+            getBoundingClientRect: () => {
+              const suggestionRects = Array.from(getNodeEntries<TSuggestionText>(editor, {
+                at: [],
+                match: (n) => n.suggestionId === activeSuggestionId,
+              })).map(([node]) => toDOMNode(editor, node)!.getBoundingClientRect());
+
+              const top = Math.min(...suggestionRects.map((r) => r.top));
+              const bottom = Math.max(...suggestionRects.map((r) => r.bottom));
+              const left = Math.min(...suggestionRects.map((r) => r.left));
+              const right = Math.max(...suggestionRects.map((r) => r.right));
+
+              const width = right - left;
+              const height = bottom - top;
+
+              return { top, bottom, left, right, width, height } as DOMRect;
+            },
+          },
+        }}
+        {...anchorProps}
+      />
+
+      <PopoverPortal {...portalProps}>
+        <PopoverContent
           sideOffset={4}
           onOpenAutoFocus={(event) => event.preventDefault()}
           onCloseAutoFocus={() => focusEditor(editor)}
           className="bg-white shadow-md rounded-lg p-4 border"
+          {...contentProps}
         >
           <div className="space-y-2">
             <div className="flex items-center gap-2">
@@ -121,31 +184,34 @@ const PlateSuggestionLeaf = <V extends Value = Value>(
               </div>
 
               {/* Minimise button */}
-              <Popover.Close className="-ml-2 p-1 aspect-square self-start translate-x-2 -translate-y-2 inline-flex justify-center items-center rounded-md transition-colors hover:bg-slate-100 text-xs">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" x2="6" y1="6" y2="18"></line><line x1="6" x2="18" y1="6" y2="18"></line></svg>
-              </Popover.Close>
+              <PopoverClose
+                className="-ml-2 p-1 aspect-square self-start translate-x-2 -translate-y-2 inline-flex justify-center items-center rounded-md transition-colors hover:bg-slate-100 text-xs"
+                {...closeProps}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" x2="6" y1="6" y2="18"></line><line x1="6" x2="18" y1="6" y2="18"></line></svg>
+              </PopoverClose>
             </div>
 
             <p className="max-w-sm text-sm">
-              {text.suggestionDeletion ? 'Remove' : 'Insert'} &ldquo;{text.text}&rdquo;
+              {/*text.suggestionDeletion ? 'Remove' : 'Insert'} &ldquo;{text.text}&rdquo;*/}
             </p>
 
             {/* Buttons */}
             <div className="grid grid-cols-2 gap-2">
               <button type="button" className="inline-flex justify-center items-center rounded-md font-medium py-2 px-4 transition-colors border hover:bg-slate-100">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
               </button>
 
               <button type="button" className="inline-flex justify-center items-center rounded-md text-sm font-medium py-2 px-4 transition-colors border hover:bg-slate-100">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" x2="6" y1="6" y2="18"></line><line x1="6" x2="18" y1="6" y2="18"></line></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" x2="6" y1="6" y2="18"></line><line x1="6" x2="18" y1="6" y2="18"></line></svg>
               </button>
             </div>
           </div>
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+        </PopoverContent>
+      </PopoverPortal>
+    </Popover>
   );
-}
+};
 
 const plugins = createMyPlugins(
   [...basicNodesPlugins, createSuggestionPlugin()],
@@ -158,15 +224,28 @@ const plugins = createMyPlugins(
 );
 
 export default () => {
-  return (
-    <PlateProvider plugins={plugins} initialValue={suggestionValue}>
-      <MySuggestionProvider>
-        <Toolbar>
-          <PlateSuggestionToolbarDropdown />
-        </Toolbar>
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
 
-        <Plate<MyValue> editableProps={editableProps} />
-      </MySuggestionProvider>
-    </PlateProvider>
+  return (
+    <div ref={setContainer} className="min-h-full">
+      <PlateProvider plugins={plugins} initialValue={suggestionValue}>
+        <MySuggestionProvider>
+          <Toolbar>
+            <PlateSuggestionToolbarDropdown />
+          </Toolbar>
+
+          <Plate<MyValue> editableProps={editableProps}>
+            <PlateFloatingSuggestions
+              popoverProps={{
+                contentProps: {
+                  collisionBoundary: container,
+                  collisionPadding: 8,
+                },
+              }}
+            />
+          </Plate>
+        </MySuggestionProvider>
+      </PlateProvider>
+    </div>
   );
 };
