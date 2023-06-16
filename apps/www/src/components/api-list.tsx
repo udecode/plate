aa1c3e9f52e9a696d7b90344750c4c475555bcc4
zbeyens <ziad.beyens@gmail.com>
2023-06-16 23:34:15 +0200

    ✨

--- apps/www/src/components/api-list.tsx
+++ apps/www/src/components/api-list.tsx
@@ -12,18 +12,16 @@ import { Icons } from './icons';
 
 import { cn } from '@/lib/utils';
 
-type PropDef = {
+type Item = {
   name: string;
-  required?: boolean;
   default?: string | boolean;
   type: string;
   description?: string;
-  descriptionCode?: string;
   value?: string;
   children: ReactNode;
 };
 
-export function APIItem({ children, name, type, value }: PropDef) {
+export function APIItem({ children, name, type, value }: Item) {
   return (
     <AccordionItem value={value ?? name}>
       <AccordionTrigger className="group">
@@ -52,6 +50,10 @@ export function APIItem({ children, name, type, value }: PropDef) {
   );
 }
 
+// export function ComponentSourceBadgeList() {
+//
+// }
+
 export function APIList({
   type = 'function',
   description,
@@ -73,26 +75,6 @@ export function APIList({
   return (
     <section className="flex w-full flex-col items-center">
       <div className="w-full pb-16">
-        {/* <div> */}
-        {/*  <h1 */}
-        {/*    className="scroll-mt-[60px] font-mono text-2xl font-semibold" */}
-        {/*    id={name} */}
-        {/*  > */}
-        {/*    <a */}
-        {/*      href={`#${name}`} */}
-        {/*      className={cn( */}
-        {/*        'opacity-0 hover:opacity-100 group-hover:opacity-100' */}
-        {/*      )} */}
-        {/*      onClick={(e) => e.stopPropagation()} */}
-        {/*    > */}
-        {/*      <div className="absolute -left-5 top-2 pr-1"> */}
-        {/*        <Icons.pragma className="h-4 w-4 text-muted-foreground" /> */}
-        {/*      </div> */}
-        {/*    </a> */}
-
-        {/*    <Code>{name}</Code> */}
-        {/*  </h1> */}
-        {/* </div> */}
         {!!description && <p className="mt-10">{description}</p>}
 
         <div className="mt-10 pb-3 ">
@@ -133,119 +115,3 @@ export function APIList({
     </section>
   );
 }
-
-// {data.map(
-//   (
-//     {
-//       name,
-//       type,
-//       required,
-//       default: defaultValue,
-//       description,
-//     },
-//     i
-//   ) => (
-//     <tr key={`${name}-${i}`}>
-//       <Box
-//         as="td"
-//         css={{
-//           borderBottom: '1px solid $gray6',
-//           py: '$3',
-//           pr: '$4',
-//           whiteSpace: 'nowrap',
-//         }}
-//       >
-//         <Code>
-//           {name}
-//           {required ? '*' : null}
-//         </Code>
-//         {description && (
-//           <Popover>
-//             <PopoverTrigger asChild>
-//               <IconButton
-//                 variant="ghost"
-//                 css={{
-//                   ml: '$1',
-//                   verticalAlign: 'middle',
-//                   color: '$gray11',
-//                 }}
-//               >
-//                 <AccessibleIcon label="Prop description">
-//                   <InfoCircledIcon />
-//                 </AccessibleIcon>
-//               </IconButton>
-//             </PopoverTrigger>
-//             <PopoverContent
-//               side="top"
-//               onOpenAutoFocus={(event) => {
-//                 event.preventDefault();
-//                 (event.currentTarget as HTMLElement)?.focus();
-//               }}
-//             >
-//               <Box css={{ py: '$2', px: '$3' }}>
-//                 <Text size="2" css={{ lineHeight: '20px' }}>
-//                   {description}
-//                 </Text>
-//               </Box>
-//             </PopoverContent>
-//           </Popover>
-//         )}
-//       </Box>
-//       <Box
-//         as="td"
-//         css={{ borderBottom: '1px solid $gray6', py: '$3', pr: '$4' }}
-//       >
-//         <Code css={{ bc: '$gray4', color: '$gray11' }}>
-//           {type || type}
-//         </Code>
-//         {Boolean(type) && (
-//           <Popover>
-//             <PopoverTrigger asChild>
-//               <IconButton
-//                 variant="ghost"
-//                 css={{
-//                   ml: '$1',
-//                   verticalAlign: 'middle',
-//                   color: '$gray11',
-//                   display: 'none',
-//                   '@bp1': { display: 'inline-flex' },
-//                 }}
-//               >
-//                 <AccessibleIcon label="See full type">
-//                   <InfoCircledIcon />
-//                 </AccessibleIcon>
-//               </IconButton>
-//             </PopoverTrigger>
-//             <PopoverContent
-//               side="top"
-//               css={{ maxWidth: 'max-content' }}
-//             >
-//               <Box css={{ py: '$2', px: '$2', whiteSpace: 'nowrap' }}>
-//                 <Code>{type}</Code>
-//               </Box>
-//             </PopoverContent>
-//           </Popover>
-//         )}
-//       </Box>
-//       <Box
-//         as="td"
-//         css={{ borderBottom: '1px solid $gray6', py: '$3', pr: '$4' }}
-//       >
-//         {defaultValue ? (
-//           <Code css={{ bc: '$gray4', color: '$gray11' }}>
-//             {defaultValue}
-//           </Code>
-//         ) : (
-//           <Box
-//             as={AccessibleIcon}
-//             label="No default value"
-//             css={{ color: '$gray8' }}
-//           >
-//             <DividerHorizontalIcon />
-//           </Box>
-//         )}
-//       </Box>
-//     </tr>
-//   )
-// )}
-// </tbody>
--- apps/www/src/components/component-source.tsx
+++ apps/www/src/components/component-source.tsx
@@ -1,19 +1,27 @@
 'use client';
 
 import * as React from 'react';
+import { CodeBlockWrapper } from './code-block-wrapper';
 
-import { CodeBlockWrapper } from '@/components/code-block-wrapper';
 import { cn } from '@/lib/utils';
 
 interface ComponentSourceProps extends React.HTMLAttributes<HTMLDivElement> {
   src: string;
+  open?: boolean;
+  name?: string;
 }
 
-export function ComponentSource({ children, className }: ComponentSourceProps) {
+export function ComponentSource({
+  children,
+  open,
+  name,
+  className,
+}: ComponentSourceProps) {
   return (
     <CodeBlockWrapper
-      expandButtonTitle="Expand"
+      expandButtonTitle={name || 'Expand'}
       className={cn('my-6 overflow-hidden rounded-md', className)}
+      open={open}
     >
       {children}
     </CodeBlockWrapper>
