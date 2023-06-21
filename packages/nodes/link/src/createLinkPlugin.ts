82d247c55c386d5a94d7bbfc550f08bad1dab62f
Sindre <91262552+vevsindre@users.noreply.github.com>
2023-06-21 19:36:22 +0200

    Added option to skip link sanitation (#2441)
    * Added option to skip link sanitation
    
    * Rename skipLinkSanitation to skipSanitation
    
    * Fixed typo
    
    * Rename to dangerouslySkipSanitization

--- packages/nodes/link/src/createLinkPlugin.ts
+++ packages/nodes/link/src/createLinkPlugin.ts
@@ -37,6 +37,12 @@ export interface LinkPlugin {
   allowedSchemes?: string[];
 
   /**
+   * Skips sanitation of links.
+   * @default false
+   */
+  dangerouslySkipSanitization?: boolean;
+
+  /**
    * Default HTML attributes for link elements.
    * @default {}
    */
@@ -71,6 +77,7 @@ export const createLinkPlugin = createPluginFactory<LinkPlugin>({
   withOverrides: withLink,
   options: {
     allowedSchemes: ['http', 'https', 'mailto', 'tel'],
+    dangerouslySkipSanitization: false,
     defaultLinkAttributes: {},
     isUrl,
     rangeBeforeOptions: {
