b754281d7d29f5b3c2d41ee10bf4c9d74c5f1554
zbeyens <ziad.beyens@gmail.com>
2023-06-29 12:06:34 +0200

    🎨

--- templates/next-template/tailwind.config.js
+++ templates/next-template/tailwind.config.js
@@ -3,7 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  content: ["src/**/*.{ts,tsx}"],
   theme: {
     container: {
       center: true,
