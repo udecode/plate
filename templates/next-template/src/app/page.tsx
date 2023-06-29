7a947698dca62131d7e5596c7c72dcac7b54d3f5
zbeyens <ziad.beyens@gmail.com>
2023-06-29 19:49:28 +0200

    🎨

--- templates/next-template/src/app/page.tsx
+++ templates/next-template/src/app/page.tsx
@@ -13,8 +13,10 @@ export default function IndexPage() {
           Plate and shadcn/ui.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Plugin system & primitive component library. CLI for styled
-          components. Customizable. Open Source. And Next.js 13 Ready.
+          Plugin system & primitive component library.{' '}
+          <br className="hidden sm:inline" />
+          CLI for styled components. Customizable. Open Source. And Next.js 13
+          Ready.
         </p>
       </div>
       <div className="flex gap-4">
--- templates/next-template/src/components/plate/editor.tsx
+++ templates/next-template/src/components/plate/editor.tsx
@@ -9,8 +9,124 @@ import {
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
-import { createPlateUI } from '@/lib/plate/createPlateUI';
 import { cn } from '@/lib/utils';
+import { ParagraphElement } from '@/components/plate-ui/paragraph-element';
+
+const plugins = createPlugins(
+  [
+    // Nodes
+    createParagraphPlugin(),
+    // createHeadingPlugin(),
+    // createBlockquotePlugin(),
+    // createCodeBlockPlugin(),
+    // createHorizontalRulePlugin(),
+    // createLinkPlugin(linkPlugin),
+    // createImagePlugin(),
+    // createMediaEmbedPlugin(),
+    // createMentionPlugin(),
+    // createTablePlugin(),
+    // createTodoListPlugin(),
+    // createExcalidrawPlugin(),
+    //
+    // // Marks
+    // createBoldPlugin(),
+    // createItalicPlugin(),
+    // createUnderlinePlugin(),
+    // createStrikethroughPlugin(),
+    // createCodePlugin(),
+    // createSubscriptPlugin(),
+    // createSuperscriptPlugin(),
+    // createFontColorPlugin(),
+    // createFontBackgroundColorPlugin(),
+    // createFontSizePlugin(),
+    // createHighlightPlugin(),
+    // createKbdPlugin(),
+    //
+    // // Block Style
+    // createAlignPlugin(alignPlugin),
+    // createIndentPlugin(indentPlugin),
+    // createIndentListPlugin(),
+    // createLineHeightPlugin(lineHeightPlugin),
+    //
+    // // Functionality
+    // createAutoformatPlugin(autoformatPlugin),
+    // createBlockSelectionPlugin({
+    //   options: {
+    //     sizes: {
+    //       top: 0,
+    //       bottom: 0,
+    //     },
+    //   },
+    // }),
+    // createComboboxPlugin(),
+    // createDndPlugin({
+    //   options: { enableScroller: true },
+    // }),
+    // createEmojiPlugin(emojiPlugin),
+    // createExitBreakPlugin(exitBreakPlugin),
+    // createNodeIdPlugin(),
+    // createResetNodePlugin(resetBlockTypePlugin),
+    // createSelectOnBackspacePlugin(selectOnBackspacePlugin),
+    // createSoftBreakPlugin(softBreakPlugin),
+    // createTabbablePlugin(tabbablePlugin),
+    // createTrailingBlockPlugin(trailingBlockPlugin),
+    // dragOverCursorPlugin,
+    //
+    // // Collaboration
+    // createCommentsPlugin(),
+    //
+    // // Deserialization
+    // createDeserializeDocxPlugin(),
+    // createDeserializeMdPlugin( ),
+    // createJuicePlugin(),
+  ],
+  {
+    components:
+      // withDraggables(
+      // withPlaceholders(
+      {
+        // [ELEMENT_BLOCKQUOTE]: BlockquoteElement,
+        // [ELEMENT_CODE_BLOCK]: CodeBlockElement,
+        // [ELEMENT_CODE_LINE]: CodeLineElement,
+        // [ELEMENT_CODE_SYNTAX]: CodeSyntaxLeaf,
+        // [ELEMENT_HR]: HrElement,
+        // [ELEMENT_H1]: withProps(HeadingElement, { variant: 'h1' }),
+        // [ELEMENT_H2]: withProps(HeadingElement, { variant: 'h2' }),
+        // [ELEMENT_H3]: withProps(HeadingElement, { variant: 'h3' }),
+        // [ELEMENT_H4]: withProps(HeadingElement, { variant: 'h4' }),
+        // [ELEMENT_H5]: withProps(HeadingElement, { variant: 'h5' }),
+        // [ELEMENT_H6]: withProps(HeadingElement, { variant: 'h6' }),
+        // [ELEMENT_IMAGE]: ImageElement,
+        // [ELEMENT_LI]: withProps(PlateElement, { as: 'li' }),
+        // [ELEMENT_LINK]: LinkElement,
+        // [ELEMENT_MEDIA_EMBED]: MediaEmbedElement,
+        // [ELEMENT_MENTION]: MentionElement,
+        // [ELEMENT_MENTION_INPUT]: MentionInputElement,
+        // [ELEMENT_UL]: withProps(ListElement, { variant: 'ul' }),
+        // [ELEMENT_OL]: withProps(ListElement, { variant: 'ol' }),
+        [ELEMENT_PARAGRAPH]: ParagraphElement,
+        // [ELEMENT_TABLE]: TableElement,
+        // [ELEMENT_TD]: TableCellElement,
+        // [ELEMENT_TH]: TableCellHeaderElement,
+        // [ELEMENT_TODO_LI]: TodoListElement,
+        // [ELEMENT_TR]: TableRowElement,
+        // [ELEMENT_EXCALIDRAW]: ExcalidrawElement,
+        // [MARK_BOLD]: withProps(PlateLeaf, { as: 'strong' }),
+        // [MARK_CODE]: CodeLeaf,
+        // [MARK_HIGHLIGHT]: HighlightLeaf,
+        // [MARK_ITALIC]: withProps(PlateLeaf, { as: 'em' }),
+        // [MARK_KBD]: KbdLeaf,
+        // [MARK_SEARCH_HIGHLIGHT]: SearchHighlightLeaf,
+        // [MARK_STRIKETHROUGH]: withProps(PlateLeaf, { as: 's' }),
+        // [MARK_SUBSCRIPT]: withProps(PlateLeaf, { as: 'sub' }),
+        // [MARK_SUPERSCRIPT]: withProps(PlateLeaf, { as: 'sup' }),
+        // [MARK_UNDERLINE]: withProps(PlateLeaf, { as: 'u' }),
+        // [MARK_COMMENT]: CommentLeaf,
+      },
+    //   )
+    // )
+  }
+);
 
 export default function Editor() {
   const containerRef = useRef(null);
@@ -21,79 +137,6 @@ export default function Editor() {
       children: [{ text: 'Hello, World!' }],
     },
   ];
-
-  const plugins = createPlugins(
-    [
-      // Nodes
-      createParagraphPlugin(),
-      // createHeadingPlugin(),
-      // createBlockquotePlugin(),
-      // createCodeBlockPlugin(),
-      // createHorizontalRulePlugin(),
-      // createLinkPlugin(linkPlugin),
-      // createImagePlugin(),
-      // createMediaEmbedPlugin(),
-      // createMentionPlugin(),
-      // createTablePlugin(),
-      // createTodoListPlugin(),
-      // createExcalidrawPlugin(),
-      //
-      // // Marks
-      // createBoldPlugin(),
-      // createItalicPlugin(),
-      // createUnderlinePlugin(),
-      // createStrikethroughPlugin(),
-      // createCodePlugin(),
-      // createSubscriptPlugin(),
-      // createSuperscriptPlugin(),
-      // createFontColorPlugin(),
-      // createFontBackgroundColorPlugin(),
-      // createFontSizePlugin(),
-      // createHighlightPlugin(),
-      // createKbdPlugin(),
-      //
-      // // Block Style
-      // createAlignPlugin(alignPlugin),
-      // createIndentPlugin(indentPlugin),
-      // createIndentListPlugin(),
-      // createLineHeightPlugin(lineHeightPlugin),
-      //
-      // // Functionality
-      // createAutoformatPlugin(autoformatPlugin),
-      // createBlockSelectionPlugin({
-      //   options: {
-      //     sizes: {
-      //       top: 0,
-      //       bottom: 0,
-      //     },
-      //   },
-      // }),
-      // createComboboxPlugin(),
-      // createDndPlugin({
-      //   options: { enableScroller: true },
-      // }),
-      // createEmojiPlugin(emojiPlugin),
-      // createExitBreakPlugin(exitBreakPlugin),
-      // createNodeIdPlugin(),
-      // createResetNodePlugin(resetBlockTypePlugin),
-      // createSelectOnBackspacePlugin(selectOnBackspacePlugin),
-      // createSoftBreakPlugin(softBreakPlugin),
-      // createTabbablePlugin(tabbablePlugin),
-      // createTrailingBlockPlugin(trailingBlockPlugin),
-      // dragOverCursorPlugin,
-      //
-      // // Collaboration
-      // createCommentsPlugin(),
-      //
-      // // Deserialization
-      // createDeserializeDocxPlugin(),
-      // createDeserializeMdPlugin( ),
-      // createJuicePlugin(),
-    ],
-    {
-      components: createPlateUI(),
-    }
-  );
 
   return (
     <DndProvider backend={HTML5Backend}>
