615181dfd3e65a863021892c3124043783440d4a
zbeyens <ziad.beyens@gmail.com>
2023-06-29 21:06:14 +0200

    🎨

--- templates/next-template/src/components/plate-ui/draggable.tsx
+++ templates/next-template/src/components/plate-ui/draggable.tsx
@@ -11,11 +11,8 @@ import { DropTargetMonitor } from 'react-dnd';
 
 import { cn } from '@/lib/utils';
 import { Icons } from '@/components/icons';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/plate-ui/tooltip';
+
+import { Tooltip, TooltipContent, TooltipTrigger } from './tooltip';
 
 export interface DraggableProps
   extends PlateElementProps,
