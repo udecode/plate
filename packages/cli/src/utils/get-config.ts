07726935a98414640eb8b5a883955b008db44d63
zbeyens <ziad.beyens@gmail.com>
2023-06-29 11:14:35 +0200

    🎨

--- packages/cli/src/utils/get-config.ts
+++ packages/cli/src/utils/get-config.ts
@@ -16,7 +16,7 @@ import { resolveImport } from './resolve-import';
 export const DEFAULT_STYLE = 'default';
 export const DEFAULT_COMPONENTS = '@/components';
 export const DEFAULT_UTILS = '@/lib/utils';
-export const DEFAULT_TAILWIND_CSS = 'app/globals.css';
+export const DEFAULT_TAILWIND_CSS = 'src/styles/globals.css';
 export const DEFAULT_TAILWIND_CONFIG = 'tailwind.config.js';
 export const DEFAULT_TAILWIND_BASE_COLOR = 'slate';
 
