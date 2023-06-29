0024945bbe83ea8683f1278e65531a9d991f255a
zbeyens <ziad.beyens@gmail.com>
2023-06-29 13:56:27 +0200

    🎨

--- packages/cli/src/utils/templates.ts
+++ packages/cli/src/utils/templates.ts
@@ -1,6 +1,6 @@
 export const UTILS = `import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
