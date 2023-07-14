ac7a2283cf1d8674146f42da578afaa810f5e3b7
zbeyens <zbeyens@udecode.io>
2023-07-14 13:03:38 +0200

    ♻️

--- scripts/sync-templates.sh
+++ scripts/sync-templates.sh
@@ -23,12 +23,14 @@ for folder in $GLOB; do
 
   NAME=${folder##*/}
   CLONE_DIR="__${NAME}__clone__"
+  
+  echo "https://${API_TOKEN_GITHUB}@github.com/${OWNER}/${NAME}.git"
 
   # sync to read-only clones
   # clone, delete files in the clone, and copy (new) files over
   # this handles file deletions, additions, and changes seamlessly
   # note: redirect output to dev/null to avoid any possibility of leaking token
-  git clone --quiet --depth 1 https://$API_TOKEN_GITHUB@github.com/$OWNER/$NAME.git $CLONE_DIR > /dev/null
+  git clone --quiet --depth 1 https://${API_TOKEN_GITHUB}@github.com/${OWNER}/${NAME}.git $CLONE_DIR > /dev/null
   cd $CLONE_DIR
   find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
   cp -r $BASE/$folder/. .
