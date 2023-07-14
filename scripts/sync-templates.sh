b2671c50f0ea10a8cf94eb973c1b1f1726311ef5
zbeyens <zbeyens@udecode.io>
2023-07-14 13:04:56 +0200

    ♻️

--- scripts/sync-templates.sh
+++ scripts/sync-templates.sh
@@ -7,6 +7,7 @@ GLOB=$1
 IS_CI="${CI:-false}"
 BASE=$(pwd)
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+PRE_GITHUB=${API_TOKEN_GITHUB}@
 
 git config --global user.email "zbeyens@udecode.io"
 git config --global user.name "zbeyens"
@@ -30,7 +31,7 @@ for folder in $GLOB; do
   # clone, delete files in the clone, and copy (new) files over
   # this handles file deletions, additions, and changes seamlessly
   # note: redirect output to dev/null to avoid any possibility of leaking token
-  git clone --quiet --depth 1 https://${API_TOKEN_GITHUB}@github.com/${OWNER}/${NAME}.git $CLONE_DIR > /dev/null
+  git clone --quiet --depth 1 https://${PRE_GITHUB}github.com/${OWNER}/${NAME}.git $CLONE_DIR > /dev/null
   cd $CLONE_DIR
   find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
   cp -r $BASE/$folder/. .
