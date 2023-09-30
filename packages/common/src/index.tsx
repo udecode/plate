c0933d241ae707a3dac1f03aed78cb3f02bfc479
Shahriar Shojib <shahriar_shojib@hotmail.com>
2023-09-30 20:35:31 +0600

    remove unnecessary exports

--- packages/common/src/index.tsx
+++ packages/common/src/index.tsx
@@ -4,4 +4,3 @@ export * from '@udecode/slate';
 export * from '@udecode/slate-react';
 export * from '@udecode/slate-utils';
 export * from '@udecode/utils';
-export type { AnyObject } from '@udecode/utils';
