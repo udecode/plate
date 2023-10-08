e931bb215f809274cba0bb8dd1c6c8986b966996
Nageen Chand <nageen523@gmail.com>
2023-10-08 21:12:51 +0530

    changeset update:

--- apps/www/src/lib/plate/demo/plugins/removeOnDeleteForwardPlugin.ts
+++ apps/www/src/lib/plate/demo/plugins/removeOnDeleteForwardPlugin.ts
@@ -1,6 +1,4 @@
 import { PlatePlugin } from '@udecode/plate-common';
-import { ELEMENT_HR } from '@udecode/plate-horizontal-rule';
-import { ELEMENT_IMAGE } from '@udecode/plate-media';
 import { ELEMENT_PARAGRAPH } from '@udecode/plate-paragraph';
 import { DeletePlugin } from '@udecode/plate-select';
 
