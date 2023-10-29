1c3ca24d0b9c86812badfa2a38fa114534fc0e2b
jkcs <1778768609@qq.com>
2023-10-29 20:55:42 +0800

    Add default behavior to strip `\n`

--- packages/core/src/plugins/html-deserializer/utils/htmlTextNodeToString.ts
+++ packages/core/src/plugins/html-deserializer/utils/htmlTextNodeToString.ts
@@ -3,64 +3,50 @@
  */
 import { isHtmlText } from './isHtmlText';
 
-function getStyleFromNode(node: HTMLElement | ChildNode): string {
-  if (!(node as HTMLElement).getAttribute) return '';
+function findParentElementWithWhiteSpace(node: HTMLElement | ChildNode): HTMLElement | null {
+  let parentNode = node.parentNode as HTMLElement;
 
-  const styleAttr = (node as HTMLElement).getAttribute('style');
-
-  return styleAttr ? styleAttr.trim() : '';
-}
-
-// Can use the npm package style-to-object.
-// https://www.npmjs.com/package/style-to-object
-function styleToObject(styleText: string): Record<string, string> {
-  const styleObject = {};
-  const declarations = styleText.split(';');
-
-  const camelCase = (str: string) => {
-    return str.replaceAll(/-([a-z])/g, (match: string, letter: string) => {
-      return letter.toUpperCase();
-    });
-  };
-
-  declarations.forEach((declaration) => {
-    declaration = declaration.trim();
-    if (declaration) {
-      const [property, value] = declaration.split(':');
-      const propertyName = camelCase(property.trim());
-      // @ts-ignore
-      styleObject[propertyName] = value.trim();
-    }
-  });
-
-  return styleObject;
-}
-
-function findParentElementWhiteSpace(node: HTMLElement | ChildNode) {
-  let parentNode = node.parentNode;
-  // Both `node.style` and `getComputedStyle` always return an empty value.
   while (parentNode != null) {
 
-    // The <pre> default style is "white-space: pre;"
-    if (parentNode.nodeName === 'PRE') return 'pre';
-
     if (parentNode.nodeType === Node.ELEMENT_NODE) {
-      const styleStr = getStyleFromNode(parentNode as typeof node);
-      const styles = styleStr ? styleToObject(styleStr) : {};
+      const style = parentNode.style;
 
-      if (styles['whiteSpace'] && styles['whiteSpace'] !== 'inherit') {
-        return styles['whiteSpace'];
+      // The <pre> default style is "white-space: pre;"
+      if (parentNode.nodeName === 'PRE') return parentNode;
+
+      if (style.whiteSpace && style.whiteSpace !== 'inherit') {
+        return parentNode;
       }
     }
 
-    parentNode = parentNode.parentNode;
+    parentNode = parentNode.parentNode as HTMLElement;
   }
 
-  return '';
+  return node.parentNode as HTMLElement;
 }
 
+// https://github.com/udecode/plate/pull/2718#discussion_r1375418430
+// Strip exactly one \n from the end of the text node
+const stripOneNewlineFromEnd = (str = '') => str.replace(/((?<!\n)\n(?!\n))$/g, '');
+
+// Strip exactly one \n from the start of the text node
+const stripOneNewlineFromStart = (str = '') => str.replace(/^((?<!\n)\n(?!\n))/g, '');
+
 const mergeWhitespace = (node: HTMLElement | ChildNode) => {
-  let parentWhiteSpace = findParentElementWhiteSpace(node);
+  let parentNode = findParentElementWithWhiteSpace(node);
+  if (!parentNode) return;
+
+  let parentWhiteSpace = parentNode.style.whiteSpace;
+
+  // The <pre> default style is "white-space: pre;"
+  if (parentNode.nodeName === 'PRE') {
+    // For white-space: pre or pre-line:
+    // Strip exactly one \n from the start of the text node
+    // Strip exactly one \n from the end of the text node
+    node.textContent =
+      node.textContent && stripOneNewlineFromStart(stripOneNewlineFromEnd(node.textContent));
+    return;
+  }
 
   if (!parentWhiteSpace) {
     parentWhiteSpace = 'normal';
@@ -75,12 +61,18 @@ const mergeWhitespace = (node: HTMLElement | ChildNode) => {
         node.textContent && node.textContent.trim().replaceAll(/\s+/g, ' ');
       break;
     }
+    // For white-space: pre or pre-line:
+    // Do not strip any \n from the start of the text node
+    // Strip exactly one \n from the end of the text node
     case 'pre-line': {
       node.textContent =
-        node.textContent && node.textContent.replaceAll(/[\t ]+/g, ' ');
+        node.textContent && stripOneNewlineFromEnd(node.textContent.replaceAll(/[\t ]+/g, ' '));
       break;
     }
     case 'pre':
+      node.textContent =
+        node.textContent && stripOneNewlineFromEnd(node.textContent);
+      break;
     case 'break-spaces':
     case 'pre-wrap':
     case 'revert': // "revert" and "revert-layer" are expected to be supported in the future.
