734a985eff6710da9d79edc85f2711247d981209
zbeyens <zbeyens@udecode.io>
2023-12-10 19:28:42 +0100

    ⬆️

--- apps/www/scripts/build-registry.ts
+++ apps/www/scripts/build-registry.ts
@@ -11,6 +11,8 @@ import { registrySchema } from '../src/registry/schema';
 import { styles } from '../src/registry/styles';
 import { themes } from '../src/registry/themes';
 
+// Until https://github.com/TypeStrong/ts-node/issues/1997 is fixed.
+
 const REGISTRY_PATH = path.join(process.cwd(), 'public/registry');
 
 const result = registrySchema.safeParse(registry);
