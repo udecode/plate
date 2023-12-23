9b96debaf6d0e22b0c026fad7e4f16f0138b3a36
Joe Anderson <joe@mousetrapped.co.uk>
2023-12-23 12:27:11 +0000

    Add comments to tabbable code

--- packages/tabbable/src/findTabDestination.ts
+++ packages/tabbable/src/findTabDestination.ts
@@ -18,6 +18,7 @@ export const findTabDestination = <V extends Value = Value>(
   editor: PlateEditor<V>,
   { tabbableEntries, activeTabbableEntry, direction }: FindTabDestinationOptions
 ): TabDestination | null => {
+  // Case 1: A tabbable entry was active before tab was pressed
   if (activeTabbableEntry) {
     // Find the next tabbable entry after the active one
     const activeTabbableEntryIndex =
@@ -26,7 +27,23 @@ export const findTabDestination = <V extends Value = Value>(
       activeTabbableEntryIndex + (direction === 'forward' ? 1 : -1);
     const nextTabbableEntry = tabbableEntries[nextTabbableEntryIndex];
 
-    // If the next tabbable entry is in the same void, focus it
+    /**
+     * If the next tabbable entry originated from the same path as the active
+     * tabbable entry, focus it.
+     *
+     * Examples of when this is true:
+     * - We're inside a void node and there is an additional tabbable inside
+     *   the same void node.
+     * - We're inside a popover containing multiple tabbable elements all
+     *   anchored to the same slate node, and there is an additional tabbable
+     *   inside the same popover.
+     *
+     * Examples of when this is false:
+     * - We're inside a void node and the next tabbable is outside the void
+     *   node.
+     * - We're in the last tabbable element of a popover.
+     * - There is no next tabbable element.
+     */
     if (
       nextTabbableEntry &&
       Path.equals(activeTabbableEntry.path, nextTabbableEntry.path)
@@ -37,7 +54,13 @@ export const findTabDestination = <V extends Value = Value>(
       };
     }
 
-    // Otherwise, focus the first path after the void
+    /**
+     * Otherwise, return the focus to the editor. If we're moving forward,
+     * focus the first point after the active tabbable's path. If we're moving
+     * backward, focus the point of the active tabbable's path.
+     * TODO: Let a tabable entry specify custom before and after points.
+     */
+
     if (direction === 'forward') {
       const pointAfter = getPointAfter(editor, activeTabbableEntry.path);
       if (!pointAfter) return null;
@@ -52,6 +75,8 @@ export const findTabDestination = <V extends Value = Value>(
       path: getPoint(editor, activeTabbableEntry.path).path,
     };
   }
+
+  // Case 2: No tabbable entry was active before tab was pressed
 
   const selectionPath = editor.selection?.anchor?.path || [];
 
