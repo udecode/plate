71cfc129d7636acec1fc7b0c25d2d2f9b7b984eb
felixfeng <felixfeng33@gmail.com>
2024-04-12 11:35:29 +0800

    feat: insert cloumns

--- apps/www/src/components/plate-ui/playground-insert-dropdown-menu.tsx
+++ apps/www/src/components/plate-ui/playground-insert-dropdown-menu.tsx
@@ -26,6 +26,7 @@ import {
   KEY_LIST_STYLE_TYPE,
   toggleIndentList,
 } from '@udecode/plate-indent-list';
+import { ELEMENT_LAYOUT, insertLayout } from '@udecode/plate-layout';
 import { ELEMENT_LINK, triggerFloatingLink } from '@udecode/plate-link';
 import { toggleList } from '@udecode/plate-list';
 import {
@@ -125,6 +126,12 @@ const items = [
         description: 'Divider (---)',
         icon: Icons.hr,
       },
+      {
+        value: ELEMENT_LAYOUT,
+        label: 'Columns',
+        description: 'Columns',
+        icon: Icons.LayoutIcon,
+      },
     ],
   },
   {
@@ -197,6 +204,11 @@ export function PlaygroundInsertDropdownMenu(props: DropdownMenuProps) {
                   className="min-w-[180px]"
                   onSelect={async () => {
                     switch (type) {
+                      case ELEMENT_LAYOUT: {
+                        insertLayout(editor);
+                        break;
+                      }
+
                       case ELEMENT_CODE_BLOCK: {
                         insertEmptyCodeBlock(editor);
 
