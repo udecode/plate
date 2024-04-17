50a602a50810b18f2ed4fcaa7ed7408156be495c
felixfeng <felixfeng33@gmail.com>
2024-04-17 09:59:38 +0800

    fix: typo

--- packages/column/src/column-store.ts
+++ packages/column/src/column-store.ts
@@ -11,35 +11,36 @@ import { TColumnElement, TColumnGroupElement } from './types';
 export const useColumnState = () => {
   const editor = useEditorRef();
 
-  const ColumnGroupElement =
+  const columnGroupElement =
     useElement<TColumnGroupElement>(ELEMENT_COLUMN_GROUP);
+
   const element = useElement<TColumnElement>(ELEMENT_COLUMN);
 
-  const layoutPath = findNodePath(editor, ColumnGroupElement);
+  const columnPath = findNodePath(editor, columnGroupElement);
 
   const setDoubleColumn = () => {
-    setNodes(editor, { layout: '1-1' }, { at: layoutPath });
+    setNodes(editor, { layout: '1-1' }, { at: columnPath });
   };
 
   const setThreeColumn = () => {
-    setNodes(editor, { layout: '1-1-1' }, { at: layoutPath });
+    setNodes(editor, { layout: '1-1-1' }, { at: columnPath });
   };
 
   const setRightSideDoubleColumn = () => {
-    setNodes(editor, { layout: '3-1' }, { at: layoutPath });
+    setNodes(editor, { layout: '3-1' }, { at: columnPath });
   };
 
   const setLeftSideDoubleColumn = () => {
-    setNodes(editor, { layout: '1-3' }, { at: layoutPath });
+    setNodes(editor, { layout: '1-3' }, { at: columnPath });
   };
 
   const setDoubleSideDoubleColumn = () => {
-    setNodes(editor, { layout: '1-2-1' }, { at: layoutPath });
+    setNodes(editor, { layout: '1-2-1' }, { at: columnPath });
   };
 
   return {
     element,
-    ColumnGroupElement,
+    columnGroupElement,
     setDoubleColumn,
     setDoubleSideDoubleColumn,
     setLeftSideDoubleColumn,
