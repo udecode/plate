ff6b8516e25c057c5c9aef974e1015d0e5803c8d
Emilien Bidet <emilienbdt@gmail.com>
2024-04-30 10:42:56 +0200

    Fix type

--- packages/heading/src/types.ts
+++ packages/heading/src/types.ts
@@ -2,11 +2,11 @@ import { HotkeyPlugin } from '@udecode/plate-common';
 
 export interface HeadingPlugin extends HotkeyPlugin {}
 
-export type HEADING_LEVEL = 1 | 2 | 3 | 4 | 5 | 6;
+export type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
 
 export interface HeadingsPlugin {
   /**
    * Heading levels supported from 1 to `levels`
    */
-  levels?: HEADING_LEVEL | HEADING_LEVEL[];
+  levels?: HeadingLevel | HeadingLevel[];
 }
