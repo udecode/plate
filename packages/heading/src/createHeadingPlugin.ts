49e1ec6ee2fa90f8d160377781817ce83b1e619e
Emilien Bidet <emilienbdt@gmail.com>
2024-04-30 11:07:35 +0200

    Fix the ts error

--- packages/heading/src/createHeadingPlugin.ts
+++ packages/heading/src/createHeadingPlugin.ts
@@ -19,11 +19,11 @@ export const createHeadingPlugin = createPluginFactory<HeadingsPlugin>({
   then: (editor, { options: { levels } = {} }) => {
     const plugins: PlatePlugin<HeadingPlugin>[] = [];
 
-    levels = Array.isArray(levels)
+    const headingLevels = Array.isArray(levels)
       ? levels
-      : Array.from({ length: levels }, (_, i) => i + 1);
+      : Array.from({ length: levels || 6 }, (_, i) => i + 1);
 
-    levels!.forEach((level) => {
+    headingLevels.forEach((level) => {
       const key = KEYS_HEADING[level - 1];
 
       const plugin: PlatePlugin<HeadingPlugin> = {
