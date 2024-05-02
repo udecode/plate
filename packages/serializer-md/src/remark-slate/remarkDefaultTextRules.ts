5d87ade89611bb8e616f79790d04d23409137dd5
zbeyens <zbeyens@udecode.io>
2024-05-03 00:09:42 +0200

    feat

--- packages/serializer-md/src/remark-slate/remarkDefaultTextRules.ts
+++ packages/serializer-md/src/remark-slate/remarkDefaultTextRules.ts
@@ -1,12 +1,11 @@
-import { MARK_BOLD, MARK_CODE, MARK_ITALIC } from '@udecode/plate-basic-marks';
 import { type Value, getPluginType } from '@udecode/plate-common/server';
 
 import type { RemarkTextRules } from './types';
 
 export const remarkDefaultTextRules: RemarkTextRules<Value> = {
-  emphasis: { mark: ({ editor }) => getPluginType(editor, MARK_ITALIC) },
+  emphasis: { mark: ({ editor }) => getPluginType(editor, 'italic') },
   html: { transform: (text: string) => text.replaceAll('<br>', '\n') },
-  inlineCode: { mark: ({ editor }) => getPluginType(editor, MARK_CODE) },
-  strong: { mark: ({ editor }) => getPluginType(editor, MARK_BOLD) },
+  inlineCode: { mark: ({ editor }) => getPluginType(editor, 'code') },
+  strong: { mark: ({ editor }) => getPluginType(editor, 'bold') },
   text: {},
 };
