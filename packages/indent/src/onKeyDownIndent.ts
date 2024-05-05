b5e7caff19d352f64278334d84e8278edabe4eaf
Feng <felixfeng33@gmail.com>
2024-05-05 15:20:12 +0800

    feat: add indent keydown api

--- packages/indent/src/onKeyDownIndent.ts
+++ packages/indent/src/onKeyDownIndent.ts
@@ -1,11 +1,14 @@
 import {
+  getPluginOptions,
   Hotkeys,
   KeyboardHandlerReturnType,
   PlateEditor,
   Value,
 } from '@udecode/plate-common';
 
+import { KEY_INDENT } from './createIndentPlugin';
 import { indent, outdent } from './transforms/index';
+import { IndentPlugin } from './types';
 
 export const onKeyDownIndent =
   <V extends Value = Value, E extends PlateEditor<V> = PlateEditor<V>>(
@@ -14,13 +17,18 @@ export const onKeyDownIndent =
   (e) => {
     if (e.defaultPrevented) return;
 
+    const { afterKeydownIndent, afterKeydownOutdent } =
+      getPluginOptions<IndentPlugin>(editor as PlateEditor, KEY_INDENT);
+
     if (Hotkeys.isTab(editor, e)) {
       e.preventDefault();
       indent(editor);
+      afterKeydownIndent && afterKeydownIndent(editor);
     }
 
     if (Hotkeys.isUntab(editor, e)) {
       e.preventDefault();
       outdent(editor);
+      afterKeydownOutdent && afterKeydownOutdent(editor);
     }
   };
--- packages/indent/src/types.ts
+++ packages/indent/src/types.ts
@@ -1,4 +1,4 @@
-import { TElement } from '@udecode/plate-common';
+import { PlateEditor, TElement, Value } from '@udecode/plate-common';
 
 export interface TIndentElement extends TElement {
   indent: number;
@@ -7,18 +7,24 @@ export interface TIndentElement extends TElement {
 export type IndentPlugin = {
   /**
    * Indentation offset used in `(offset * element.indent) + unit`.
+   *
    * @default 40
    */
   offset?: number;
 
   /**
    * Indentation unit used in `(offset * element.indent) + unit`.
+   *
    * @default 'px'
    */
   unit?: string;
 
-  /**
-   * Maximum number of indentation.
-   */
+  /** Maximum number of indentation. */
   indentMax?: number;
+
+  /** Call after indent */
+  afterKeydownIndent?: <V extends Value>(editor: PlateEditor<V>) => void;
+
+  /** Call after outdent */
+  afterKeydownOutdent?: <V extends Value>(editor: PlateEditor<V>) => void;
 };
