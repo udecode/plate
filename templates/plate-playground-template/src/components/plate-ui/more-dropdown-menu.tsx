2ec99caf0c1d628e06f63242db357e890f66042c
Leonidas <77194479+LeonMueller-OneAndOnly@users.noreply.github.com>
2024-05-31 19:47:04 +0200

    fix: reapply same fix as previous

--- templates/plate-playground-template/src/components/plate-ui/more-dropdown-menu.tsx
+++ templates/plate-playground-template/src/components/plate-ui/more-dropdown-menu.tsx
@@ -33,8 +33,8 @@ export function MoreDropdownMenu(props: DropdownMenuProps) {
         <DropdownMenuItem
           onSelect={() => {
             toggleMark(editor, {
-              key: MARK_SUBSCRIPT,
-              clear: MARK_SUPERSCRIPT,
+              key: MARK_SUPERSCRIPT,
+               clear: [MARK_SUPERSCRIPT, MARK_SUBSCRIPT],
             });
             focusEditor(editor);
           }}
@@ -46,8 +46,8 @@ export function MoreDropdownMenu(props: DropdownMenuProps) {
         <DropdownMenuItem
           onSelect={() => {
             toggleMark(editor, {
-              key: MARK_SUPERSCRIPT,
               clear: MARK_SUBSCRIPT,
+              clear: [MARK_SUPERSCRIPT, MARK_SUBSCRIPT],
             });
             focusEditor(editor);
           }}
