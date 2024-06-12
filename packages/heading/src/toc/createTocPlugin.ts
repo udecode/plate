da5276b2112121546ddbca3551ed180f4e4bdf92
natamox <natamoxc@gmail.com>
2024-06-12 20:58:26 +0800

    rm useless

--- packages/heading/src/toc/createTocPlugin.ts
+++ packages/heading/src/toc/createTocPlugin.ts
@@ -2,7 +2,7 @@ import { createPluginFactory } from '@udecode/plate-common';
 
 export const ELEMENT_TOC = 'toc';
 
-export const createTocPlugin = createPluginFactory<{ continerRef: any }>({
+export const createTocPlugin = createPluginFactory({
   isElement: true,
   isVoid: true,
   key: ELEMENT_TOC,
