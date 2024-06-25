0013f42ec9f12bcb8cf7ff0e726acb7b51d6bdbc
Beeant <huangxb0512@gmail.com>
2024-06-25 04:49:17 +0000

    feat: enhance isEncoded test case

--- packages/link/src/utils/encodeUrlIfNeeded.spec.ts
+++ packages/link/src/utils/encodeUrlIfNeeded.spec.ts
@@ -1,19 +1,35 @@
 import { isEncoded } from './encodeUrlIfNeeded';
 
-describe('encodeUrlIfNeeded', () => {
-  describe('when url is encoded', () => {
-    const url = 'https://www.baidu.com/s?wd=platejs%E6%8F%92%E4%BB%B6';
-
-    it('the url is encoded', () => {
-      expect(isEncoded(url)).toEqual(true);
-    });
+describe('isEncoded function', () => {
+  it('should return false for an unencoded URI', () => {
+    expect(isEncoded('https://example.com/path?query=value')).toEqual(false);
   });
 
-  describe('when url is encoded', () => {
-    const url = 'https://www.baidu.com/s?wd=platejs插件';
+  it('should return true for an encoded URI with special characters', () => {
+    expect(
+      isEncoded(
+        'https://example.com/path%20with%20spaces?query=value%2Bencoded'
+      )
+    ).toEqual(true);
+  });
 
-    it('the url is not encoded', () => {
-      expect(isEncoded(url)).toEqual(false);
-    });
+  it('should handle URI with no changes after decoding', () => {
+    expect(isEncoded('https://example.com/simplepath')).toEqual(false);
+  });
+
+  it('should handle empty string', () => {
+    expect(isEncoded('')).toEqual(false);
+  });
+
+  it('should handle URI with only encoding characters', () => {
+    expect(
+      isEncoded(
+        '%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%40%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%40'
+      )
+    ).toEqual(true);
+  });
+
+  it('should handle non-URI strings', () => {
+    expect(isEncoded('Just a random string without URI format')).toEqual(false);
   });
 });
