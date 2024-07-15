7f8da46d1f9db8994448157f900f36d00dae8664
Beeant <huangxb0512@gmail.com>
2024-07-15 05:38:38 +0000

    fix: #3355

--- packages/table/src/queries/getTableColumnCount.ts
+++ packages/table/src/queries/getTableColumnCount.ts
@@ -1,5 +1,14 @@
 import type { TElement } from '@udecode/plate-common';
 
 export const getTableColumnCount = (tableNode: TElement) => {
-  return (tableNode.children as TElement[])?.[0]?.children?.length ?? 0;
+  if ((tableNode.children as TElement[])?.[0]) {
+    return (tableNode.children as TElement[])[0].children
+      .map(
+        (element) =>
+          element.colSpan || (element?.attributes as any)?.colspan || 1
+      )
+      .reduce((total: number, num: number) => total + num);
+  }
+
+  return 0;
 };
