de33137003d1615187ede98d3f200febe13018f9
Filip Sikora <contact@fsikora.com>
2024-07-17 13:48:58 +0000

    feat: `parseTwitterUrl` accept x.com

--- packages/media/src/media-embed/parseTwitterUrl.ts
+++ packages/media/src/media-embed/parseTwitterUrl.ts
@@ -2,7 +2,7 @@ import type { EmbedUrlData } from '../media';
 
 const twitterRegex =
   // eslint-disable-next-line regexp/no-unused-capturing-group
-  /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(?<id>\d+)/;
+  /^https?:\/\/(?:twitter|x)\.com\/(?:#!\/)?(\w+)\/status(es)?\/(?<id>\d+)/;
 
 export const parseTwitterUrl = (url: string): EmbedUrlData | undefined => {
   if (url.match(twitterRegex)) {
