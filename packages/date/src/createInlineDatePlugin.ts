454c3e5899f8eac210e4157927d030dbd64ca5c9
Felix Feng <felixfeng33@gmail.com>
2024-08-17 15:54:09 +0800

    fix

--- packages/date/src/createInlineDatePlugin.ts
+++ packages/date/src/createInlineDatePlugin.ts
@@ -1,8 +1,13 @@
 import {
   type TElement,
   createPluginFactory,
-  isCollapsed,
+  getNodeEntry,
+  getRange,
+  isEndPoint,
+  isSelectionExpanded,
+  isStartPoint,
 } from '@udecode/plate-common/server';
+import { Path } from 'slate';
 
 export const ELEMENT_INLINE_DATE = 'inline_date';
 
@@ -15,27 +20,91 @@ export const createInlineDatePlugin = createPluginFactory({
   withOverrides: (editor) => {
     const { isSelectable, move } = editor;
 
-    // https://github.com/ianstormtaylor/slate/blob/main/site/examples/inlines.tsx#L74-L96
+    editor.isSelectable = (element) => {
+      return (
+        (element as TElement).type !== ELEMENT_INLINE_DATE &&
+        isSelectable(element)
+      );
+    };
+
+    // check if cursor is next to a date node. if it is set the unit:'offset'
     // Default left/right behavior is unit:'character'.
     // This fails to distinguish between two cursor positions, such as
     // <inline>foo<cursor/></inline> vs <inline>foo</inline><cursor/>.
     // Here we modify the behavior to unit:'offset'.
     // This lets the user step into and out of the inline without stepping over characters.
     // You may wish to customize this further to only use unit:'offset' in specific cases.
-    editor.move = (op) => {
-      if (editor.selection && isCollapsed(editor.selection)) {
-        return move({
-          ...op,
-          unit: 'offset',
-        });
+    editor.move = (options) => {
+      const {
+        distance = 1,
+        reverse = false,
+        unit = 'character',
+      } = options || {};
+
+      if (
+        unit === 'character' &&
+        distance === 1 &&
+        editor.selection &&
+        !isSelectionExpanded(editor)
+      ) {
+        const point = editor.selection.anchor;
+        const selectedRange = getRange(editor, point.path);
+
+        const boundary = (() => {
+          let isStart = false;
+          let isEnd = false;
+
+          if (isStartPoint(editor, point, selectedRange)) {
+            isStart = true;
+          }
+          if (isEndPoint(editor, point, selectedRange)) {
+            isEnd = true;
+          }
+          if (isStart && isEnd) {
+            return 'single';
+          }
+          if (isStart) {
+            return 'start';
+          }
+          if (isEnd) {
+            return 'end';
+          }
+
+          return null;
+        })();
+
+        if (!boundary) return move(options);
+
+        const adjacentPathFn = (path: Path) => {
+          try {
+            if (reverse && boundary === 'start') return Path.previous(path);
+            if (!reverse && boundary === 'end') return Path.next(path);
+            if (boundary === 'single') {
+              return reverse ? Path.previous(path) : Path.next(path);
+            }
+          } catch (error) {
+            return null;
+          }
+        };
+
+        if (!adjacentPathFn) return move(options);
+
+        const adjacentPath = adjacentPathFn(point.path);
+
+        if (!adjacentPath) return move(options);
+
+        const nextNodeEntry = getNodeEntry(editor, adjacentPath) ?? null;
+
+        if (nextNodeEntry && nextNodeEntry[0].type === ELEMENT_INLINE_DATE) {
+          return move({
+            unit: 'offset',
+            ...options,
+          });
+        }
       }
 
-      move(op);
+      return move(options);
     };
-
-    editor.isSelectable = (element) =>
-      (element as TElement).type !== ELEMENT_INLINE_DATE &&
-      isSelectable(element);
 
     return editor;
   },
