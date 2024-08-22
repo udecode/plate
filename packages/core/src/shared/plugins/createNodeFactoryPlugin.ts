443b68b10b42b615c04a2ad0ceefeded98c4f949
Nikola Hristov <nklhtv@gmail.com>
2024-08-22 19:25:12 +0300

    configurable node factory plugin

--- packages/core/src/shared/plugins/createNodeFactoryPlugin.ts
+++ packages/core/src/shared/plugins/createNodeFactoryPlugin.ts
@@ -1,19 +1,34 @@
+import type { EElement, TElement, Value } from '@udecode/slate';
+
+import type { WithOverride } from '../types/index';
+
 import { ELEMENT_DEFAULT } from '../constants';
 import { createPluginFactory } from '../utils/createPluginFactory';
 import { getPluginType } from '../utils/index';
 
 export const KEY_NODE_FACTORY = 'nodeFactory';
 
-export const createNodeFactoryPlugin = createPluginFactory({
-  key: KEY_NODE_FACTORY,
-  withOverrides: (editor) => {
-    editor.blockFactory = (node) => ({
-      children: [{ text: '' }],
-      type: getPluginType(editor, ELEMENT_DEFAULT),
-      ...node,
-    });
-    editor.childrenFactory = () => [editor.blockFactory()];
+export type NodeFactoryPlugiin = {
+  createBlock?: <V extends Value = Value>(
+    node?: Partial<TElement>
+  ) => EElement<V>;
+};
 
-    return editor;
-  },
+export const withNodeFactory: WithOverride<NodeFactoryPlugiin> = (
+  editor,
+  { options }
+) => {
+  editor.blockFactory = (node) => ({
+    children: [{ text: '' }],
+    type: getPluginType(editor, ELEMENT_DEFAULT),
+    ...(options.createBlock?.(node) ?? node),
+  });
+  editor.childrenFactory = () => [editor.blockFactory()];
+
+  return editor;
+};
+
+export const createNodeFactoryPlugin = createPluginFactory<NodeFactoryPlugiin>({
+  key: KEY_NODE_FACTORY,
+  withOverrides: withNodeFactory,
 });
