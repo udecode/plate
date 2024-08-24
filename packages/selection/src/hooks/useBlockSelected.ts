afec55e89604055acf0747bb5749b90f3eec93f6
Felix Feng <felixfeng33@gmail.com>
2024-08-24 15:40:30 +0800

    Fix `useBlockSelected` when block-selection plugin is disabled

--- packages/selection/src/hooks/useBlockSelected.ts
+++ packages/selection/src/hooks/useBlockSelected.ts
@@ -1,7 +1,17 @@
+import { useEditorRef } from '@udecode/plate-common';
+
 import { useBlockSelectionSelectors } from '../blockSelectionStore';
+import { KEY_BLOCK_SELECTION } from '../createBlockSelectionPlugin';
 
 export const useBlockSelected = (id?: string) => {
-  return useBlockSelectionSelectors().isSelected(id);
+  const editor = useEditorRef();
+
+  // check if block selection is enabled
+  const hasBlockSelection = editor.plugins.find(
+    (item) => item.key === KEY_BLOCK_SELECTION
+  );
+
+  return useBlockSelectionSelectors().isSelected(id) && hasBlockSelection;
 };
 
 export const useHasBlockSelected = () => {
