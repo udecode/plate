0f9cbfa6e30eae76767b0cc596f98dafaddcac8a
Nikola Hristov <nklhtv@gmail.com>
2024-08-26 14:08:15 +0300

    feat: Add type guard to isBlock

--- packages/slate/src/interfaces/editor/isBlock.ts
+++ packages/slate/src/interfaces/editor/isBlock.ts
@@ -2,10 +2,11 @@ import { Editor } from 'slate';
 
 import type { TEditor, Value } from './TEditor';
 
-import { isElement } from '../element';
+import { type TElement, isElement } from '../element';
 
 /** Check if a value is a block `Element` object. */
 export const isBlock = <V extends Value>(
   editor: TEditor<V>,
   value: any
-): boolean => isElement(value) && Editor.isBlock(editor as any, value);
+): value is TElement =>
+  isElement(value) && Editor.isBlock(editor as any, value);
