6dac1cefa20a7ccbf9174e1b5de41a148f735cee
zbeyens <zbeyens@udecode.dev>
2024-08-29 18:25:08 +0200

    export

---
+++ packages/emoji/src/react/EmojiPlugin.tsx
@@ -0,0 +1,5 @@
+import { toPlatePlugin } from '@udecode/plate-common/react';
+
+import { EmojiPlugin as BaseEmojiPlugin } from '../lib';
+
+export const EmojiPlugin = toPlatePlugin(BaseEmojiPlugin);
