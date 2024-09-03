6c35dabeae1944fb63fa604f686ac60576f35e6b
zbeyens <zbeyens@udecode.dev>
2024-09-04 01:20:48 +0200

    fix

--- packages/core/src/react/stores/plate/selectors/useEditorPlugin.ts
+++ packages/core/src/react/stores/plate/selectors/useEditorPlugin.ts
@@ -17,9 +17,10 @@ export function useEditorPlugin<
   P extends AnyPluginConfig | PlatePlugin<AnyPluginConfig>,
   E extends PlateEditor = PlateEditor,
 >(
-  p: WithRequiredKey<P>
+  p: WithRequiredKey<P>,
+  id?: string
 ): PlatePluginContext<InferConfig<P> extends never ? P : InferConfig<P>, E> {
-  const editor = useEditorRef();
+  const editor = useEditorRef(id);
 
   return getEditorPlugin(editor, p) as any;
 }
