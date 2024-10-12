fdcb110b45cb0112d775c08cc3c63def3c60f63f
Felix Feng <felixfeng33@gmail.com>
2024-10-12 21:26:45 +0800

    iframe

---
+++ apps/www/src/app/_components/potion-tab.tsx
@@ -0,0 +1,9 @@
+export default function PotionTab() {
+  return (
+    <iframe
+      className="h-[800px] w-full rounded-lg border"
+      title="potion"
+      src="https://potion.platejs.org/"
+    />
+  );
+}
