bea337babbbea71fd4f7ddba7727118bdf2863ca
Felix Feng <felixfeng33@gmail.com>
2024-10-16 10:02:30 +0800

    fix

--- apps/www/src/app/_components/potion-tab.tsx
+++ apps/www/src/app/_components/potion-tab.tsx
@@ -3,7 +3,7 @@ export default function PotionTab() {
     <iframe
       className="h-[800px] w-full rounded-lg border"
       title="potion"
-      src="https://potion.platejs.org?iframe-blank=true"
+      src="https://potion.platejs.org/ai-menu/?iframe-blank=true"
     />
   );
 }
