69ffd5bdc8afd02930fbb12eebebc85207f25a53
Felix Feng <felixfeng33@gmail.com>
2024-10-17 12:09:11 +0800

    indent

--- apps/www/src/registry/default/plate-ui/block-context-menu.tsx
+++ apps/www/src/registry/default/plate-ui/block-context-menu.tsx
@@ -1,17 +1,22 @@
-import { useEditorPlugin } from '@udecode/plate-core/react';
+import { useCallback } from 'react';
+
+import { BlockquotePlugin } from '@udecode/plate-block-quote/react';
+import { ParagraphPlugin, useEditorPlugin } from '@udecode/plate-core/react';
+import { HEADING_KEYS } from '@udecode/plate-heading';
+import { IndentListPlugin } from '@udecode/plate-indent-list/react';
 import {
   BLOCK_CONTEXT_MENU_ID,
   BlockMenuPlugin,
+  BlockSelectionPlugin,
 } from '@udecode/plate-selection/react';
+import { unsetNodes } from '@udecode/slate';
+import { focusEditor } from '@udecode/slate-react';
 
 import {
   ContextMenu,
   ContextMenuCheckboxItem,
   ContextMenuContent,
   ContextMenuItem,
-  ContextMenuLabel,
-  ContextMenuRadioGroup,
-  ContextMenuRadioItem,
   ContextMenuSeparator,
   ContextMenuShortcut,
   ContextMenuSub,
@@ -21,9 +26,34 @@ import {
 } from './context-menu';
 
 export const BlockContextMenu = (props: { children: React.ReactNode }) => {
-  const { api } = useEditorPlugin(BlockMenuPlugin);
+  const { api, editor } = useEditorPlugin(BlockMenuPlugin);
 
   const { children } = props;
+
+  const handleTurnInto = useCallback(
+    (type: string) => {
+      editor
+        .getApi(BlockSelectionPlugin)
+        .blockSelection.getNodes()
+        .forEach(([node, path]) => {
+          if (node[IndentListPlugin.key]) {
+            unsetNodes(editor, [IndentListPlugin.key, 'indent'], { at: path });
+          }
+
+          editor.tf.toggle.block({ type }, { at: path });
+        });
+    },
+    [editor]
+  );
+
+  const handleAlign = useCallback(
+    (align: 'center' | 'left' | 'right') => {
+      editor
+        .getTransforms(BlockSelectionPlugin)
+        .blockSelection.setNodes({ align });
+    },
+    [editor]
+  );
 
   return (
     <ContextMenu>
@@ -44,46 +74,89 @@ export const BlockContextMenu = (props: { children: React.ReactNode }) => {
         {children}
       </ContextMenuTrigger>
       <ContextMenuContent className="w-64">
-        <ContextMenuItem inset>
-          Back
-          <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+        <ContextMenuItem inset>Ask AI</ContextMenuItem>
+        <ContextMenuItem
+          onClick={() => {
+            editor
+              .getTransforms(BlockSelectionPlugin)
+              .blockSelection.removeNodes();
+            focusEditor(editor);
+          }}
+          inset
+        >
+          Delete
         </ContextMenuItem>
-        <ContextMenuItem disabled inset>
-          Forward
-          <ContextMenuShortcut>⌘]</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem inset>
-          Reload
-          <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+        <ContextMenuItem
+          onClick={() => {
+            editor
+              .getTransforms(BlockSelectionPlugin)
+              .blockSelection.duplicate(
+                editor.getApi(BlockSelectionPlugin).blockSelection.getNodes()
+              );
+          }}
+          inset
+        >
+          Duplicate
+          <ContextMenuShortcut>⌘ + D</ContextMenuShortcut>
         </ContextMenuItem>
         <ContextMenuSub>
-          <ContextMenuSubTrigger inset>More Tools</ContextMenuSubTrigger>
+          <ContextMenuSubTrigger inset>Turn into</ContextMenuSubTrigger>
           <ContextMenuSubContent className="w-48">
-            <ContextMenuItem>
-              Save Page As...
-              <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
+            <ContextMenuItem
+              onClick={() => handleTurnInto(ParagraphPlugin.key)}
+            >
+              Paragraph
             </ContextMenuItem>
-            <ContextMenuItem>Create Shortcut...</ContextMenuItem>
-            <ContextMenuItem>Name Window...</ContextMenuItem>
-            <ContextMenuSeparator />
-            <ContextMenuItem>Developer Tools</ContextMenuItem>
+
+            <ContextMenuItem onClick={() => handleTurnInto(HEADING_KEYS.h1)}>
+              Heading 1
+            </ContextMenuItem>
+            <ContextMenuItem onClick={() => handleTurnInto(HEADING_KEYS.h2)}>
+              Heading 2
+            </ContextMenuItem>
+            <ContextMenuItem onClick={() => handleTurnInto(HEADING_KEYS.h3)}>
+              Heading 3
+            </ContextMenuItem>
+            <ContextMenuItem
+              onClick={() => handleTurnInto(BlockquotePlugin.key)}
+            >
+              Blockquote
+            </ContextMenuItem>
           </ContextMenuSubContent>
         </ContextMenuSub>
         <ContextMenuSeparator />
-        <ContextMenuCheckboxItem checked>
-          Show Bookmarks Bar
-          <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
+        <ContextMenuCheckboxItem
+          onClick={() =>
+            editor
+              .getTransforms(BlockSelectionPlugin)
+              .blockSelection.setIndent(1)
+          }
+        >
+          Increase indent
         </ContextMenuCheckboxItem>
-        <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
-        <ContextMenuSeparator />
-        <ContextMenuRadioGroup value="pedro">
-          <ContextMenuLabel inset>People</ContextMenuLabel>
-          <ContextMenuSeparator />
-          <ContextMenuRadioItem value="pedro">
-            Pedro Duarte
-          </ContextMenuRadioItem>
-          <ContextMenuRadioItem value="colm">Colm Tuite</ContextMenuRadioItem>
-        </ContextMenuRadioGroup>
+        <ContextMenuCheckboxItem
+          onClick={() =>
+            editor
+              .getTransforms(BlockSelectionPlugin)
+              .blockSelection.setIndent(-1)
+          }
+        >
+          Decrease indent
+        </ContextMenuCheckboxItem>
+        <ContextMenuSub>
+          <ContextMenuSubTrigger inset>Align</ContextMenuSubTrigger>
+          <ContextMenuSubContent className="w-48">
+            <ContextMenuItem onClick={() => handleAlign('left')}>
+              Left
+            </ContextMenuItem>
+            <ContextMenuItem onClick={() => handleAlign('center')}>
+              Center
+            </ContextMenuItem>
+            <ContextMenuItem onClick={() => handleAlign('right')}>
+              Right
+            </ContextMenuItem>
+          </ContextMenuSubContent>
+        </ContextMenuSub>
       </ContextMenuContent>
     </ContextMenu>
   );
