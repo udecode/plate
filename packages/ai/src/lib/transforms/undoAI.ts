b5dfdc1c3f4cb760685b61e3925e82ff894cf4ab
zbeyens <zbeyens@udecode.dev>
2024-10-24 01:01:05 +0200

    fix

--- packages/ai/src/lib/transforms/undoAI.ts
+++ packages/ai/src/lib/transforms/undoAI.ts
@@ -1,7 +1,15 @@
-import type { SlateEditor } from '@udecode/plate-common';
+import { type SlateEditor, someNode } from '@udecode/plate-common';
+
+import { AIPlugin } from '../../react';
 
 export const undoAI = (editor: SlateEditor) => {
-  if ((editor.history.undos.at(-1) as any)?.ai) {
+  if (
+    (editor.history.undos.at(-1) as any)?.ai &&
+    someNode(editor, {
+      at: [],
+      match: (n) => !!n[AIPlugin.key],
+    })
+  ) {
     editor.undo();
     editor.history.redos.pop();
   }
