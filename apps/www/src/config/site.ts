6f30e93f1295bd27074ddee74f113c4a17aa44e7
Felix Feng <felixfeng33@gmail.com>
2024-10-24 09:32:31 +0800

    iframe

--- apps/www/src/config/site.ts
+++ apps/www/src/config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
     discord: 'https://discord.gg/mAZRuBzGM3',
     github: 'https://github.com/udecode/plate',
     platePro: 'https://pro.platejs.org',
-    potionIframe: 'https://potion.platejs.org/ai-menu/?iframe=true',
+    potionIframe: 'https://potion.platejs.org/playground?iframe=true',
     potionTemplate: 'https://pro.platejs.org/docs/templates/potion',
     profile: 'https://github.com/zbeyens',
     twitter: 'https://twitter.com/zbeyens',
