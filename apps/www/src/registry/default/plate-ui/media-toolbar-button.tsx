65eaf88f5fc046021fb57bffab24a3a37509725e
Felix Feng <felixfeng33@gmail.com>
2024-11-11 20:54:56 +0800

    icons

--- apps/www/src/registry/default/plate-ui/media-toolbar-button.tsx
+++ apps/www/src/registry/default/plate-ui/media-toolbar-button.tsx
@@ -2,26 +2,59 @@
 
 import React from 'react';
 
-import type { ImagePlugin, MediaEmbedPlugin } from '@udecode/plate-media/react';
-
 import { withRef } from '@udecode/cn';
 import { useEditorRef } from '@udecode/plate-common/react';
-import { ImageIcon } from 'lucide-react';
+import {
+  AudioPlugin,
+  FilePlugin,
+  ImagePlugin,
+  VideoPlugin,
+} from '@udecode/plate-media/react';
+import { AudioLinesIcon, FileUpIcon, Film, ImageIcon } from 'lucide-react';
 import { useFilePicker } from 'use-file-picker';
 
 import { ToolbarButton } from './toolbar';
 
+const MEDIA_CONFIG: Record<
+  string,
+  {
+    accept: string[];
+    icon: React.ReactNode;
+  }
+> = {
+  [AudioPlugin.key]: {
+    accept: ['audio/*'],
+    icon: <AudioLinesIcon />,
+  },
+  [FilePlugin.key]: {
+    accept: ['*'],
+    icon: <FileUpIcon />,
+  },
+  [ImagePlugin.key]: {
+    accept: ['image/*'],
+    icon: <ImageIcon />,
+  },
+  [VideoPlugin.key]: {
+    accept: ['video/*'],
+    icon: <Film />,
+  },
+};
+
 export const MediaToolbarButton = withRef<
   typeof ToolbarButton,
   {
-    nodeType?: typeof ImagePlugin.key | typeof MediaEmbedPlugin.key;
+    nodeType:
+      | typeof AudioPlugin.key
+      | typeof FilePlugin.key
+      | typeof ImagePlugin.key
+      | typeof VideoPlugin.key;
   }
 >(({ nodeType, ...rest }, ref) => {
   const editor = useEditorRef();
 
   /** Open file picker */
   const { openFilePicker } = useFilePicker({
-    accept: ['image/*'],
+    accept: MEDIA_CONFIG[nodeType].accept,
     multiple: true,
     onFilesSelected: ({ plainFiles: updatedFiles }) => {
       (editor as any).tf.insert.media(updatedFiles);
@@ -30,7 +63,7 @@ export const MediaToolbarButton = withRef<
 
   return (
     <ToolbarButton ref={ref} onClick={openFilePicker} {...rest}>
-      <ImageIcon />
+      {MEDIA_CONFIG[nodeType].icon}
     </ToolbarButton>
   );
 });
