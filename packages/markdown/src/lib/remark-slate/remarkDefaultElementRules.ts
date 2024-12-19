444307e08b03dec2d329ded0916d39b63e89dcc3
tomdyqin <tomdyqin@tencent.com>
2024-12-19 16:43:04 +0800

    fix: action_item not checked

--- packages/markdown/src/lib/remark-slate/remarkDefaultElementRules.ts
+++ packages/markdown/src/lib/remark-slate/remarkDefaultElementRules.ts
@@ -82,7 +82,7 @@ export const remarkDefaultElementRules: RemarkElementRules = {
           _node.children?.forEach((listItem) => {
             const defaultType = options.editor.getType({ key: 'p' });
             const todoListType = options.editor.getType({ key: 'action_item' }) ?? defaultType
-            const listItemType = listItem.checked ? todoListType : defaultType;
+            const listItemType = typeof listItem.checked !== 'undefined' ? todoListType : defaultType;
             if (!listItem.children) {
               listItems.push({
                 children: remarkTransformElementChildren(listItem, options),
