e80501a9a451e91587156981de63ad9c0b9ea35a
Felix Feng <felixfeng33@gmail.com>
2024-12-21 15:36:17 +0800

    content cn

--- apps/www/contentlayer.config.js
+++ apps/www/contentlayer.config.js
@@ -95,7 +95,7 @@ export const Doc = defineDocumentType(() => ({
 }));
 
 export default makeSource({
-  contentDirPath: './content',
+  contentDirPath: './content/en',
   documentTypes: [Doc],
   mdx: {
     rehypePlugins: [
