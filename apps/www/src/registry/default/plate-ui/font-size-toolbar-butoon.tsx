f91c5ec2b80279e231831b7e3f239ff59ba3cc8e
Felix Feng <felixfeng33@gmail.com>
2024-12-26 20:12:20 +0800

    feat

--- apps/www/src/registry/default/plate-ui/font-size-toolbar-butoon.tsx
+++ apps/www/src/registry/default/plate-ui/font-size-toolbar-butoon.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
 
+import { cn } from '@udecode/cn';
 import {
   type TElement,
   getAboveNode,
@@ -19,10 +20,22 @@ import { Minus, Plus } from 'lucide-react';
 
 import { ToolbarButton } from './toolbar';
 
+const DEFAULT_FONT_SIZE = '16px';
+
 const FONT_SIZE_MAP = {
   [HEADING_KEYS.h1]: '36px',
   [HEADING_KEYS.h2]: '24px',
   [HEADING_KEYS.h3]: '20px',
+};
+
+const pxUnitConfig = {
+  increment: 1,
+  unitRegex: /^([\d.]+)px$/i,
+};
+
+const remUnitConfig = {
+  increment: 0.125,
+  unitRegex: /^([\d.]+)rem$/i,
 };
 
 export function FontSizeToolbarButton() {
@@ -40,14 +53,14 @@ export function FontSizeToolbarButton() {
       at: editor.selection?.focus,
     });
 
-    if (!entry) return '16px';
+    if (!entry) return DEFAULT_FONT_SIZE;
 
     const [node] = entry;
 
     if (node.type in FONT_SIZE_MAP)
       return FONT_SIZE_MAP[node.type as keyof typeof FONT_SIZE_MAP];
 
-    return '16px';
+    return DEFAULT_FONT_SIZE;
   }, []);
 
   const handleInputChange = () => {
@@ -63,17 +76,21 @@ export function FontSizeToolbarButton() {
   const displayValue = isFocused ? inputValue : cursorFontSize;
 
   return (
-    <div className="flex items-center gap-1 rounded-md bg-muted/70 p-0">
+    <div className="flex items-center gap-1 rounded-md bg-muted/60 p-0">
       <ToolbarButton
-        onClick={() =>
-          setChangedFontSize({ fontSize: displayValue, increase: false })
-        }
-        onMouseDown={(e) => e.preventDefault()}
+        onClick={() => {
+          setChangedFontSize({
+            fontSize: displayValue,
+            increase: false,
+            unitConfig: [pxUnitConfig, remUnitConfig],
+          });
+          focusEditor(editor);
+        }}
       >
         <Minus />
       </ToolbarButton>
       <input
-        className="w-16 bg-transparent px-2 text-center"
+        className={cn('w-16 bg-transparent px-2 text-center hover:bg-muted')}
         value={displayValue}
         onBlur={() => {
           setIsFocused(false);
@@ -85,13 +102,18 @@ export function FontSizeToolbarButton() {
           setInputValue(cursorFontSize);
         }}
         onKeyDown={(e) => e.key === 'Enter' && handleInputChange()}
+        data-plate-focus="true"
         type="text"
       />
       <ToolbarButton
-        onClick={() =>
-          setChangedFontSize({ fontSize: displayValue, increase: true })
-        }
-        onMouseDown={(e) => e.preventDefault()}
+        onClick={() => {
+          setChangedFontSize({
+            fontSize: displayValue,
+            increase: true,
+            unitConfig: [pxUnitConfig, remUnitConfig],
+          });
+          focusEditor(editor);
+        }}
       >
         <Plus />
       </ToolbarButton>
--- packages/font/src/lib/utils/setChangedFontSize.ts
+++ packages/font/src/lib/utils/setChangedFontSize.ts
@@ -6,43 +6,46 @@ export const setChangedFontSize = (
   editor: SlateEditor,
   options: getChangedFontSizeOptions
 ): void => {
-  const { fontSize, increase } = options;
+  const { fontSize, increase, unitConfig } = options;
 
   setMarks(editor, {
-    [BaseFontSizePlugin.key]: getChangedFontSize({ fontSize, increase }),
+    [BaseFontSizePlugin.key]: getChangedFontSize({
+      fontSize,
+      increase,
+      unitConfig,
+    }),
   });
 };
 
 export type getChangedFontSizeOptions = {
+  unitConfig: {
+    increment: number;
+    unitRegex: RegExp;
+  }[];
   fontSize: string;
   increase: boolean;
 };
 
 const getChangedFontSize = (options: getChangedFontSizeOptions): string => {
-  const { fontSize, increase } = options;
+  const { fontSize, increase, unitConfig } = options;
 
   if (!fontSize) return '16px';
 
-  const pxMatch = /^([\d.]+)px$/i.exec(fontSize);
+  for (const config of unitConfig) {
+    const match = config.unitRegex.exec(fontSize);
 
-  if (pxMatch) {
-    const [, value] = pxMatch;
-    const numericValue = Number.parseFloat(value);
-    const newValue = increase ? numericValue + 2 : numericValue - 2;
+    if (match) {
+      const [, value] = match;
+      const numericValue = Number.parseFloat(value);
+      const newValue = increase
+        ? numericValue + config.increment
+        : numericValue - config.increment;
 
-    return `${Number(newValue.toFixed(3))}px`;
+      const unit = match[0].replace(value, '');
+
+      return `${Number(newValue.toFixed(3))}${unit}`;
+    }
   }
 
-  const remMatch = /^([\d.]+)rem$/i.exec(fontSize);
-
-  if (remMatch) {
-    const [, value] = remMatch;
-    const numericValue = Number.parseFloat(value);
-    const newValue = increase ? numericValue + 0.125 : numericValue - 0.125;
-
-    return `${Number(newValue.toFixed(3))}rem`;
-  }
-
-  // TODO: Handle other units
   return fontSize;
 };
