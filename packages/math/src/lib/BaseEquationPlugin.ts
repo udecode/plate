73f3ec63691ee8c47eda93f3647d159dc62e50ec
Felix Feng <felixfeng33@gmail.com>
2024-12-27 17:25:31 +0800

    fix

--- packages/math/src/lib/BaseEquationPlugin.ts
+++ packages/math/src/lib/BaseEquationPlugin.ts
@@ -14,16 +14,6 @@ export interface TEquationElement extends TElement {
 
 export const BaseEquationPlugin = createSlatePlugin({
   key: 'equation',
-  extendEditor: ({ editor, type }) => {
-    console.log('🚀 ~ type:', type);
-    const { isSelectable } = editor;
-
-    editor.isSelectable = (element) => {
-      return (element as TElement).type !== type && isSelectable(element);
-    };
-
-    return editor;
-  },
   node: { isElement: true, isVoid: true },
 }).extendEditorTransforms(({ editor }) => ({
   insert: {
