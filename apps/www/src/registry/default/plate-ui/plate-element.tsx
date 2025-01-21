dd2af04b018fb2f6f8481d2d52fef95eae1a2181
zbeyens <zbeyens@udecode.dev>
2025-01-21 18:57:17 +0100

    fix

--- apps/www/src/registry/default/plate-ui/plate-element.tsx
+++ apps/www/src/registry/default/plate-ui/plate-element.tsx
@@ -8,14 +8,17 @@ import { PlateElement as PlateElementPrimitive } from '@udecode/plate/react';
 
 import { BlockSelection } from './block-selection';
 
-export const PlateElement = React.forwardRef<HTMLDivElement, PlateElementProps>(
-  ({ children, ...props }: PlateElementProps, ref) => {
-    return (
-      <PlateElementPrimitive ref={ref} {...props}>
-        {children}
+export const PlateElement = React.forwardRef<
+  HTMLDivElement,
+  PlateElementProps & { blockSelectionClassName?: string }
+>(({ blockSelectionClassName, children, ...props }, ref) => {
+  return (
+    <PlateElementPrimitive ref={ref} {...props}>
+      {children}
 
-        {props.className?.includes('slate-selectable') && <BlockSelection />}
-      </PlateElementPrimitive>
-    );
-  }
-);
+      {props.className?.includes('slate-selectable') && (
+        <BlockSelection className={blockSelectionClassName} />
+      )}
+    </PlateElementPrimitive>
+  );
+});
