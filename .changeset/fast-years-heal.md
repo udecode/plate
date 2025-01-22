a122f202bf99b0ae239151f7b46c58342842bfb9
Josh Gillies <319694+joshgillies@users.noreply.github.com>
2025-01-22 17:56:38 +1100

    add missing `editor.api.isEditor` method

---
+++ .changeset/fast-years-heal.md
@@ -0,0 +1,5 @@
+---
+'@udecode/slate': patch
+---
+
+add missing `editor.api.isEditor` method
--- packages/slate/src/create-editor.ts
+++ packages/slate/src/create-editor.ts
@@ -17,6 +17,7 @@ import type { Editor, Value } from './interfaces/editor/editor-type';
 import {
   type EditorApi,
   type LegacyEditorMethods,
+  NodeApi,
   RangeApi,
 } from './interfaces';
 import { blur } from './internal/dom-editor/blur';
@@ -297,6 +298,7 @@ export const createEditor = <V extends Value>({
     isCollapsed: () => RangeApi.isCollapsed(editor.selection),
     isComposing: bindFirst(isComposing, editor),
     isEditorEnd: bindFirst(isEditorEnd, editor),
+    isEditor: (value) => NodeApi.isEditor(value),
     isExpanded: () => RangeApi.isExpanded(editor.selection),
     isFocused: bindFirst(isFocused, editor),
     isMerging: bindFirst(HistoryApi.isMerging, editor as any) as any,
