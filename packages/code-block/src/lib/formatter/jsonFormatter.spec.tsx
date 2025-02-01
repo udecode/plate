9b89880e2376a6ab8cee25985a2ac966c9873e73
patrickhertling <patrickhertling@gmail.com>
2025-02-01 14:13:18 +0200

    Fix tests

--- packages/code-block/src/lib/formatter/jsonFormatter.spec.tsx
+++ packages/code-block/src/lib/formatter/jsonFormatter.spec.tsx
@@ -1,28 +1,21 @@
-import { createEditor } from '@udecode/plate';
-import { createPlateEditor } from '@udecode/plate/react';
-
 import { formatCode, isValidSyntax } from './formatter';
 
 describe('JsonFormatter', () => {
   it('should detect valid JSON', () => {
     const json = '{ "name": "ChatGPT", "type": "AI" }';
-    const isValid = isValidSyntax(json);
+    const isValid = isValidSyntax(json, 'json');
     expect(isValid).toBe(true);
   });
 
   it('should detect invalid JSON', () => {
-    const editor = createEditor();
-    const plateEditor = createPlateEditor({ editor });
     const json = '{ name: "ChatGPT", type: AI }';
-    const isValid = isValidSyntax(json);
+    const isValid = isValidSyntax(json, 'json');
     expect(isValid).toBe(false);
   });
 
   it('should format JSON', () => {
-    const editor = createEditor();
-    const plateEditor = createPlateEditor({ editor });
     const json = '{"name":"ChatGPT","type":"AI"}';
-    const formattedJson = formatCode(json);
+    const formattedJson = formatCode(json, 'json');
     const expected = `{
   "name": "ChatGPT",
   "type": "AI"
@@ -31,10 +24,8 @@ describe('JsonFormatter', () => {
   });
 
   it('should not format invalid JSON', () => {
-    const editor = createEditor();
-    const plateEditor = createPlateEditor({ editor });
     const json = '{ name: "ChatGPT", type: AI }';
-    const formattedJson = formatCode(json);
+    const formattedJson = formatCode(json, 'json');
     expect(formattedJson).toBe(json);
   });
 });
