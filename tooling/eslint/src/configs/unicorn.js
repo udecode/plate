77402c9165c9c558fb65ef6225f4d76839012d4d
zbeyens <zbeyens@udecode.dev>
2025-02-06 19:51:24 +0100

    chore

--- tooling/eslint/src/configs/unicorn.js
+++ tooling/eslint/src/configs/unicorn.js
@@ -37,6 +37,7 @@ export default defineConfig(
       'unicorn/no-nested-ternary': 'off',
       'unicorn/no-new-array': 'off',
       'unicorn/no-null': 'off',
+      'unicorn/no-process-exit': 'off',
       'unicorn/no-thenable': 'off',
       'unicorn/no-useless-undefined': 'off',
       'unicorn/prefer-export-from': 'off',
