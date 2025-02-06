fc72e1ad2b1d75762b2bd7296679ee1e89142e9f
zbeyens <zbeyens@udecode.dev>
2025-02-07 00:55:19 +0100

    upgrade

--- templates/plate-playground-template/postcss.config.mjs
+++ templates/plate-playground-template/postcss.config.mjs
@@ -1,8 +1,1 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
+export default { plugins: { '@tailwindcss/postcss': {} } };
