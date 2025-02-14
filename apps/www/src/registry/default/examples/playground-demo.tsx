cd0594c147ec6ad48eb46e4b160d9a783181deef
Felix Feng <felixfeng33@gmail.com>
2025-02-14 14:33:30 +0800

    revert

--- apps/www/src/registry/default/examples/playground-demo.tsx
+++ apps/www/src/registry/default/examples/playground-demo.tsx
@@ -100,12 +100,7 @@ export default function PlaygroundDemo({ className }: { className?: string }) {
   );
 
   return (
-    <Plate
-      onValueChange={({ value }) => {
-        console.log(value, 'fj');
-      }}
-      editor={editor}
-    >
+    <Plate editor={editor}>
       <EditorContainer className={className}>
         <Editor variant="demo" className="pb-[20vh]" spellCheck={false} />
       </EditorContainer>
