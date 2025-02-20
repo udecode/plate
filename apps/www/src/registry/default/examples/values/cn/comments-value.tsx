1cdd7db7304515ffe551dacd57dd1bbd2a012f9d
Felix Feng <felixfeng33@gmail.com>
2025-02-20 10:23:06 +0800

    suggestion

--- apps/www/src/registry/default/examples/values/cn/comments-value.tsx
+++ apps/www/src/registry/default/examples/values/cn/comments-value.tsx
@@ -1,21 +1,55 @@
 import type { Value } from '@udecode/plate';
 export const commentsValue: Value = [
   {
-    children: [{ text: '评论与建议' }],
+    children: [{ text: '评论和建议' }],
     type: 'h2',
   },
   {
     children: [
       {
-        text: '为您的内容添加评论或建议，以提供额外的上下文、见解或与他人协作',
+        text: '为',
       },
-    ],
-    type: 'p',
-  },
-  {
-    children: [
       {
-        text: '通过选择文本并点击工具栏中的按钮来试用。',
+        comment: true,
+        comment_discussion1: true,
+        text: '您的内容添加评论',
+      },
+      {
+        text: '以提供额外',
+      },
+      {
+        text: '的上下文、',
+      },
+      {
+        suggestion: true,
+        suggestion_suggestion1: {
+          id: 'suggestion1',
+          createdAt: Date.now(),
+          type: 'remove',
+          userId: 'user3',
+        },
+        text: '见解',
+      },
+      {
+        suggestion: true,
+        suggestion_suggestion1: {
+          id: 'suggestion1',
+          createdAt: Date.now(),
+          type: 'insert',
+          userId: 'user3',
+        },
+        text: '建议',
+      },
+      {
+        text: '，或者',
+      },
+      {
+        comment: true,
+        comment_discussion2: true,
+        text: '协作',
+      },
+      {
+        text: '与他人',
       },
     ],
     type: 'p',
