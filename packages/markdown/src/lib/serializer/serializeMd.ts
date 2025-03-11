906da647c1e17fd07c7fa216ac8c4555f03f5ade
Martin Lysk <martin.lysk@gmail.com>
2025-03-11 15:36:26 +0100

    chore - removes irregular whitespace

--- packages/markdown/src/lib/serializer/serializeMd.ts
+++ packages/markdown/src/lib/serializer/serializeMd.ts
@@ -12,7 +12,7 @@ import { serializeMdNodes } from './serializeMdNodes';
 /** Serialize the editor value to Markdown. */
 export const serializeMd = (
   editor: SlateEditor,
-  options?: {value: Parameters<typeof serializeMdNodes>['0']} & Parameters<typeof serializeMdNodes>['1']
+  options?: {value: Parameters<typeof serializeMdNodes>['0']} & Parameters<typeof serializeMdNodes>['1']
 ) => {
   const plugins = editor.pluginList.filter(
     (p) => p.node.isElement || p.node.isLeaf
