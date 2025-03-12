c5163053514d4176abcf16ba15526dbfc2aba6d6
zbeyens <zbeyens@users.noreply.github.com>
2025-03-12 14:10:44 +0000

    docs

--- apps/www/src/__registry__/index.tsx
+++ apps/www/src/__registry__/index.tsx
@@ -1007,7 +1007,7 @@ export const Index: Record<string, any> = {
       name: "tooltip",
       description: "",
       type: "registry:ui",
-      registryDependencies: [],
+      registryDependencies: ["button"],
       files: [{
         path: "src/registry/default/plate-ui/tooltip.tsx",
         type: "registry:ui",
