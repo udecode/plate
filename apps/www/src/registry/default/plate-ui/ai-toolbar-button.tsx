9dd6b5142eef40b6dc9332fed4dffc7f26bef898
Felix Feng <felixfeng33@gmail.com>
2025-04-10 18:31:48 +0800

    fix

--- apps/www/src/registry/default/plate-ui/ai-toolbar-button.tsx
+++ apps/www/src/registry/default/plate-ui/ai-toolbar-button.tsx
@@ -4,14 +4,12 @@ import React from 'react';
 
 import { withRef } from '@udecode/cn';
 import { AIChatPlugin } from '@udecode/plate-ai/react';
-import { useEditorPlugin, useEditorRef } from '@udecode/plate/react';
+import { useEditorPlugin } from '@udecode/plate/react';
 
 import { ToolbarButton } from './toolbar';
 
 export const AIToolbarButton = withRef<typeof ToolbarButton>(
   ({ children, ...rest }, ref) => {
-    const editor = useEditorRef();
-
     const { api } = useEditorPlugin(AIChatPlugin);
 
     return (
@@ -19,17 +17,7 @@ export const AIToolbarButton = withRef<typeof ToolbarButton>(
         ref={ref}
         {...rest}
         onClick={() => {
-          editor.tf.insertNodes(
-            {
-              children: [{ text: 'anchor' }],
-              type: 'p',
-            },
-            {
-              at: [0],
-              nextBlock: true,
-            }
-          );
-          // api.aiChat.show();
+          api.aiChat.show();
         }}
         onMouseDown={(e) => {
           e.preventDefault();
