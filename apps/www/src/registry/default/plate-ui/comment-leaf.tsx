ad020275526b738cc6edb3cb0ed76cf97ba46f09
zbeyens <zbeyens@udecode.dev>
2025-04-30 15:09:38 +0200

    className before

--- apps/www/src/registry/default/plate-ui/comment-leaf.tsx
+++ apps/www/src/registry/default/plate-ui/comment-leaf.tsx
@@ -32,20 +32,18 @@ export function CommentLeaf({
     <PlateLeaf
       {...props}
       className={cn(
+        className,
         'border-b-2 border-b-highlight/[.36] bg-highlight/[.13] transition-colors duration-200',
         (isHover || isActive) && 'border-b-highlight bg-highlight/25',
         isOverlapping && 'border-b-2 border-b-highlight/[.7] bg-highlight/25',
         (isHover || isActive) &&
           isOverlapping &&
-          'border-b-highlight bg-highlight/45',
-        className
+          'border-b-highlight bg-highlight/45'
       )}
       onClick={() => setOption('activeId', currentId ?? null)}
       onMouseEnter={() => setOption('hoverId', currentId ?? null)}
       onMouseLeave={() => setOption('hoverId', null)}
-      nodeProps={{
-        ...nodeProps,
-      }}
+      nodeProps={{ ...nodeProps }}
     >
       {children}
     </PlateLeaf>
