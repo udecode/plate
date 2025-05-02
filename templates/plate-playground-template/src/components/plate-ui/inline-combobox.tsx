93a726ed9d5e0d6b1564cee72105fbad570c7ddd
SyedTayyabUlMazhar <43899564+SyedTayyabUlMazhar@users.noreply.github.com>
2025-05-02 19:11:02 +0500

    Update inline-combobox.tsx
    Fix tailwind styling. Change z-500 to z-[500] because z-500 is not recognised by default tailwind css.

--- templates/plate-playground-template/src/components/plate-ui/inline-combobox.tsx
+++ templates/plate-playground-template/src/components/plate-ui/inline-combobox.tsx
@@ -272,7 +272,7 @@ const InlineComboboxContent: typeof ComboboxPopover = ({
     <Portal>
       <ComboboxPopover
         className={cn(
-          'z-500 max-h-[288px] w-[300px] overflow-y-auto rounded-md bg-popover shadow-md',
+          'z-[500] max-h-[288px] w-[300px] overflow-y-auto rounded-md bg-popover shadow-md',
           className
         )}
         {...props}
