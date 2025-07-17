5f0c8718ff6b3e634caf1f284169e266e50d4622
Felix Feng <felixfeng33@gmail.com>
2025-07-17 19:23:04 +0800

    blogs

--- apps/www/src/app/(app)/blogs/_components/mdx-components.tsx
+++ apps/www/src/app/(app)/blogs/_components/mdx-components.tsx
@@ -1,10 +1,15 @@
 import type { MDXComponents } from 'mdx/types';
 
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import Image from 'next/image';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+    img: (props: any) => {
+      return <Image className='border rounded-lg' {...props} />;
+    },
+
   };
 }
