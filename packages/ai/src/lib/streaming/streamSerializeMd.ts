d0d4c787418a44dd31008ffbbe30903444f035c5
Felix Feng <felixfeng33@gmail.com>
2025-07-26 16:21:36 +0800

    fix test

--- packages/ai/src/lib/streaming/streamSerializeMd.ts
+++ packages/ai/src/lib/streaming/streamSerializeMd.ts
@@ -16,7 +16,7 @@ const trimEndHeading = (
 
   if (
     lastBlock &&
-    headingKeys.has(getPluginType(editor, KEYS.p) as any) &&
+    headingKeys.has(getPluginType(editor, lastBlock.type as string) as any) &&
     ElementApi.isElement(lastBlock)
   ) {
     const lastTextNode = lastBlock.children.at(-1);
