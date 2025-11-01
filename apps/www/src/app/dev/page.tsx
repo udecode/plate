88fce7469206a4ed5593b4dbf841ca1788e07383
Felix Feng <felixfeng33@gmail.com>
2025-11-01 16:50:46 +0800

    test

--- apps/www/src/app/dev/page.tsx
+++ apps/www/src/app/dev/page.tsx
@@ -4,7 +4,8 @@ export default async function DevPage() {
   return (
     <main>
       {/* <EditorViewClient /> */}
-      <MarkdownStreamDemo />
+      {/* <MarkdownStreamDemo /> */}
+      <iframe height="500px" width="40%" src="https://evergarden.ai/iframe/nh7b9bwz6r72ntgr5e95ye5brs7tjacp" ></iframe>
     </main>
   );
 }
