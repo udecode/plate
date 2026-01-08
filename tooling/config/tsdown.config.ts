b4637bcddb23c3b766ef1906a92b47ba821e1b6e
Felix Feng <felixfeng33@gmail.com>
2026-01-08 21:31:17 +0800

    fix

--- tooling/config/tsdown.config.ts
+++ tooling/config/tsdown.config.ts
@@ -61,6 +61,7 @@ export default defineConfig((opts) => [
     plugins: [
       pluginBabel({
         babelHelpers: 'bundled',
+        exclude: '**/static/**',
         parserOpts: {
           sourceType: 'module',
           plugins: ['jsx', 'typescript'],
