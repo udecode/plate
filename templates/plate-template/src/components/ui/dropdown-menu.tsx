550b47716188484cd37e1515ed59088cee7e2be2
Ziad Beyens <ziad.beyens@gmail.com>
2026-09-04 17:08:34 +0200

    chore: fix template sync after release [skip release]

--- templates/plate-template/src/components/ui/dropdown-menu.tsx
+++ templates/plate-template/src/components/ui/dropdown-menu.tsx
@@ -1,10 +1,9 @@
 'use client';
 
+import { cn } from 'cn';
 import { CheckIcon, ChevronRightIcon, CircleIcon } from 'lucide-react';
 import { DropdownMenu as DropdownMenuPrimitive } from 'radix-ui';
 import type * as React from 'react';
-
-import { cn } from '@/lib/utils';
 
 function DropdownMenu({
   ...props
